1e37114201d941a0417d6a727d5b66d82e1c9f96
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2022-11-15 00:58:26 +0800

    automatic sync when opening the alias file and init.vim

--- aliases.zsh
+++ aliases.zsh
@@ -10,14 +10,27 @@ alias v="nvim"
 alias prog="cd /mnt/c/Programming"
 alias libv2="cd /mnt/c/Users/serge/Documents/AutoHotkey/Lib"
 alias del="rm -rf"
-alias g="google"
-alias y="youtube"
 
 # Aliases
 alias gitcreate="git add . && git commit -m \"first commit\" && git -u push origin main"
 
+# Paths
+nvim_windows=/mnt/c/Programming/dotfiles/init.vim
+nvim_linux=~/.config/nvim/init.vim
+aliases_windows=/mnt/c/Programming/dotfiles/aliases.zsh
+aliases_linux=$ZSH/custom/aliases.zsh
+
+syncFrom() {
+   cp -u $nvim_windows $nvim_linux
+   cp -u $aliases_windows $aliases_linux
+}
+syncTo() {
+   cp -u $nvim_linux $nvim_windows
+   cp -u $aliases_linux $aliases_windows
+}
+
 # Files
-alias n="cp -u /mnt/c/Programming/backups/init.vim ~/.config/nvim/init.vim && nvim ~/.config/nvim/init.vim && cp -u ~/.config/nvim/init.vim /mnt/c/Programming/backups/init.vim"
-alias alis="nvim $ZSH/custom/aliases.zsh"
+alias n="syncFrom && nvim $nvim_linux && syncTo"
+alias alis="syncFrom && nvim $aliases_linux && syncTo"
 alias zshConf="nvim ~/.zshrc"
 alias p10kConf="nvim ~/.p10k.zsh"
