2aea4e3a6afb593ca7b620f865245f91267680ef
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-09 08:35:07 +0800

    change directory color to pink

--- .p10k.zsh
+++ .p10k.zsh
@@ -210,7 +210,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=31
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=5
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
