302f3a7e45f3f79e2735cc7533d6ce130090f84e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-11 09:02:26 +0800

    change prompt symbol to always stay the same color for less clutter

--- .p10k.zsh
+++ .p10k.zsh
@@ -232,7 +232,7 @@
 	# Green prompt symbol if the last command succeeded.
 	typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=218
 	# Red prompt symbol if the last command failed.
-	typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=210
+	typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=218
 	# Default prompt symbol.
 	typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='>>>'
 	# Prompt symbol in command vi mode.
