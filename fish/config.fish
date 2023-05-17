0d5a9f343da8cee20c22907b6265da053d99d203
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-17 18:42:53 +0800

    configure prompt character

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,13 @@ set -g fish_lazy_load_functions 'true'
 set -g tide_left_prompt_items pwd context shlvl jobs git status cmd_duration newline time character
 set -g tide_right_prompt_items
 
+set -g tide_character_color $color_pink
+set -g tide_character_color_failure $color_pink
+set -g tide_character_icon '>>>'
+set -g tide_character_vi_icon_default '$$$'
+set -g tide_character_vi_icon_replace '$$$'
+set -g tide_character_vi_icon_visual 'OOO'
+
 set -g fish_cursor_default block
 set -g fish_cursor_insert line
 set -g fish_cursor_replace_one underscore
