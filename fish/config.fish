0585ee8bc7d5a5842d98b9c4b3314fd8e2ffcb9f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-17 18:42:40 +0800

    configure items in fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -3,12 +3,12 @@
 set -g color_pink FFAFD7
 set -g color_purple AF87FF
 
-set -g __fish_git_prompt_char_stateseparator ' '
 set -g fish_lazy_load_completions 'true'
 set -g fish_lazy_load_functions 'true'
-set -g fish_color_cwd $color_pink
-set -g fish_color_symbol $color_pink
-set -g fish_color_date $color_purple
+
+set -g tide_left_prompt_items pwd context shlvl jobs git status cmd_duration newline time character
+set -g tide_right_prompt_items
+
 set -g fish_cursor_default block
 set -g fish_cursor_insert line
 set -g fish_cursor_replace_one underscore
