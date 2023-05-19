7a5b2883040c6dde04ab947af4a6b43f1c398435
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-20 07:00:39 +0800

    add scripts that sets up symlinks

---
+++ fish/symlinks.fish
@@ -0,0 +1,7 @@
+#!usr/bin/fish
+mkdir -p ~/.config/fish
+rm -f ~/.config/fish/config.fish
+ln -s /mnt/c/Programming/dotfiles/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/nvim
+rm -f ~/.config/nvim/init.lua
+ln -s /mnt/c/Programming/dotfiles/init.lua ~/.config/nvim/init.lua
