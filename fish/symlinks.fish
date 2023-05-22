2bdbd42252bfdf254a022446b6d06fbdb464aabe
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-22 13:39:11 +0800

    move csharp symlinks into their own file because they depend on building

--- fish/symlinks.fish
+++ fish/symlinks.fish
@@ -6,13 +6,3 @@ ln -s /mnt/c/Programming/dotfiles/fish/config.fish ~/.config/fish/config.fish
 mkdir -p ~/.config/nvim
 rm -f ~/.config/nvim/init.lua
 ln -s /mnt/c/Programming/dotfiles/init.lua ~/.config/nvim/init.lua
-
-set -l choreTrackerBrokenSymlink /mnt/c/Programming/binaries/ChoreTracker.exe
-set -l choreTrackerTarget /mnt/c/Programming/csproj/ChoreTracker/ChoreTracker/bin/Debug/net7.0/ChoreTracker.exe
-rm -f $choreTrackerBrokenSymlink
-ln -s $choreTrackerTarget $choreTrackerBrokenSymlink
-
-set -l weldeBrokenSymlink /mnt/c/Programming/binaries/Welde.exe
-set -l weldeTarget /mnt/c/Programming/csproj/Welde/Welde/bin/Debug/net7.0/Welde.exe
-rm -f $weldeBrokenSymlink
-ln -s $weldeTarget $weldeBrokenSymlink
