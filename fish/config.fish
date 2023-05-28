95b16382fc1f4be6b0c775f29b2f6b130fbc5069
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-28 18:56:53 +0800

    move shlvl to right prompt

--- fish/config.fish
+++ fish/config.fish
@@ -46,9 +46,10 @@ set -g tide_prompt_add_newline_before false
 set -g tide_prompt_min_cols 50
 set -g tide_prompt_pad_items false
 set -g tide_left_prompt_frame_enabled true
+set -g tide_left_prompt_prefix
 set -g tide_left_prompt_suffix
-set -g tide_left_prompt_items character pwd context shlvl jobs git status newline character
-set -g tide_right_prompt_items private_mode
+set -g tide_left_prompt_items pwd context jobs git status newline character
+set -g tide_right_prompt_items shlvl private_mode
 
 set -g tide_character_color -o $color_yellow
 set -g tide_character_color_failure -o $color_yellow
