bab38644c663874976fb3a00bbb9a48e65c4cafa
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-31 20:16:33 +0800

    packages moved into symlinks

--- fish/symlinks.fish
+++ fish/symlinks.fish
@@ -1,8 +1,29 @@
 #!/usr/bin/env fish
+
+command -q git || sudo apt install git
+git config --global user.email 101342105+Axlefublr@users.noreply.github.com
+git config --global user.name Axlefublr
+git config --global init.defaultBranch main
+git config --global credential.helper store
+git config --global credential.helper '/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe'
+
+command -q pip || sudo apt install pip
+command -q exrex || pip install exrex
+command -q termdown || pip install termdown
+
+command -q tree || sudo apt install tree
+command -q fzf || sudo apt install fzf
+command -q batcat || sudo apt install bat
+command -q neofetch || sudo apt install neofetch
+command -q ncdu || sudo apt install ncdu
+command -q cmatrix || sudo apt install cmatrix
+command -q lolcat || sudo apt install lolcat
+command -q cowsay || sudo apt install cowsay
+
 mkdir -p ~/.config/fish
 rm -f ~/.config/fish/config.fish
 ln -s /mnt/c/Programming/dotfiles/fish/config.fish ~/.config/fish/config.fish
 
 mkdir -p ~/.config/nvim
-rm -f ~/.config/nvim/init.lua
 ln -s /mnt/c/Programming/dotfiles/init.lua ~/.config/nvim/init.lua
+
