81e261d725290e8e4e6ec32302b9f998821a566a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-15 02:54:50 +0800

    add file existance checks for csharp setupper

--- fish/config.fish
+++ fish/config.fish
@@ -117,6 +117,7 @@ abbr -a clock 'termdown -z'
 abbr -a xcode 'xargs code-insiders'
 abbr -a fishp 'fish -P'
 abbr -a mvf   'mv -f'
+abbr -a pri   'echo $fish_private_mode'
 
 abbr -a rm  'trash-put'
 abbr -a rma 'trash-put *'
--- fish/setup/csharp.fish
+++ fish/setup/csharp.fish
@@ -2,8 +2,16 @@
 
 set -l choreTrackerBrokenSymlink /mnt/c/Programming/binaries/ChoreTracker.exe
 set -l choreTrackerTarget /mnt/c/Programming/csproj/ChoreTracker/ChoreTracker/bin/Debug/net7.0/ChoreTracker.exe
-ln -sf $choreTrackerTarget $choreTrackerBrokenSymlink
+if test -e $choreTrackerTarget
+	ln -sf $choreTrackerTarget $choreTrackerBrokenSymlink
+else
+	echo 'the target "'$choreTrackerTarget '" doesn\'t exist!'
+end
 
 set -l weldeBrokenSymlink /mnt/c/Programming/binaries/Welde.exe
 set -l weldeTarget /mnt/c/Programming/csproj/Welde/Welde/bin/Debug/net7.0/Welde.exe
-ln -sf $weldeTarget $weldeBrokenSymlink
+if test -e $weldeTarget
+	ln -sf $weldeTarget $weldeBrokenSymlink
+else
+	echo 'the target "'$weldeTarget'" doesn\'t exist!'
+end
