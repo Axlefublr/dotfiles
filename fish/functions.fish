8f550b5e802a1ff516360567ba8c153871cd789a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-16 02:57:51 +0800

    if in a git repo, path up to the repo is shown

--- fish/functions.fish
+++ fish/functions.fish
@@ -250,8 +250,18 @@ function fish_prompt
 		echo -n $hostname
 	end
 	set_color $color_pink
-	set -l home (string escape --style=regex $HOME)
-	echo -n ' '(string replace -r "^$home" '~' $PWD)
+	printf ' '
+	set -l git_repo_full (git rev-parse --show-toplevel 2> /dev/null)
+	if test $git_repo_full
+		set -l git_repo (basename $git_repo_full)
+		set -l not_git_repo (string replace $git_repo '' $git_repo_full)
+		printf (string replace "$not_git_repo" '' $PWD)
+	else
+		set -l home (string escape --style=regex $HOME)
+		set -l cwd (string replace -r "^$home" '~' $PWD)
+		set -l base (basename $cwd)
+		printf $base
+	end
 	set -l curr_branch (git branch --show-current 2> /dev/null)
 	if test curr_branch
 		set_color $color_purple
