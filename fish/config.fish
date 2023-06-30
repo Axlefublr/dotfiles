e0bff594f6ccd5cca9b4afe1b254d30008b786fe
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-30 17:18:23 +0800

    remove pick related functions

--- fish/config.fish
+++ fish/config.fish
@@ -262,10 +262,10 @@ abbr -a ghgl    'gh gist list'
 abbr -a ghgc    'gh gist create'
 abbr -a ghge    'gh gist edit'
 
-bind -M insert \cD paste_important_path
-bind -M insert \cF paste_parent_path
-bind -M insert \cS paste_relative_path
-bind -M insert \cQ 'commandline -i (cat /tmp/pickie)'
+bind -M insert \cS paste_ranger_file
+bind -M insert \cD paste_ranger_dir
+bind -M insert \cQ 'commandline -i (cat /tmp/dickie)'
+bind -M insert \cA 'commandline -i (cat /tmp/flickie)'
 
 bind -M insert  \cR hist_insert
 bind -M default R   hist_replace
