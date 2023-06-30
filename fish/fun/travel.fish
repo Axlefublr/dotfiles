0d6f501fdbece893f33e2625365651cab3446ef5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-30 17:32:48 +0800

    bring back pick and paste_relative_path

--- fish/fun/travel.fish
+++ fish/fun/travel.fish
@@ -1,5 +1,39 @@
 #!/usr/bin/env fish
 
+function pick
+	set -l current $argv[1]
+	set -l here $argv[1]
+	while test -d $current
+		set here (ls $current -Ab -w 1 2> /dev/null)
+		if test (echo $here) = ''
+			break
+		end
+		if set -q argv[2] # prune out files if you pass the 2nd argument
+			set -l newHere
+			for obj in $here
+				if not test -f $current/$obj
+					set newHere $newHere $obj
+				end
+			end
+			set here $newHere
+		end
+		if test (echo $here) = ''
+			break
+		end
+		set here (prli $here | fzf -e --cycle --tac)
+		if not test $here
+			break
+		end
+		set current "$current/$here"
+	end
+	if test $current = '.'
+		return 1
+	end
+	set current (string replace -r '^\/\/' '/' $current)
+	printf $current | tee /tmp/pickie
+end
+funcsave pick > /dev/null
+
 function paste_ranger_file
 	ranger --choosefile /tmp/flickie
 	commandline -i (cat /tmp/flickie)
@@ -11,3 +45,12 @@ function paste_ranger_dir
 	commandline -i (cat /tmp/dickie)
 end
 funcsave paste_ranger_dir > /dev/null
+
+function paste_relative_path
+	set -l picked (pick .)
+	if not test $picked
+		return 1
+	end
+	commandline -i $picked
+end
+funcsave paste_relative_path > /dev/null
