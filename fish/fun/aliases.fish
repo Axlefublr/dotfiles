b59a607f8da6e4abd90c322da26f575f88613b1b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-02 02:22:37 +0800

    add tgg alias (it's like -m for git commit)

--- fish/fun/aliases.fish
+++ fish/fun/aliases.fish
@@ -10,3 +10,4 @@ alias --save node 'node.exe' > /dev/null
 alias --save ... 'cd ../..' > /dev/null
 alias --save .... 'cd ../../..' > /dev/null
 alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode &| tee /tmp/pagie &| less' > /dev/null
+alias --save tgg 'tgpt "$argv"' > /dev/null
