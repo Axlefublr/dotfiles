e91aacd4fff0f213b298b8f0fca206777676340a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-01 16:49:00 +0800

    disable auto open_with

--- fish/setup/ranger.fish
+++ fish/setup/ranger.fish
@@ -4,3 +4,5 @@ command -q ranger || sudo apt install ranger
 ranger --copy-config all
 cp -f ~/.config/ranger/rc.conf /mnt/c/Programming/dotfiles/rc.conf
 ln -sf /mnt/c/Programming/dotfiles/rc.conf ~/.config/ranger/rc.conf
+cp -f ~/.config/ranger/rifle.conf /mnt/c/Programming/dotfiles/rifle.conf
+ln -sf /mnt/c/Programming/dotfiles/rifle.conf ~/.config/ranger/rifle.conf
