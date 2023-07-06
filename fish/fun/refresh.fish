165d706f1a485cc277fc225f4c991c9e68dceef6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-06 17:28:15 +0800

    remove non-existing hist from refresh

--- fish/fun/refresh.fish
+++ fish/fun/refresh.fish
@@ -6,6 +6,5 @@ rm ~/.config/fish/functions/*
 /mnt/c/Programming/dotfiles/fish/fun/fffunctions.fish
 /mnt/c/Programming/dotfiles/fish/fun/functions.fish
 /mnt/c/Programming/dotfiles/fish/fun/gittie.fish
-/mnt/c/Programming/dotfiles/fish/fun/hist.fish
 /mnt/c/Programming/dotfiles/fish/fun/prompt.fish
 /mnt/c/Programming/dotfiles/fish/fun/travel.fish
