40d83bc56670738f0e309d27638846641496bc22
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-07 21:28:27 +0800

    bring back bacon

--- fish/setup/rust.fish
+++ fish/setup/rust.fish
@@ -8,7 +8,7 @@ cargo binstall -y cargo-quickinstall
 cargo binstall -y cargo-info
 command -q rg || cargo binstall -y ripgrep
 command -q speedtest-rs || cargo binstall -y speedtest-rs
-# command -q bacon || cargo binstall -y bacon
-# ln -sf /mnt/c/Programming/dotfiles/bacon/prefs.toml ~/.config/bacon/prefs.toml
+command -q bacon || cargo binstall -y bacon
+ln -sf /mnt/c/Programming/dotfiles/bacon/prefs.toml ~/.config/bacon/prefs.toml
 command -q watchexec || cargo binstall -y watchexec-cli
 command -q fd || cargo binstall -y fd-find
