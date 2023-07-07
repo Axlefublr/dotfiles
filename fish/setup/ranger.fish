4684e1e9b4026eaee1b9fdf7a2a3d7e9f2b12ee3
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-07 11:59:47 +0800

    install rust programs

--- fish/setup/ranger.fish
+++ fish/setup/ranger.fish
@@ -3,6 +3,6 @@
 command -q ranger || sudo apt install ranger
 ranger --copy-config all
 cp -f ~/.config/ranger/rc.conf /mnt/c/Programming/dotfiles/rc.conf
-ln -sf /mnt/c/Programming/dotfiles/rc.conf ~/.config/ranger/rc.conf
+ln -sf /mnt/c/Programming/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 cp -f ~/.config/ranger/rifle.conf /mnt/c/Programming/dotfiles/rifle.conf
-ln -sf /mnt/c/Programming/dotfiles/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf /mnt/c/Programming/dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
--- fish/setup/rust.fish
+++ fish/setup/rust.fish
@@ -1,9 +1,13 @@
 #!/usr/bin/env fish
 
 command -q cargo || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-command -q rg || cargo install ripgrep
 rustup update
 rustup component add clippy
-command -q bacon || cargo install bacon
-ln -sf /mnt/c/Programming/dotfiles/rust/prefs.toml ~/.config/bacon/prefs.toml
-cargo install cargo-info
+cargo install binstall
+cargo binstall cargo-quickinstall
+cargo binstall cargo-info
+command -q rg || cargo binstall ripgrep
+command -q speedtest-rs || cargo binstall speedtest-rs
+# command -q bacon || cargo binstall bacon
+# ln -sf /mnt/c/Programming/dotfiles/bacon/prefs.toml ~/.config/bacon/prefs.toml
+command -q watchexec || cargo binstall watchexec-cli
