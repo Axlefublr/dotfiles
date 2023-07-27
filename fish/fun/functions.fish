8930ed1c0bfe26d1ff13fa17e02ada86c91bbd2a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-28 01:01:29 +0800

    make work work

--- fish/fun/functions.fish
+++ fish/fun/functions.fish
@@ -5,35 +5,6 @@ function prli
 end
 funcsave prli > /dev/null
 
-function timer
-	termdown $argv[1] && Ting.exe $argv[2..3]
-	if test $argv[4]
-		exit
-	end
-end
-funcsave timer > /dev/null
-
-function postvideo
-	trash-put /mnt/c/Pictures/Screenvideos/*
-	trash-put /mnt/c/Pictures/*.png
-end
-funcsave postvideo > /dev/null
-
-# made by https://github.com/Micha-ohne-el
-function new --description='Creates new files or directories and all required parent directories'
-	for arg in $argv
-		if string match -rq '/$' -- $arg
-			mkdir -p $arg
-		else
-			set -l dir (string match -rg '(.*)/.+?$' -- $arg)
-			and mkdir -p $dir
-
-			touch $arg
-		end
-	end
-end
-funcsave new > /dev/null
-
 function rga
 	rg --color always $argv &| tee /tmp/pagie &| less
 end
@@ -41,8 +12,8 @@ funcsave rga > /dev/null
 
 function work
 	while true
-		timer 25m 'Work session ended, time to rest!' no-sound || break
-		timer 5m 'I hope you rested well, time to work!' || break
+		termdown 25m -T 'Work!' || break
+		termdown 5m -T 'Rest!' || break
 	end
 	clear -x
 end
@@ -58,34 +29,6 @@ function tg
 	tgpt "$tempText"
 end
 funcsave tg > /dev/null
-
-function finde
-	set -l paths
-	set -l options
-	set -l current_list paths
-
-	for arg in $argv
-		if test $arg = '--'
-			set current_list options
-			continue
-		end
-
-		if test $current_list = 'paths'
-			set paths $paths $arg
-		else
-			set options $options $arg
-		end
-	end
-	find $paths \( -name .git -o -name .npm -o -name .vscode -o -name obj -o -name target \) -prune -o $options -not -name '.' -not -name '..' -print
-end
-funcsave finde > /dev/null
-
-function cado
-	set -l project_name (basename $PWD)
-	set -l full_path $PWD/target/doc/$project_name/index.html
-	$BROWSER (wslpath -w $full_path)
-end
-funcsave cado > /dev/null
 
 function _showie
 	echo "$(cat /tmp/dickie)
