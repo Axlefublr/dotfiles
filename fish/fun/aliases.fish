14c0a7ed94fcf46855762f7dedffa698f67abf74
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-06 00:27:05 +0800

    xclip trims newline

--- fish/fun/aliases.fish
+++ fish/fun/aliases.fish
@@ -8,3 +8,4 @@ alias --save ... 'cd ../..' > /dev/null
 alias --save .... 'cd ../../..' > /dev/null
 alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode &| tee /tmp/pagie &| less' > /dev/null
 alias --save tgg 'tgpt "$argv"' > /dev/null
+alias --save xcp 'xclip -r -selection clipboard' > /dev/null
