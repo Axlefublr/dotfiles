759066a4877afb9ddd776831636a26a4c2c8adc5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-10 21:11:48 +0800

    make rust release function

--- fish/fun/release.fish
+++ fish/fun/release.fish
@@ -18,3 +18,25 @@ function csharp-release
 	gh release create $taggedVersion -n "" ./bin/Release/net7.0/linux-x64/publish/$binaryName ./bin/Release/win-x64/publish/$binaryName.exe
 end
 funcsave csharp-release > /dev/null
+
+function rust-release-publish
+	if not set -q argv[1]
+		echo "you didn't set the version!"
+		return 1
+	end
+	if not command -q gh
+		echo "you don't have gh!"
+		return 1
+	end
+	set -l taggedVersion v$argv[1]
+	git add Cargo.toml
+	git commit -m $taggedVersion
+	git push
+	git tag $taggedVersion
+	git push origin $taggedVersion
+	cargo build --release
+	set -l binaryName (basename $PWD)
+	gh release create $taggedVersion -n "" ./target/release/$binaryName
+	cargo publish
+end
+funcsave rust-release-publish > /dev/null
