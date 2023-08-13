596691564fa481bf74697539aec8edb0dc038a31
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-13 15:52:25 +0800

    xcp alias -> abbreviation

--- fish/fun/aliases.fish
+++ fish/fun/aliases.fish
@@ -8,4 +8,3 @@ alias --save ... 'cd ../..' > /dev/null
 alias --save .... 'cd ../../..' > /dev/null
 alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode' > /dev/null
 alias --save tgg 'tgpt "$argv"' > /dev/null
-alias --save xcp 'xclip -r -selection clipboard' > /dev/null
