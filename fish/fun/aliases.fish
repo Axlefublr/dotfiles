ad92d4a31f1847851f108950cb7a5593b1ce0c5c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-13 17:18:01 +0800

    add bell alias

--- fish/fun/aliases.fish
+++ fish/fun/aliases.fish
@@ -8,3 +8,4 @@ alias --save ... 'cd ../..' > /dev/null
 alias --save .... 'cd ../../..' > /dev/null
 alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode' > /dev/null
 alias --save tgg 'tgpt "$argv"' > /dev/null
+alias --save bell 'printf \a' > /dev/null
