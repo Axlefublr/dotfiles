7a138146de4a333600a60bacbddb41ee6a11b226
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-15 19:30:03 +0800

    nvim: clean up formatting: features

--- neovim/features.lua
+++ neovim/features.lua
@@ -1,32 +1,33 @@
-local function goto_end_of_prev_line() FeedKeysInt(vim.v.count1 .. "k$") end
-Map("", "_", goto_end_of_prev_line)
+Map("n", "gJ", "j0d^kgJ") -- Join current line with the next line with no space in between, *also* discarding any leading whitespace of the next line. Because gJ would include indentation. Stupidly.
 
-local join_lines_no_space = "j0d^kgJ"
-Map("n", "gJ", join_lines_no_space)
+Map("i", "<C-h>", '<C-o>"_S<Esc><C-o>gI<BS>') -- Delete from the current position to the last character on the previous line
+
+Map("n", "<leader>dl", 'dil\'_dd', { remap = true }) -- Take the contents of the line, but delete the line too
+
+Map("n", "<leader>di", '"_ddddpvaB<Esc>') -- Push line of code after block into block
+
+Map("", "<leader>do", 'ddm' .. THROWAWAY_MARK .. 'ggP`' .. THROWAWAY_MARK) -- Move line to the top
+Map("", "<leader>db", 'ddm' .. THROWAWAY_MARK .. 'Gp`' .. THROWAWAY_MARK) -- Bottom
+
+Map("", "_", function() FeedKeysInt(vim.v.count1 .. "k$") end)
+
+local function multiply() FeedKeysInt("yl" .. vim.v.count1 .. "p") end
+Map("n", "<leader>q", multiply)
 
 local function multiply_visual() FeedKeysInt("ygv<Esc>" .. vim.v.count1 .. "p") end
 Map("v", "<leader>q", multiply_visual)
 
-local delete_up_to_last_line_end = '<C-o>"_S<Esc><C-o>gI<BS>'
-Map("i", "<C-h>", delete_up_to_last_line_end)
+Map("n", "J", function()
+	for i = 1, vim.v.count1 do
+		FeedKeys("J")
+	end
+end)
 
 local function remove_highlighting__escape()
 	Remove_highlighting()
 	FeedKeysInt("<Esc>")
 end
 Map("n", "<Esc>", remove_highlighting__escape)
-
-local function multiply() FeedKeysInt("yl" .. vim.v.count1 .. "p") end
-Map("n", "<leader>q", multiply)
-
-local vore_out_line_into_block = '"_ddddpvaB<Esc>'
-Map("n", "<leader>di", vore_out_line_into_block)
-
-local convert_to_arrow_function = 'vaBo<Esc>"_s=> <Esc>Jj"_dd'
-Map("n", "<leader>da", convert_to_arrow_function)
-
-local convert_to_normal_function = '^f(%f="_c3l{<cr><Esc>o}<Esc>'
-Map("n", "<leader>dn", convert_to_normal_function)
 
 local function add_character_at_the_end_of_line()
 	local char = GetChar("Press a character:")
@@ -45,11 +46,3 @@ local function add_character_at_the_start_of_line()
 	FeedKeys("`" .. THROWAWAY_MARK)
 end
 Map("n", "<leader>:", add_character_at_the_start_of_line)
-
-local move_line_to_top = 'ddm' .. THROWAWAY_MARK .. 'ggP`' .. THROWAWAY_MARK
-Map("", "<leader>do", move_line_to_top)
-
-local move_line_to_bottom = 'ddm' .. THROWAWAY_MARK .. 'Gp`' .. THROWAWAY_MARK
-Map("", "<leader>db", move_line_to_bottom)
-
-Map("n", "<leader>dl", 'dil\'_dd', { remap = true })
