6a625936628c580db74c6251ee7399e0e4aa1b98
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-26 20:12:33 +0800

    nvim: search for selection accepts leader to enable /e

--- neovim/big.lua
+++ neovim/big.lua
@@ -1,14 +1,16 @@
 -- I call it death because that's where we end up in. Just like /e or no /e
-function Search_for_selection(death)
+function Search_for_selection(direction, death)
 	FeedKeys('y')
 	vim.schedule(function()
 		local escaped_selection = EscapeForLiteralSearch(vim.fn.getreg('"'))
-		FeedKeys('/\\V' .. escaped_selection .. death)
+		FeedKeys(direction .. '\\V' .. escaped_selection .. death)
 		FeedKeysInt('<cr>')
 	end)
 end
-Map("v", "*", function() Search_for_selection('') end)
-Map("v", "#", function() Search_for_selection('/e') end)
+Map("v", "*", function() Search_for_selection('/', '') end)
+Map("v", "<leader>*", function() Search_for_selection('/', '/e') end)
+Map("v", "#", function() Search_for_selection('?', '') end)
+Map("v", "<leader>#", function() Search_for_selection('?', '/e') end)
 
 function Search_for_register(death)
 	local char = GetChar("Input register key to search for:")
