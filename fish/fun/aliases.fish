f809af260b6ca15a5b7bedb2f4b04afd1cac25a2
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-27 16:52:49 +0800

    fish: abbreviations moved out to its own file; u goes up

--- fish/fun/aliases.fish
+++ fish/fun/aliases.fish
@@ -4,8 +4,6 @@ alias --save fd 'fd --no-require-git' > /dev/null
 alias --save rg 'rg --engine auto' > /dev/null
 alias --save less 'less --use-color -R' > /dev/null
 alias --save termdown 'termdown -W -f roman' > /dev/null
-alias --save ... 'cd ../..' > /dev/null
-alias --save .... 'cd ../../..' > /dev/null
 alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode' > /dev/null
 alias --save tgg 'tgpt "$argv"' > /dev/null
 alias --save bell 'printf \a' > /dev/null
