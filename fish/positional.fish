09b84eef2e26ba62c776d8ef978fec2a459e762e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-28 23:12:04 +0800

    fish: :p -> pwd

--- fish/positional.fish
+++ fish/positional.fish
@@ -1,3 +1,4 @@
 abbr -a :dn --position anywhere -- '> /dev/null'
 abbr -a :edn --position anywhere -- '2> /dev/null'
 abbr -a :bdn --position anywhere -- '&> /dev/null'
+abbr -a :p --position anywhere -- (pwd)/
