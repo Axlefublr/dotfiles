e6ec3f0b44458b5b4ff42c969c8d8796b8ac3ff2
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-14 02:04:16 +0800

    nvim: use qQ for leap

--- neovim/plugins/keymap.lua
+++ neovim/plugins/keymap.lua
@@ -109,4 +109,8 @@ end, { desc = "Delete surrounding indentation" })
 
 Map("", "ga", "<Plug>(EasyAlign)")
 Map("n", "grr", "<Plug>ReplaceWithRegisterLine")
-Map({"v", "n", "o"}, ",q", "<Plug>CamelCaseMotion_ge")
+
+Map({"n", "x", "o"}, "q", "<Plug>(leap-forward-to)")
+Map({"n", "x", "o"}, "Q", "<Plug>(leap-backward-to)")
+Map({"x", "o"}, "x", "<Plug>(leap-forward-till)")
+Map({"x", "o"}, "X", "<Plug>(leap-backward-till)")
