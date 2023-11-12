4cea9ed2792ebbc51674016c1c87a5109faab251
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-11-13 01:17:15 +0800

    hotkeys `#e` `#!e` don't modify your clipboard if the edit is empty

--- hotkey-commands.sh
+++ hotkey-commands.sh
@@ -2,9 +2,9 @@ xclip -selection clipboard -t image/png -o 2>/dev/null | display
 
 uclanr -tj '' 3 | xclip -r -selection clipboard
 
-xclip -selection clipboard -o > /tmp/bibren ; kitty -T temporary-terminal nvim -n /tmp/bibren ; cat /tmp/bibren | xclip -r -selection clipboard
+xclip -selection clipboard -o > /tmp/bibren ; kitty -T temporary-terminal nvim -n /tmp/bibren ; test -s /tmp/bibren && cat /tmp/bibren | xclip -r -selection clipboard
 
-truncate -s 0 /tmp/bibren ; kitty -T temporary-terminal nvim -n /tmp/bibren ; cat /tmp/bibren | xclip -r -selection clipboard
+truncate -s 0 /tmp/bibren ; kitty -T temporary-terminal nvim -n /tmp/bibren ; test -s /tmp/bibren && cat /tmp/bibren | xclip -r -selection clipboard
 
 kitty --hold kitten @set-window-title timer
 
