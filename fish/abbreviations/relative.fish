3f0a24ab712eefef2ea6451f2e53169401716acc
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-02 03:02:53 +0800

    remove .txt after release-notes

--- fish/abbreviations/relative.fish
+++ fish/abbreviations/relative.fish
@@ -13,25 +13,25 @@ abbr --set-cursor -a gammp 'git add . &&
 abbr --set-cursor -a alarmb 'alarm %
 	bell'
 
-abbr --set-cursor -a care 'set -l taggedVersion %
+abbr --set-cursor -a care 'set -l taggedVersion % &&
 	test -e README.md &&
 	test -e release-notes &&
 	git add . &&
 	git commit -m $taggedVersion &&
 	git push &&
-	git tag $taggedVersion -F release-notes.txt &&
+	git tag $taggedVersion -F release-notes &&
 	git push origin $taggedVersion &&
 	cargo publish'
 
-abbr --set-cursor -a dnre 'set -l taggedVersion %
+abbr --set-cursor -a dnre 'set -l taggedVersion % &&
 	test -e README.md &&
-	test -e release-notes.txt &&
+	test -e release-notes &&
 	git add . &&
 	git commit -m $taggedVersion &&
 	git push &&
-	git tag $taggedVersion -F release-notes.txt &&
+	git tag $taggedVersion -F release-notes &&
 	git push origin $taggedVersion &&
 	dotnet publish --configuration Release --runtime linux-x64 --self-contained &&
 	dotnet publish --configuration Release --runtime win-x64 --self-contained &&
 	set -l binaryName (basename $PWD) &&
-	gh release create $taggedVersion -t $taggedVersion -F release-notes.txt ./bin/Release/net7.0/linux-x64/publish/$binaryName ./bin/Release/win-x64/publish/$binaryName.exe'
+	gh release create $taggedVersion -t $taggedVersion -F release-notes ./bin/Release/net7.0/linux-x64/publish/$binaryName ./bin/Release/win-x64/publish/$binaryName.exe'
