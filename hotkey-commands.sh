ac48c8c68587ed6c2e9f01c95f4d9aa330ed9ca6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-03 13:57:00 +0800

    move over kitty starter

--- hotkey-commands.sh
+++ hotkey-commands.sh
@@ -1,12 +1,8 @@
-xclip -selection clipboard -t image/png -o 2>/dev/null | display
-
 uclanr -tj '' 3 | xclip -r -selection clipboard
 
 xclip -selection clipboard -o > /tmp/bibren ; kitty -T edit-clipboard nvim -n /tmp/bibren ; test -s /tmp/bibren && cat /tmp/bibren | xclip -r -selection clipboard
 
 truncate -s 0 /tmp/bibren ; kitty -T edit-clipboard nvim -n /tmp/bibren ; test -s /tmp/bibren && cat /tmp/bibren | xclip -r -selection clipboard
-
-kitty --hold kitten @set-window-title timer
 
 kitty --detach --hold -T infoterm printwait
 
