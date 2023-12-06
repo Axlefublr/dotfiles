5779d609af6a55d56212062e3b5a34d49b102e5c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-06 17:06:35 +0800

    fish: remove C# release abbreviation

--- fish/abbreviations/release.fish
+++ fish/abbreviations/release.fish
@@ -72,17 +72,3 @@ function rust-release
 	cargo publish
 end
 funcsave rust-release > /dev/null
-
-
-abbr --set-cursor -a csharp-release 'set -l taggedVersion % &&
-	test -e README.md &&
-	test -e release-notes &&
-	git add . &&
-	git commit -m $taggedVersion &&
-	git push &&
-	git tag $taggedVersion -F release-notes &&
-	git push origin $taggedVersion &&
-	dotnet publish --configuration Release --runtime linux-x64 --self-contained &&
-	dotnet publish --configuration Release --runtime win-x64 --self-contained &&
-	set -l binaryName (basename $PWD) &&
-	gh release create $taggedVersion -t $taggedVersion -F release-notes ./bin/Release/net7.0/linux-x64/publish/$binaryName ./bin/Release/win-x64/publish/$binaryName.exe'
