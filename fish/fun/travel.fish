41103c0c2f73f20d438739986756edd0dac859f3
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-20 16:31:04 +0800

    fish: remove pick

--- fish/fun/travel.fish
+++ fish/fun/travel.fish
@@ -1,48 +1,5 @@
 #!/usr/bin/env fish
 
-function pick
-	set -l current $argv[1]
-	set -l here $argv[1]
-	while test -d $current
-		set here (ls $current -Ab -w 1 2> /dev/null)
-		if test (echo $here) = ''
-			break
-		end
-		if set -q argv[2] # prune out files if you pass the 2nd argument
-			set -l newHere
-			for obj in $here
-				if not test -f $current/$obj
-					set newHere $newHere $obj
-				end
-			end
-			set here $newHere
-		end
-		if test (echo $here) = ''
-			break
-		end
-		set here (prli $here | fzf -e --cycle --tac)
-		if not test $here
-			break
-		end
-		set current "$current/$here"
-	end
-	if test $current = '.'
-		return 1
-	end
-	set current (string replace -r '^\/\/' '/' $current)
-	printf $current | tee /tmp/pickie
-end
-funcsave pick > /dev/null
-
-function paste_relative_path
-	set -l picked (pick .)
-	if not test $picked
-		return 1
-	end
-	commandline -i $picked
-end
-funcsave paste_relative_path > /dev/null
-
 function tippity_tappity
 	set -l picked (fd -u -tf -td '' (commandline -t | string replace -r '^$' '.' | string replace -r '^~' $HOME) | fzf -e --cycle --tac | string replace $HOME '~')
 	commandline -t $picked
