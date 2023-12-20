eebcd28cba65f2e5152d28ba5766ecdf15729168
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-20 16:31:29 +0800

    fish: remove tippity_tappity function

--- fish/fun/travel.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-function tippity_tappity
-	set -l picked (fd -u -tf -td '' (commandline -t | string replace -r '^$' '.' | string replace -r '^~' $HOME) | fzf -e --cycle --tac | string replace $HOME '~')
-	commandline -t $picked
-	commandline -f repaint
-end
-funcsave tippity_tappity > /dev/null
