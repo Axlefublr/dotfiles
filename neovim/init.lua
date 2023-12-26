54b2d4b99a034b56eb5364f1c7ba10b3967ca929
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-27 00:41:50 +0800

    kitty & nvim: no line numbers for !m nvim

--- neovim/init.lua
+++ neovim/init.lua
@@ -30,4 +30,20 @@ require('plugins/plugins')
 require('plugins/configuration')
 require('plugins/keymap')
 
+-- When I use my hotkeys to open some output I see in my kitty terminal,
+-- I'm already looking at some place I want to (usually) copy.
+-- Once I open it in neovim, I get the extra margin to fit in line numbers.
+-- Having those makes me have to *refind* the place I was just looking at,
+-- and because of that I might've been underusing the feature.
+-- So I remove them, to make it *easier* to find myself in that neovim instance,
+-- ironically, since usually having line numbers is helpful.
+-- To add, a lot of output calculates how much to wrap using $COLUMNS.
+-- If you suddenly occupy 2 (or however many) columns that were used prior,
+-- the wrapping efforts go to shit and the kitty+nvim hotkeys become unreasonable to use,
+-- making me have to resort to worse, built in kitty scrolling
+if os.getenv("KITTY_PIPE_DATA") ~= nil then
+	vim.opt.number = false
+	vim.opt.relativenumber = false
+end
+
 print("nvim loaded")
