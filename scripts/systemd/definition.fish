df2b42ce33f7798bdee2ee827776a5f0c06bd2d9
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-01-18 07:59:06 +0800

    systemd-timers: enable monday and thursday

--- scripts/systemd/definition.fish
+++ scripts/systemd/definition.fish
@@ -5,11 +5,20 @@ ln -sf ~/prog/dotfiles/scripts/systemd/timers/* ~/.config/systemd/user
 
 systemctl --user daemon-reload
 
-systemctl --user enable monthly.timer
-systemctl --user start monthly.timer
-systemctl --user enable saturday.timer
-systemctl --user start saturday.timer
-systemctl --user enable sunday.timer
-systemctl --user start sunday.timer
 systemctl --user enable daily.timer
 systemctl --user start daily.timer
+
+systemctl --user enable monthly.timer
+systemctl --user start monthly.timer
+
+systemctl --user enable monday.timer
+systemctl --user start monday.timer
+
+systemctl --user enable thursday.timer
+systemctl --user start thursday.timer
+
+systemctl --user enable saturday.timer
+systemctl --user start saturday.timer
+
+systemctl --user enable sunday.timer
+systemctl --user start sunday.timer
