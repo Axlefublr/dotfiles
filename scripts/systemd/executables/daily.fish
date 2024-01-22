943ed39182b2a22393ebaba46380348b9dbd0c4c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-01-22 15:55:17 +0800

    systemd-timers: remove job search reminder

--- scripts/systemd/executables/daily.fish
+++ scripts/systemd/executables/daily.fish
@@ -6,10 +6,6 @@ clorange megafon increment
 yeared-parse
 yearless-parse
 
-if test (math (clorange days show) % 2) -eq 0
-	kitty -T task holup 'job search'
-end
-
 if test (math (clorange megafon show) % 30) -eq 0
 	kitty -T task holup 'megafon (504) tomorrow'
 end
