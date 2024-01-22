d40a20ed3aafa307bd4d5ed0266a01dce805a64e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-01-22 17:35:01 +0800

    fish: move window related function to its own file

--- fish/fun/general.fish
+++ fish/fun/general.fish
@@ -59,51 +59,6 @@ function atc
 end
 funcsave atc > /dev/null
 
-function winwaitname
-	set -l counter 0
-	while test (xdotool search --onlyvisible --name $argv[1] | count) -lt 1
-		set counter (math $counter + 1)
-		if set -q argv[2]
-			sleep $argv[2]
-		end
-		if set -q argv[4]
-			if test $counter -ge $argv[4]
-				return 1
-			end
-		end
-	end
-	if set -q argv[3]
-		sleep $argv[3]
-	end
-end
-funcsave winwaitname > /dev/null
-
-function winwaitclass
-	set -l counter 0
-	while test (xdotool search --onlyvisible --class $argv[1] | count) -lt 1
-		set counter (math $counter + 1)
-		if set -q argv[2]
-			sleep $argv[2]
-		end
-		if set -q argv[4]
-			if test $counter -ge $argv[4]
-				return 1
-			end
-		end
-	end
-	if set -q argv[3]
-		sleep $argv[3]
-	end
-end
-funcsave winwaitclass > /dev/null
-
-function winmoveall
-	for win_id in (wmctrl -lx | rg $argv[1] | awk '{print $1}')
-		wmctrl -ir $win_id -t $argv[2]
-	end
-end
-funcsave winmoveall > /dev/null
-
 function uboot
 	paru
 	if test (math (clorange updates show) % 5) -eq 0
@@ -125,18 +80,6 @@ function uboot
 	end
 end
 funcsave uboot > /dev/null
-
-function minigun-save
-	set -l coords (xdotool getmouselocation | string match -gr '\\w:(\\d+) \\w:(\\d+)')
-	printf $coords[1]' '$coords[2] > ~/.local/share/minigun/$argv[1]
-end
-funcsave minigun-save > /dev/null
-
-function minigun-apply
-	set -l coords (cat ~/.local/share/minigun/$argv[1] | string split ' ')
-	xdotool mousemove $coords[1] $coords[2]
-end
-funcsave minigun-apply > /dev/null
 
 function take
 	math "$argv[1] + ($argv[2] / 20) + $argv[2]"
