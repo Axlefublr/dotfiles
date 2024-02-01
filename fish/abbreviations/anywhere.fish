6585d1e1d9ddaf4ebe92834b70925cc54c937ac7
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-02 04:13:53 +0800

    fish: change redirection abbreviations

--- fish/abbreviations/anywhere.fish
+++ fish/abbreviations/anywhere.fish
@@ -2,8 +2,9 @@ abbr -a ... --position anywhere -- '../../'
 abbr -a .... --position anywhere -- '../../../'
 abbr -a ..... --position anywhere -- '../../../../'
 
-abbr -a ,dn --position anywhere -- '> /dev/null'
-abbr -a ,edn --position anywhere -- '2> /dev/null'
-abbr -a ,bdn --position anywhere -- '&> /dev/null'
+abbr -a ,sr --position anywhere -- '> /dev/null'
+abbr -a ,er --position anywhere -- '2> /dev/null'
+abbr -a ,ar --position anywhere -- '&> /dev/null'
 abbr -a ,h --position anywhere -- $HOME/
 abbr -a ,ds --position anywhere -- '&> /dev/null & disown'
+abbr -a ,dsr --position anywhere --set-cursor -- '&> % & disown'
