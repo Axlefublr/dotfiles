bfa917d12fcfc9f7f71c42ec1dd038d7922442f7
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-04 22:46:01 +0800

    nvim: install dial.nvim

--- neovim/plugins/plugins.lua
+++ neovim/plugins/plugins.lua
@@ -11,6 +11,7 @@ require("packer").startup(function(use)
 	use "farmergreg/vim-lastplace"
 	use "bkad/CamelCaseMotion"
 	use "xiyaowong/transparent.nvim"
+	use "monaqa/dial.nvim"
 	use {
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true }
@@ -91,3 +92,85 @@ require('lualine').setup {
 	inactive_winbar = {},
 	extensions = {}
 }
+
+local augend = require("dial.augend")
+require("dial.config").augends:register_group{
+	default = {
+		augend.integer.alias.decimal_int,
+		augend.integer.alias.hex,
+		augend.integer.alias.octal,
+		augend.integer.alias.binary,
+		augend.hexcolor.new {
+			case = "upper"
+		},
+		augend.semver.alias.semver,
+		augend.constant.alias.bool,
+		augend.constant.new{
+			elements = {"and", "or"},
+			word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+			cyclic = true,
+		},
+		augend.constant.new{
+			elements = {"AND", "OR"},
+			word = true,
+			cyclic = true,
+		},
+		augend.constant.new{
+			elements = {"&&", "||"},
+			word = false,
+			cyclic = true,
+		},
+		augend.date.new {
+			pattern = "%y.%m.%d",
+			default_kind = "day",
+			only_valid = true,
+		},
+		augend.date.alias["%H:%M"],
+		augend.date.new {
+			pattern = "%B", -- titlecased month names
+			default_kind = "day",
+		},
+		augend.constant.new {
+			elements = { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" },
+			word = true,
+			cyclic = true
+		},
+		augend.constant.new {
+			elements = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" },
+			word = true,
+			cyclic = true
+		},
+		augend.constant.new {
+			elements = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" },
+			word = true,
+			cyclic = true
+		},
+		augend.constant.alias.alpha,
+		augend.constant.alias.Alpha,
+	},
+}
+
+vim.keymap.set("n", "<C-a>", function()
+	require("dial.map").manipulate("increment", "normal")
+end)
+vim.keymap.set("n", "<C-x>", function()
+	require("dial.map").manipulate("decrement", "normal")
+end)
+vim.keymap.set("n", "g<C-a>", function()
+	require("dial.map").manipulate("increment", "gnormal")
+end)
+vim.keymap.set("n", "g<C-x>", function()
+	require("dial.map").manipulate("decrement", "gnormal")
+end)
+vim.keymap.set("v", "<C-a>", function()
+	require("dial.map").manipulate("increment", "visual")
+end)
+vim.keymap.set("v", "<C-x>", function()
+	require("dial.map").manipulate("decrement", "visual")
+end)
+vim.keymap.set("v", "g<C-a>", function()
+	require("dial.map").manipulate("increment", "gvisual")
+end)
+vim.keymap.set("v", "g<C-x>", function()
+	require("dial.map").manipulate("decrement", "gvisual")
+end)
