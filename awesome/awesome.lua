d4b17cb53af646ec5e8ec4a0fed49e72805869d5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-08 21:50:33 +0800

    awesome: login script called from within config

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -651,3 +651,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awful.spawn.once("/home/axlefublr/prog/dotfiles/scripts/setup/login.fish")
--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -10,38 +10,38 @@ gromit-mpx -k "none" -u "none" &> /tmp/log/gromit-mpx.txt & disown
 kitty --hold kitten @set-window-title timerkitty & disown
 kitty -T meow & disown
 
-winwaitclass code 0.5 0 20
+# winwaitclass code 0.5 0 20
 kitty -d ~/prog/dotfiles & disown
-winwaitname 'dotfiles' 0.5 0 20
-wmctrl -r 'dotfiles' -t 0
-wmctrl -s 0
-wm-rotate
-wm-rotate
-for __ in (seq 1 5)
-	wm-increase-master-window-size
-end
-sleep 1
+# winwaitname 'dotfiles' 0.5 0 20
+# wmctrl -r 'dotfiles' -t 0
+# wmctrl -s 0
+# wm-rotate
+# wm-rotate
+# for __ in (seq 1 5)
+# 	wm-increase-master-window-size
+# end
+# sleep 1
 
-winmoveall code 4
-wmctrl -s 4
-wm-rotate
-wm-rotate
-sleep 1
+# winmoveall code 4
+# wmctrl -s 4
+# wm-rotate
+# wm-rotate
+# sleep 1
 
-winwaitclass vivaldi-stable 0.5 0 20
-winmoveall vivaldi-stable 1
-winwaitname 'Discord' 0.5 0 20
-wmctrl -r 'Discord' -t 8
-winwaitname 'YouTube' 0.5 0 20
-wmctrl -r 'YouTube' -t 10
+# winwaitclass vivaldi-stable 0.5 0 20
+# winmoveall vivaldi-stable 1
+# winwaitname 'Discord' 0.5 0 20
+# wmctrl -r 'Discord' -t 8
+# winwaitname 'YouTube' 0.5 0 20
+# wmctrl -r 'YouTube' -t 10
 
-wmctrl -s 6
-wm-decrease-master-windows
-sleep 1
+# wmctrl -s 6
+# wm-decrease-master-windows
+# sleep 1
 
-wmctrl -s 7
-wm-decrease-master-windows
-sleep 1
+# wmctrl -s 7
+# wm-decrease-master-windows
+# sleep 1
 
 loopuntil is-internet 0.5 0 60
 spotify-launcher -v &> /tmp/log/spotify.txt & disown
