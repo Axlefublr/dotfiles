84e669bcb3e2fbcd33fe0424d7b2d9c0d9388f13
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-08 21:49:45 +0800

    awesome: #[] switches between layouts

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -317,9 +317,10 @@ globalkeys = gears.table.join(
 		{ description = "increase the number of columns", group = "layout" }),
 	awful.key({ modkey, "Control" }, "l", function() awful.tag.incncol(-1, nil, true) end,
 		{ description = "decrease the number of columns", group = "layout" }),
-	awful.key({ modkey, }, "space", function() awful.layout.inc(1) end,
+
+	awful.key({ modkey }, "]", function() awful.layout.inc(1) end,
 		{ description = "select next", group = "layout" }),
-	awful.key({ modkey, "Shift" }, "space", function() awful.layout.inc(-1) end,
+	awful.key({ modkey }, "[", function() awful.layout.inc(-1) end,
 		{ description = "select previous", group = "layout" }),
 
 	awful.key({ modkey, "Control" }, "n",
