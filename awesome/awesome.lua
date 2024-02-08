0348327e3ac2399dab897c0ca95aee807cc28ebf
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-08 21:48:04 +0800

    awesome: rename tags

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -176,8 +176,8 @@ awful.screen.connect_for_each_screen(function(s)
 	-- Wallpaper
 	set_wallpaper(s)
 
-	-- Each screen has its own tag table.
-	local names = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G" }
+	-- Tag definition
+	local names = { "u", "i", "o", "p", "m", ",", ".", "/", "u", "i", "o", "p", "m", ",", ".", "/" }
 	local l = awful.layout.suit
 	local layouts = { l.tile, l.tile, l.tile, l.tile, l.tile, l.tile, l.tile, l.tile, l.tile }
 	awful.tag(names, s, layouts)
