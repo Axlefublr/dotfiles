b5912a319086e094fa0ec6849d5cbfba39fd8812
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-08 21:49:11 +0800

    awesome: #hl change master bigness by a lower amount

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -302,20 +302,17 @@ globalkeys = gears.table.join(
 		{ description = "go back", group = "client" }),
 
 	-- Standard program
-	awful.key({ modkey, }, "Return", function() awful.spawn(terminal) end,
-		{ description = "open a terminal", group = "launcher" }),
-	awful.key({ modkey, "Control" }, "r", awesome.restart,
-		{ description = "reload awesome", group = "awesome" }),
-	awful.key({ modkey, "Shift" }, "q", awesome.quit,
-		{ description = "quit awesome", group = "awesome" }),
-	awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
+
+	awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.02) end,
 		{ description = "increase master width factor", group = "layout" }),
-	awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.05) end,
+	awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.02) end,
 		{ description = "decrease master width factor", group = "layout" }),
+
 	awful.key({ modkey, "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
 		{ description = "increase the number of master clients", group = "layout" }),
 	awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
 		{ description = "decrease the number of master clients", group = "layout" }),
+
 	awful.key({ modkey, "Control" }, "h", function() awful.tag.incncol(1, nil, true) end,
 		{ description = "increase the number of columns", group = "layout" }),
 	awful.key({ modkey, "Control" }, "l", function() awful.tag.incncol(-1, nil, true) end,
