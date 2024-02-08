748e38da56411c01c5a9b225e1a064c75914df7a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-08 19:53:57 +0800

    awesome: prompt on `#;`

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -337,7 +337,7 @@ globalkeys = gears.table.join(
 		{ description = "restore minimized", group = "client" }),
 
 	-- Prompt
-	awful.key({ modkey }, "r", function() awful.screen.focused().mypromptbox:run() end,
+	awful.key({ modkey }, ";", function() awful.screen.focused().mypromptbox:run() end,
 		{ description = "run prompt", group = "launcher" }),
 
 	awful.key({ modkey }, "x",
