9c451080e33d50a84fc413cc396f131bc9463b48
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-10 02:01:22 +0800

    awesome: disable scrolling on desktop switching tags

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -310,9 +310,9 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-	awful.button({}, 3, function() mymainmenu:toggle() end),
-	awful.button({}, 4, awful.tag.viewnext),
-	awful.button({}, 5, awful.tag.viewprev)
+	awful.button({}, 3, function() mymainmenu:toggle() end)
+	-- awful.button({}, 4, awful.tag.viewnext),
+	-- awful.button({}, 5, awful.tag.viewprev)
 ))
 -- }}}
 
