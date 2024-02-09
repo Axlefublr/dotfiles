ed299f33a2526c1eaccdc928db2288ae64f770e6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-09 23:33:16 +0800

    awesome: remove preventing clients from being unreachable on screen cou
    nt changes

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -689,17 +689,6 @@ client.connect_signal("manage", function(c)
 	-- i.e. put it at the end of others instead of setting it master.
 	-- if not awesome.startup then awful.client.setslave(c) end
 
-	if awesome.startup
-		 and not c.size_hints.user_position
-		 and not c.size_hints.program_position then
-		-- Prevent clients from being unreachable after screen count changes.
-		awful.placement.no_offscreen(c)
-	end
-	-- if not awesome.startup then
-	-- 	awful.client.setslave(c)
-	-- 	c.maximized = false
-	-- 	c.floating = false
-	-- end
 end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
