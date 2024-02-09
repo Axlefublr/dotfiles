c572e4e08bb7a8b847d0982d67c98e5531855597
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-10 02:08:11 +0800

    awesome: remove request::titlebars

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -507,31 +507,31 @@ clientkeys = gears.table.join(
 	-- 	{ description = "move to screen", group = "client" }),
 	-- awful.key({ modkey, }, "t", function(c) c.ontop = not c.ontop end,
 	-- 	{ description = "toggle keep on top", group = "client" }),
-	awful.key({ modkey, }, "n",
-		function(c)
-			-- The client currently has the input focus, so it cannot be
-			-- minimized, since minimized clients can't have the focus.
-			c.minimized = true
-		end,
-		{ description = "minimize", group = "client" }),
+	-- awful.key({ modkey, }, "n",
+	-- 	function(c)
+	-- 		-- The client currently has the input focus, so it cannot be
+	-- 		-- minimized, since minimized clients can't have the focus.
+	-- 		c.minimized = true
+	-- 	end,
+	-- 	{ description = "minimize", group = "client" }),
 	-- awful.key({ modkey, }, "m",
 	-- 	function(c)
 	-- 		c.maximized = not c.maximized
 	-- 		c:raise()
 	-- 	end,
 	-- 	{ description = "(un)maximize", group = "client" }),
-	awful.key({ modkey, "Control" }, "m",
-		function(c)
-			c.maximized_vertical = not c.maximized_vertical
-			c:raise()
-		end,
-		{ description = "(un)maximize vertically", group = "client" }),
-	awful.key({ modkey, "Shift" }, "m",
-		function(c)
-			c.maximized_horizontal = not c.maximized_horizontal
-			c:raise()
-		end,
-		{ description = "(un)maximize horizontally", group = "client" })
+	-- awful.key({ modkey, "Control" }, "m",
+	-- 	function(c)
+	-- 		c.maximized_vertical = not c.maximized_vertical
+	-- 		c:raise()
+	-- 	end,
+	-- 	{ description = "(un)maximize vertically", group = "client" }),
+	-- awful.key({ modkey, "Shift" }, "m",
+	-- 	function(c)
+	-- 		c.maximized_horizontal = not c.maximized_horizontal
+	-- 		c:raise()
+	-- 	end,
+	-- 	{ description = "(un)maximize horizontally", group = "client" })
 )
 
 -- Bind all key numbers to tags.
@@ -685,51 +685,6 @@ client.connect_signal("manage", function(c)
 	-- i.e. put it at the end of others instead of setting it master.
 	if not awesome.startup then awful.client.setslave(c) end
 
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-	-- buttons for the titlebar
-	local buttons = gears.table.join(
-		awful.button({}, 1, function()
-			c:emit_signal("request::activate", "titlebar", { raise = true })
-			awful.mouse.client.move(c)
-		end),
-		awful.button({}, 3, function()
-			c:emit_signal("request::activate", "titlebar", { raise = true })
-			awful.mouse.client.resize(c)
-		end)
-	)
-
-	awful.titlebar(c):setup {
-		{ -- Left
-			awful.titlebar.widget.iconwidget(c),
-			buttons = buttons,
-			layout  = wibox.layout.fixed.horizontal
-		},
-		{ -- Middle
-			{ -- Title
-				align  = "center",
-				widget = awful.titlebar.widget.titlewidget(c)
-			},
-			buttons = buttons,
-			layout  = wibox.layout.flex.horizontal
-		},
-		{ -- Right
-			awful.titlebar.widget.floatingbutton(c),
-			awful.titlebar.widget.maximizedbutton(c),
-			awful.titlebar.widget.stickybutton(c),
-			awful.titlebar.widget.ontopbutton(c),
-			awful.titlebar.widget.closebutton(c),
-			layout = wibox.layout.fixed.horizontal()
-		},
-		layout = wibox.layout.align.horizontal
-	}
-end)
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-	c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
