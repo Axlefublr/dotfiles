08c4e46f404fb4ab43264abaff5bc320d4230ff8
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-09 23:33:50 +0800

    awesome: new windows are not master windows

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -687,7 +687,7 @@ awful.rules.rules = {
 client.connect_signal("manage", function(c)
 	-- Set the windows at the slave,
 	-- i.e. put it at the end of others instead of setting it master.
-	-- if not awesome.startup then awful.client.setslave(c) end
+	if not awesome.startup then awful.client.setslave(c) end
 
 end)
 
