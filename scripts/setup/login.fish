3f8b61bdb9b93f147b6684317bd44a052e7bbb58
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-09 23:49:43 +0800

    awesome: moving vivaldis back to login script

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -7,43 +7,20 @@ ydotoold >> /tmp/log/ydotoold.txt & disown
 gromit-mpx -k "none" -u "none" >> /tmp/log/gromit-mpx.txt & disown
 # ollama serve >> /tmp/log/ollama.txt & disown
 
-# vivaldi-stable --force-dark-mode >> /tmp/log/vivaldi.txt & disown
+vivaldi-stable --force-dark-mode >> /tmp/log/vivaldi.txt & disown
 code >> /tmp/log/vscode.txt & disown
-# kitty --hold kitten @set-window-title timerkitty & disown
-# kitty -T meow & disown
 
-# winwaitclass code 0.5 0 20
+winwaitclass code 0.5 0 20
 kitty -d ~/prog/dotfiles & disown
-# winwaitname 'dotfiles' 0.5 0 20
-# wmctrl -r 'dotfiles' -t 0
-# wmctrl -s 0
-# wm-rotate
-# wm-rotate
-# for __ in (seq 1 5)
-# 	wm-increase-master-window-size
-# end
-# sleep 1
-
 # winmoveall code 4
 # wmctrl -s 4
-# wm-rotate
-# wm-rotate
-# sleep 1
 
-# winwaitclass vivaldi-stable 0.5 0 20
-# winmoveall vivaldi-stable 1
-# winwaitname 'Discord' 0.5 0 20
-# wmctrl -r 'Discord' -t 8
-# winwaitname 'YouTube' 0.5 0 20
-# wmctrl -r 'YouTube' -t 10
-
-# wmctrl -s 6
-# wm-decrease-master-windows
-# sleep 1
-
-# wmctrl -s 7
-# wm-decrease-master-windows
-# sleep 1
+winwaitclass vivaldi-stable 0.5 0 20
+winmoveall vivaldi-stable 1
+winwaitname 'Discord' 0.5 0 20
+wmctrl -r 'Discord' -t 8
+winwaitname 'YouTube' 0.5 0 20
+wmctrl -r 'YouTube' -t 10
 
 loopuntil is-internet 0.5 0 60
 spotify-launcher -v >> /tmp/log/spotify.txt & disown
--- scripts/setup/paru.sh
+++ scripts/setup/paru.sh
@@ -2,14 +2,13 @@
 
 paru ttf-twemoji
 paru ascii-image-converter
-paru mapscii
+paru nodejs-mapscii
 paru unimatrix-git
 paru zoom
 paru anki-bin
-paru kwin-bismuth
 paru mullvad-vpn-bin
 paru xremap-x11-bin
 paru gromit-mpx
 paru visual-studio-code-bin
-# paru spicetify-cli
 paru tgpt-bin
+paru nvim-packer-git
