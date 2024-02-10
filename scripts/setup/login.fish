583739a64271dbaa23d4889a118e280125aae173
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-10 19:42:08 +0800

    login: increase sleep before x related commands

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -25,7 +25,7 @@ wmctrl -r 'YouTube' -t 10
 loopuntil is-internet 0.5 0 60
 spotify-launcher -v >> /tmp/log/spotify.txt & disown
 
-sleep 5
+sleep 10
 xset r rate 170 40 >> /tmp/log/xset.txt
 xset s off >> /tmp/log/xset.txt
 xmodmap ~/prog/dotfiles/x11/xmodmap.txt >> /tmp/log/xmodmap.txt
