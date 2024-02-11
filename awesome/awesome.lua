ee000c06dde03499d53c15936552411cfa2ef433
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-12 01:29:39 +0800

    awesome: move out layouts to its own file

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -59,25 +59,7 @@ editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
 
-awful.layout.layouts = {
-	-- awful.layout.suit.floating,
-	awful.layout.suit.tile,
-	awful.layout.suit.tile.left,
-	awful.layout.suit.tile.bottom,
-	awful.layout.suit.tile.top,
-	awful.layout.suit.fair,
-	awful.layout.suit.fair.horizontal,
-	-- awful.layout.suit.spiral,
-	-- awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
-	-- awful.layout.suit.max.fullscreen,
-	-- awful.layout.suit.magnifier,
-	awful.layout.suit.corner.nw,
-	awful.layout.suit.corner.ne,
-	awful.layout.suit.corner.sw,
-	awful.layout.suit.corner.se,
-}
-
+require('layouts')
 require('screen')
 require('keys')
 require('rules')
