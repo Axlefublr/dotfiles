5ce228522aab3e249b7bb71f0f394939a55e584b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-12 02:09:15 +0800

    awesome: make notification font size bigger

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -54,6 +54,9 @@ end
 beautiful.init("/home/axlefublr/.config/awesome/theme.lua")
 beautiful.gap_single_client = false
 
+naughty.config.defaults.font = "Ubuntu NF 16"
+naughty.config.defaults.bg = beautiful.darkerest
+
 terminal = "kitty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
