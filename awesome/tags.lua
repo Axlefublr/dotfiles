a8f0af515b6ab4592f71b6e8b7aa3c38d1865cf5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-12 02:10:12 +0800

    awesome: move out tag definition to a module; anki is fair

---
+++ awesome/tags.lua
@@ -0,0 +1,81 @@
+awful.tag.add("u", {
+	layout              = awful.layout.suit.tile.left,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.74,
+	selected            = true,
+})
+awful.tag.add("i", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("o", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("p", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("m", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add(",", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add(".", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("/", {
+	layout              = awful.layout.suit.fair,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("U", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("I", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("O", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("P", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("M", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("<", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add(">", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
+awful.tag.add("?", {
+	layout              = awful.layout.suit.tile,
+	master_fill_policy  = "expand",
+	master_width_factor = 0.5,
+})
