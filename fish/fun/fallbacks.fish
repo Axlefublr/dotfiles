7ad1b045032ab58182eb46adfbaf405ae1aeeb12
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-17 10:46:29 +0800

    xremap: fix quiet prompt

--- fish/fun/fallbacks.fish
+++ fish/fun/fallbacks.fish
@@ -8,3 +8,4 @@ alias --save ssls 'sudo systemctl status' > /dev/null
 alias --save bl 'bluetoothctl' > /dev/null
 alias --save sv 'set_volume' > /dev/null
 alias --save smv 'set_mic_volume' > /dev/null
+alias --save pik 'pikaur' > /dev/null
--- scripts/setup/removed-system-packages.sh
+++ scripts/setup/removed-system-packages.sh
@@ -4,9 +4,6 @@ sudo pacman -Rns --noconfirm eos-update-notifier
 sudo pacman -Rns --noconfirm kate
 sudo pacman -Rns --noconfirm konsole
 sudo pacman -Rns --noconfirm nano-syntax-highlighting
-sudo pacman -Rns --noconfirm nano
-sudo pacman -Rns --noconfirm vim
 sudo pacman -Rns --noconfirm htop
-sudo pacman -Rns --noconfirm amdvlk
 sudo pacman -Rns --noconfirm meld
 sudo pacman -Rns --noconfirm yay
