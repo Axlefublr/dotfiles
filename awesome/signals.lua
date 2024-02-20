bf674152a8d7d1bddf05456c45548a2ae21015f9
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-02-20 13:30:05 +0800

    awesome: borders are updated on non-visible tags too

--- awesome/signals.lua
+++ awesome/signals.lua
@@ -1,31 +1,39 @@
-function Adjust_borders(client)
-	local tag = screen.primary.selected_tag
-	if not tag then
-		return
+local function clients_tag(client)
+	local client_on_selected = client:isvisible()
+	if client_on_selected then
+		return screen.primary.selected_tag
+	else
+		local fallback = client.first_tag or screen.primary.selected_tag or screen.primary.tags[1]
+		return fallback
 	end
-	local clients = tag:clients()
-	local non_weirds = 0
+end
+
+local function is_only_unstated_client(client, clients)
+	local only = true
 	for _, other_client in ipairs(clients) do
-		if other_client ~= client and not (other_client.maximized or other_client.floating) then
-			non_weirds = non_weirds + 1
+		local stated = other_client.maximized or other_client.floating
+		if other_client ~= client and not stated then
+			only = false
 		end
 	end
+	return only
+end
+
+function Adjust_borders(client, tag)
+	local tag = tag or clients_tag(client)
 	if client.maximized
-		or tag.layout.name == "max"
-		or non_weirds == 0 then
+		or tag.layout.name == 'max'
+		or is_only_unstated_client(client, tag:clients()) then
 		client.border_width = 0
 	else
 		client.border_width = beautiful.border_width
 	end
 end
 
-function Adjust_all_borders(tag)
-	local tag = tag or screen.primary.selected_tag
-	if not tag then
-		return
-	end
+function Adjust_all_borders(tag, client)
+	local tag = tag or clients_tag(client)
 	for _, client in ipairs(tag:clients()) do
-		Adjust_borders(client)
+		Adjust_borders(client, tag)
 	end
 end
 
@@ -72,23 +80,24 @@ client.connect_signal("manage", function(client)
 	if client.class == "Anki" and string.match(client.name, "^Browse") then
 		Move_window_to_tag(16)
 	end
-	Adjust_all_borders()
+	Adjust_all_borders(nil, client)
 	Widget_update_clients(screen.primary.selected_tag)
 end)
 
 client.connect_signal("unmanage", function(client)
+	Adjust_all_borders(nil, client)
 	Widget_update_clients(screen.primary.selected_tag)
 end)
 
 client.connect_signal("property::maximized", function(client)
 	Widget_update_clients(screen.primary.selected_tag)
 	Widget_update_maximized(client)
-	Adjust_all_borders()
+	Adjust_all_borders(nil, client) -- all because with two clients, the other, not maximized client, will effectively get maximized
 end)
 
 client.connect_signal("property::floating", function(client)
 	Widget_update_floating(client)
-	Adjust_all_borders()
+	Adjust_all_borders(nil, client) -- all because with two clients, the other, not floating client, will effectively get maximized. if the borders on the floating client are not focused, that means that the other client is focused, so we don't need the borders
 end)
 
 client.connect_signal("property::ontop", function(client)
@@ -100,7 +109,7 @@ client.connect_signal("property::sticky", function(client)
 end)
 
 awful.tag.attached_connect_signal(screen.primary, "property::layout", function (tag)
-	Adjust_all_borders(tag)
+	Adjust_all_borders(tag) -- because of the 'max' layout
 	Widget_update_malumn(tag)
 	Widget_update_clients(tag)
 end)
@@ -125,7 +134,8 @@ client.connect_signal("focus", function(client)
 	Widget_update_sticky(client)
 	Widget_update_floating(client)
 
-	Adjust_borders(client)
+	-- this shouldn't be needed, but kept as a safety measure for when I'm bad at logic in the other signals updating all borders
+	-- Adjust_borders(client)
 	client.border_color = beautiful.border_focus
 end)
 
