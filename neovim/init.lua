891e620715f742e5db6dfeafd791e8f9fad788e7
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-03 05:14:51 +0800

    rofi: move up to match the new bar width

--- neovim/init.lua
+++ neovim/init.lua
@@ -72,6 +72,11 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
 	command = 'setfiletype xcompose',
 })
 
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+	pattern = { '*.rasi' },
+	command = 'setfiletype rasi',
+})
+
 -- vim.api.nvim_create_autocmd('BufLeave', {
 -- 	callback = Save,
 -- })
