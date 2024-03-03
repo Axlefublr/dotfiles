74f4588854f390baf9b40c6c62c34aa7a2ec4e76
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-03 19:35:09 +0800

    picom: remove transparency to fix gromit-mpx

--- neovim/init.lua
+++ neovim/init.lua
@@ -17,7 +17,7 @@ THROWAWAY_MARK = 'I'
 if vim.g.neovide then
 	vim.o.guifont = 'JetBrainsMono Nerd Font:h17'
 	vim.g.neovide_scroll_animation_length = 0.15
-	vim.g.neovide_scroll_animation_far_lines = 3
+	vim.g.neovide_scroll_animation_far_lines = 5
 	vim.g.neovide_hide_mouse_when_typing = true
 	vim.g.neovide_cursor_animation_length = 0.07
 	vim.g.neovide_cursor_trail_size = 0.4
