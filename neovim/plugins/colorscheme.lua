4192a321d4675bc9bd970b2e9699ec0744bef37d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-08 06:19:28 +0800

    nvim: make nicer syntax for defining highlight changes

--- neovim/plugins/colorscheme.lua
+++ neovim/plugins/colorscheme.lua
@@ -1,39 +1,53 @@
 local highlight_modifications = function()
-	vim.api.nvim_set_hl(0, '@comment', { fg = Colors.grey })
-	vim.api.nvim_set_hl(0, '@string', { fg = Colors.yellow })
-	vim.api.nvim_set_hl(0, '@string.escape', { fg = Colors.grey })
-	vim.api.nvim_set_hl(0, '@punctuation.delimiter', { fg = Colors.white } )
 
-	vim.api.nvim_set_hl(0, '@operator.lua', { fg = Colors.white })
-	vim.api.nvim_set_hl(0, '@number.lua', { fg = Colors.orange })
-	vim.api.nvim_set_hl(0, '@boolean.lua', { fg = Colors.cyan, italic = true })
-	vim.api.nvim_set_hl(0, '@lsp.type.property.lua', { fg = Colors.white })
-	vim.api.nvim_set_hl(0, '@lsp.type.comment.lua', { fg = Colors.grey })
-	vim.api.nvim_set_hl(0, '@variable.member.lua', { fg = Colors.white })
+	local recolors = {
+		['@string'] = 'yellow',
+		['@comment'] = 'grey',
+		['@punctuation.delimiter'] = 'white',
 
-	vim.api.nvim_set_hl(0, '@comment.yaml', { fg = Colors.grey })
-	vim.api.nvim_set_hl(0, '@property.yaml', { fg = Colors.mint })
-	vim.api.nvim_set_hl(0, '@boolean.yaml', { fg = Colors.cyan, italic = true })
+		['@operator.lua'] = 'white',
+		['@number.lua'] = 'orange',
+		['@boolean.lua'] = { fg = Colors.cyan, italic = true },
+		['@lsp.type.property.lua'] = 'white',
+		['@lsp.type.comment.lua'] = 'grey',
+		['@variable.member.lua'] = 'white',
+		['@string.regexp.lua'] = { fg = Colors.yellow, bold = true },
 
-	vim.api.nvim_set_hl(0, '@number.fish', { fg = Colors.orange } )
-	vim.api.nvim_set_hl(0, '@boolean.fish', { fg = Colors.cyan, italic = true } )
-	vim.api.nvim_set_hl(0, '@constant.fish', { fg = Colors.purple } )
-	vim.api.nvim_set_hl(0, '@variable.fish', { fg = Colors.purple } )
+		['@comment.yaml'] = 'grey',
+		['@property.yaml'] = 'mint',
+		['@boolean.yaml'] = { fg = Colors.cyan, italic = true },
 
-	vim.api.nvim_set_hl(0, '@type.css', { fg = Colors.orange } ) -- .class
-	vim.api.nvim_set_hl(0, '@number.css', { fg = Colors.orange } )
-	vim.api.nvim_set_hl(0, '@property.css', { fg = Colors.mint } )
-	vim.api.nvim_set_hl(0, '@operator.css', { fg = Colors.white } )
-	vim.api.nvim_set_hl(0, '@type.qualifier.css', { fg = Colors.red } )
-	vim.api.nvim_set_hl(0, '@tag.attribute.css', { fg = Colors.mint } )
-	vim.api.nvim_set_hl(0, '@tag.css', { fg = Colors.purple } )
-	vim.api.nvim_set_hl(0, '@variable.css', { fg = Colors.white, italic = true } )
-	vim.api.nvim_set_hl(0, '@constant.css', { fg = Colors.cyan } ) -- #id
-	vim.api.nvim_set_hl(0, '@attribute.css', { fg = Colors.cyan } ) -- [something="something"]
-	vim.api.nvim_set_hl(0, '@number.float.css', { fg = Colors.orange } )
+		['@number.fish'] = 'orange',
+		['@boolean.fish'] = { fg = Colors.cyan, italic = true },
+		['@constant.fish'] = 'purple',
+		['@variable.fish'] = 'purple',
 
-	vim.api.nvim_set_hl(0, 'confComment', { fg = Colors.grey })
-	vim.api.nvim_set_hl(0, 'confString', { fg = Colors.yellow })
+		['@type.css'] = 'orange',
+		['@number.css'] = 'orange',
+		['@property.css'] = 'mint',
+		['@operator.css'] = 'white',
+		['@type.qualifier.css'] = 'red',
+		['@tag.attribute.css'] = 'mint',
+		['@tag.css'] = 'purple',
+		['@variable.css'] = { fg = Colors.white, italic = true },
+		['@constant.css'] = 'cyan',
+		['@attribute.css'] = 'cyan', -- [something="something"]
+		['@number.float.css'] = 'orange',
+
+		['confComment'] = 'grey',
+		['confString'] = 'yellow',
+	}
+
+	for highlight, color in pairs(recolors) do
+		local color_table
+		if type(color) == 'string' then
+			color_table = { fg = Colors[color] }
+		else
+			color_table = color
+		end
+		vim.api.nvim_set_hl(0, highlight, color_table)
+	end
+
 end
 
 return {
