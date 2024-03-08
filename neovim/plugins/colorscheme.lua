dcea7601ee53b1c3cde1178477b4ae21ad152c42
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-09 02:43:38 +0800

    nvim: set priority for colorscheme

--- neovim/plugins/colorscheme.lua
+++ neovim/plugins/colorscheme.lua
@@ -65,6 +65,8 @@ end
 return {
 	{
 		'sainnhe/gruvbox-material',
+		lazy = false,
+		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme('gruvbox-material')
 			highlight_modifications()
