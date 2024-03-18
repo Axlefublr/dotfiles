4d10d007f83bbe6a3ee242dfd013ab70e80c35e2
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-18 21:10:51 +0800

    nvim: `,dq` shows $HOME as `~`

--- neovim/plugins/neotree.lua
+++ neovim/plugins/neotree.lua
@@ -224,7 +224,7 @@ return {
 					group_empty_dirs = true,      -- when true, empty folders will be grouped together
 					search_limit = 50,            -- max number of search results when using filters
 					follow_current_file = {
-						enabled = false,           -- This will find and focus the file in the active buffer every time
+						enabled = true,           -- This will find and focus the file in the active buffer every time
 						--               -- the current file is changed while the tree is open.
 						leave_dirs_open = false,   -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 					},
