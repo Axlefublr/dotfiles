90ea9b8e634d7237fb110d8ec7a4bcbfef34847c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-21 04:07:13 +0800

    smd: daily parse shows events

--- fish/fun/timers.fish
+++ fish/fun/timers.fish
@@ -1,96 +1,110 @@
 #!/usr/bin/env fish
 
 function timer
-	while true
-		termdown $argv || return 1
-		read -ln 1 response
-		if not test $response
-			return 1
-		end
-		if test $response = 'e'
-			exit
-		else if test $response = ' '
-			clear -x
-			continue
-		else
-			break
-		end
-	end
+    while true
+        termdown $argv || return 1
+        read -ln 1 response
+        if not test $response
+            return 1
+        end
+        if test $response = e
+            exit
+        else if test $response = ' '
+            clear -x
+            continue
+        else
+            break
+        end
+    end
 end
-funcsave timer > /dev/null
+funcsave timer >/dev/null
 
 function alarm
-	set -l input $argv[1]
-	set -l first (string sub -l 1 $input)
-	if test $first != 0 && test $first != 1 && test $first != 2
-		set input '0'$input
-	end
-	set -l input (string pad -r -c 0 -w 6 $input)
-	while test $input -gt (date +%H%M%S)
-		sleep 0.1
-	end
-	sleep 0.5
-	bell
+    set -l input $argv[1]
+    set -l first (string sub -l 1 $input)
+    if test $first != 0 && test $first != 1 && test $first != 2
+        set input '0'$input
+    end
+    set -l input (string pad -r -c 0 -w 6 $input)
+    while test $input -gt (date +%H%M%S)
+        sleep 0.1
+    end
+    sleep 0.5
+    bell
 end
-funcsave alarm > /dev/null
+funcsave alarm >/dev/null
 
 function doti
-	while true
-		kitten @set-window-title action
-		if set -q argv[1]
-			termdown $argv[1] || break
-		else
-			termdown 25m || break
-		end
-		echo "You just acted! Rest now?"
-		read -ln 1 should_continue
-		if not test $should_continue
-			break
-		end
-		clear
+    while true
+        kitten @set-window-title action
+        if set -q argv[1]
+            termdown $argv[1] || break
+        else
+            termdown 25m || break
+        end
+        echo "You just acted! Rest now?"
+        read -ln 1 should_continue
+        if not test $should_continue
+            break
+        end
+        clear
 
-		kitten @set-window-title rest
-		if set -q argv[2]
-			termdown $argv[2] || break
-		else
-			termdown 5m || break
-		end
-		echo "You just rested! Act now?"
-		read -ln 1 should_continue
-		if not test $should_continue
-			break
-		end
-		clear
-	end
-	clear
-	kitten @set-window-title
+        kitten @set-window-title rest
+        if set -q argv[2]
+            termdown $argv[2] || break
+        else
+            termdown 5m || break
+        end
+        echo "You just rested! Act now?"
+        read -ln 1 should_continue
+        if not test $should_continue
+            break
+        end
+        clear
+    end
+    clear
+    kitten @set-window-title
 end
-funcsave doti > /dev/null
+funcsave doti >/dev/null
 
 function yeared_parse
-	for line in (cat ~/prog/noties/anniversaries.txt | string split '\n')
-		set -l match (string match -gr '(\\d+).(\\d+.\\d+) — (.*)' $line)
-		set -l year $match[1]
-		set -l date $match[2]
-		set -l description $match[3]
-		if not test $date = (date +%m.%d)
-			continue
-		end
-		set year (math (date +%y) - $year)
-		kitty -T task holup "$year years ago: $description" &
-	end
+    for line in (cat ~/prog/noties/anniversaries.txt | string split '\n')
+        set -l match (string match -gr '(\\d+).(\\d+.\\d+) — (.*)' $line)
+        set -l year $match[1]
+        set -l date $match[2]
+        set -l description $match[3]
+        if not test $date = (date +%m.%d)
+            continue
+        end
+        set year (math (date +%y) - $year)
+        alacritty -T task -e holup "$year years ago: $description" &
+    end
 end
-funcsave yeared_parse > /dev/null
+funcsave yeared_parse >/dev/null
 
 function yearless_parse
-	for line in (cat ~/prog/noties/yearly.txt | string split '\n')
-		set -l match (string match -gr '(\\d+.\\d+) — (.*)' $line)
-		set -l date $match[1]
-		set -l description $match[2]
-		if not test $date = (date +%m.%d)
-			continue
-		end
-		kitty -T task holup "$description" &
-	end
+    for line in (cat ~/prog/noties/yearly.txt | string split '\n')
+        set -l match (string match -gr '(\\d+.\\d+) — (.*)' $line)
+        set -l date $match[1]
+        set -l description $match[2]
+        if not test $date = (date +%m.%d)
+            continue
+        end
+        alacritty -T task -e holup "$description" &
+    end
 end
-funcsave yearless_parse > /dev/null
+funcsave yearless_parse >/dev/null
+
+function daily_parse
+    for line in (cat ~/prog/noties/events.txt | string split '\n')
+        set -l match (string match -gr '(\\d+.\\d+.\\d+) (\\d+:\\d+) — (.*)' $line)
+        set -l date $match[1]
+        set -l time $match[2]
+        set -l description $match[3]
+        if not test $date = (date '+%y.%m.%d')
+            continue
+        end
+        alacritty -T task -e holup "today at $time — $description" &
+    end
+end
+funcsave daily_parse >/dev/null
--- scripts/systemd/executables/daily.fish
+++ scripts/systemd/executables/daily.fish
@@ -7,6 +7,7 @@ trash-empty -f 7
 
 yeared_parse
 yearless_parse
+daily_parse
 
 if test (math (clorange megafon show) % 30) -eq 0
     alacritty -T task -e holup 'megafon (504) tomorrow' &
