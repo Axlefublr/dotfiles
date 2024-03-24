f855a81c0df8a6f09876c9e5c337fb1e58a24dad
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-03-24 18:23:48 +0800

    xremap: `c-2` closes notifications

--- scripts/widget_update/media_title.fish
+++ scripts/widget_update/media_title.fish
@@ -2,9 +2,9 @@
 
 while true
     set output "$(get_media_title)"
-    set previous "$(cat ~/.local/share/magazine/s || echo 'ඞ')"
+    set previous "$(cat ~/.local/share/magazine/t || echo 'ඞ')"
     if test $output != $previous
-        printf $output >~/.local/share/magazine/s
+        printf $output >~/.local/share/magazine/t
     end
     set output "$(playerctl metadata --format '{{album}}')"
     set previous "$(cat ~/.local/share/magazine/b || echo 'ඞ')"
