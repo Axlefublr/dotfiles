e8f063ee1e102c47ac9e7c5e3516562b517fc16b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-04-02 23:37:33 +0800

    nvim: no numbers in manpages again

--- neovim/init.lua
+++ neovim/init.lua
@@ -109,7 +109,6 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'man',
 	callback = function()
-		vim.opt_local.relativenumber = true
 		vim.keymap.set({ 'n', 'x', 'o' }, 'q', '<Plug>(leap-forward-to)', { buffer = true })
 		vim.keymap.set({ 'n', 'x', 'o' }, 'Q', '<Plug>(leap-backward-to)', { buffer = true })
 		vim.keymap.set({ 'n', 'x', 'o' }, ',q', '<Plug>(leap-forward-till)', { buffer = true })
