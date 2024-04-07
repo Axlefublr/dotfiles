e8e221d2f775016018b24411cdf9c7dd99260a3f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-04-08 02:17:49 +0800

    sdt: loago gets updated minutely
    Because `loago do` already updates it ahead of time, so high precision
    is not necessary for polling

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
+widget_update mature_tasks_line Loago
 awesome-client 'Hunger_wu()'
--- scripts/widget_update/loago.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-while true
-    widget_update mature_tasks_line Loago
-    sleep 10
-end
