6006a45550ca07c987c45578ba9e45053cd52e4a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-05-26 20:27:01 +0800

    fish: `runner_notification` creates notification with rofied text

--- fish/fun/runners.fish
+++ fish/fun/runners.fish
@@ -279,3 +279,13 @@ function runner_project
     end >/dev/shm/project.txt
 end
 funcsave runner_project >/dev/null
+
+function runner_notification
+    set result (rofi -dmenu 2>/dev/null ; echo $status)
+    if test $result[-1] -ne 0
+        return 1
+    end
+    set -e result[-1]
+    notify-send -t 0 "$result"
+end
+funcsave runner_notification >/dev/null
