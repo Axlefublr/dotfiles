a84c0e6c8a6ebe1fc93f44001f0b2faae683eb4d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-06-04 18:13:45 +0800

    smd: add automatic syncing for magazine, harp, loago

--- scripts/systemd/executables/daily.fish
+++ scripts/systemd/executables/daily.fish
@@ -4,6 +4,29 @@ clorange megafon increment
 
 trash-empty -f 3
 
+for file in ~/.local/share/magazine/*
+    if not test -s $file
+        rm -fr $file
+    end
+end
+cp -fr ~/.local/share/magazine ~/prog/backup
+cd ~/prog/backup
+set -l changed_magazines (git status -s | rg magazine | wl -l)
+if test $changed_magazines -gt 0
+    git add magazine/
+    git commit -m "sync magazine $(date '+%y.%m.%d'): $changed_magazines changes"
+end
+cp -fr ~/.local/share/harp/harp.yml ~/prog/backup/automatic/harp.yml
+if git status -s | rg harp.yml
+    git add automatic/harp.yml
+    git commit -m "sync harp $(date '+%y.%m.%d')"
+end
+cp -fr ~/.local/share/loago/loago.json ~/prog/backup/automatic/loago.json
+if git status -s | rg loago.json
+    git add automatic/loago.json
+    git commit -m "sync loago $(date '+%y.%m.%d')"
+end
+
 yeared_parse
 yearless_parse
 daily_parse
@@ -28,3 +51,5 @@ end
 if test (date '+%A') = Sunday
     task ask
 end
+
+gpp
