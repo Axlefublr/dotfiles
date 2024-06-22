99ae2949d79a6222fb0f2eff9d3d74792e93abd6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-06-22 23:20:07 +0800

    smd: sync axleizer

--- scripts/systemd/executables/daily.fish
+++ scripts/systemd/executables/daily.fish
@@ -11,20 +11,29 @@ for file in ~/.local/share/magazine/*
 end
 cp -fr ~/.local/share/magazine ~/prog/backup
 cd ~/prog/backup
+
 set -l changed_magazines (git status -s | rg magazine | wc -l)
 if test "$changed_magazines" -gt 0
     git add magazine/
     git commit -m "sync magazine $(date '+%y.%m.%d'): $changed_magazines changes"
 end
+
 cp -fr ~/.local/share/harp/harp.yml ~/prog/backup/automatic/harp.yml
 if git status -s | rg harp.yml
     git add automatic/harp.yml
     git commit -m "sync harp $(date '+%y.%m.%d')"
 end
+
 cp -fr ~/.local/share/loago/loago.json ~/prog/backup/automatic/loago.json
 if git status -s | rg loago.json
     git add automatic/loago.json
     git commit -m "sync loago $(date '+%y.%m.%d')"
+end
+
+cp -fr ~/.local/share/axleizer.json ~/prog/backup/automatic/axleizer.json
+if git status -s | rg axleizer.json
+    git add automatic/axleizer.json
+    git commit -m "sync axleizer $(date '+%y.%m.%d')"
 end
 
 yeared_parse
