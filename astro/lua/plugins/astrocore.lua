43dfad049e8057920a7186613316cca21f26d79e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-06-25 15:17:02 +0800

    astro: move toggle mappings to `,c`

--- astro/lua/plugins/astrocore.lua
+++ astro/lua/plugins/astrocore.lua
@@ -323,20 +323,6 @@ local normal_mappings = {
 		local repo_root = get_repo_root()
 		if repo_root then vim.cmd.tcd(repo_root) end
 	end,
-	gL = {
-		function()
-			---@diagnostic disable-next-line: undefined-field
-			if vim.diagnostic.config().virtual_text then
-				vim.diagnostic.config({ virtual_lines = false })
-				vim.diagnostic.config({ virtual_text = false })
-				vim.diagnostic.config({ underline = false })
-			else
-				vim.diagnostic.config({ virtual_lines = true })
-				vim.diagnostic.config({ virtual_text = true })
-				vim.diagnostic.config({ underline = true })
-			end
-		end,
-	},
 	['<Leader>P'] = '<Cmd>pu!<CR>',
 	['<Leader>di'] = '"_ddddpvaB<Esc>>iB', -- Push line of code after block into block
 	['<Leader>dl'] = { "dil'dd", remap = true },
@@ -347,7 +333,23 @@ local normal_mappings = {
 	['z?'] = '<CMD>execute "normal! " . rand() % line(\'$\') . "G"<CR>',
 	du = function() count_repeats_keys('dd') end,
 	gJ = '0d^kgJ', -- Join current line with the next line with no space in between, *also* discarding any leading whitespace of the next line. Because gJ would include indentation. Stupidly.
-	gy = '<Cmd>set list!<CR>',
+
+	-- Toggles
+	['<Leader>ce'] = function()
+		---@diagnostic disable-next-line: undefined-field
+		if vim.diagnostic.config().virtual_text then
+			vim.diagnostic.config({ virtual_lines = false })
+			vim.diagnostic.config({ virtual_text = false })
+			vim.diagnostic.config({ underline = false })
+		else
+			vim.diagnostic.config({ virtual_lines = true })
+			vim.diagnostic.config({ virtual_text = true })
+			vim.diagnostic.config({ underline = true })
+		end
+	end,
+	['<Leader>cl'] = '<Cmd>set list!<CR>',
+	['<Leader>co'] = function() require('astrolsp.toggles').buffer_inlay_hints() end,
+	['<Leader>cO'] = function() require('astrolsp.toggles').inlay_hints() end,
 
 	-- Lsp
 	['<Leader>lr'] = vim.lsp.buf.rename,
@@ -384,7 +386,6 @@ local normal_mappings = {
 		vim.b.ufo_foldlevel = math.min(99, (vim.b.ufo_foldlevel + vim.v.count1))
 		require('ufo').closeFoldsWith(vim.b.ufo_foldlevel)
 	end,
-	['go'] = function() require('astrolsp.toggles').buffer_inlay_hints() end,
 
 	-- Gaf
 	['<Leader>cam'] = function()
