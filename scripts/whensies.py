0de8fff8ae9d49666be72154b6bfccb2dcbb8f5b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-06-27 19:20:14 +0800

    sdt: tomorrow parser accepts more date formats

--- scripts/whensies.py
+++ scripts/whensies.py
@@ -7,7 +7,21 @@ file_path = "/home/axlefublr/.local/share/magazine/T"
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 
-date_format = "%y.%m.%d"
+def triple_parse(date):
+    try:
+        parsed_date = datetime.datetime.strptime(date, "%y.%m.%d").date()
+    except ValueError:
+        today = datetime.datetime.today()
+        this_year = today.year
+        this_month = today.month
+        try:
+            parsed_date = datetime.datetime.strptime(f"{this_year}.{date}", "%Y.%m.%d").date()
+        except ValueError:
+            try:
+                parsed_date = datetime.datetime.strptime(f"{this_year}.{this_month}.{date}", "%Y.%m.%d").date()
+            except ValueError:
+                return None
+    return parsed_date
 
 lines_to_keep = []
 
@@ -15,20 +29,23 @@ with open(file_path, "r") as file:
     lines = file.readlines()
 
 for line in lines:
-    try:
-        event_date_str = line.split(" ")[0]
-        event_date = datetime.datetime.strptime(event_date_str, date_format).date()
-        event_name = line[9:].rstrip()
+    elements = line.split(" ")
+    maybe_date = elements[0]
+    rest = elements[1:]
+    event_name = " ".join(rest)
 
-        if event_date == today:
-            print(f"today at {event_name}")
-        elif event_date == tomorrow:
-            print(f"tomorrow at {event_name}")
-            lines_to_keep.append(line)
-        elif event_date > tomorrow:
-            lines_to_keep.append(line)
-    except ValueError:
+    event_date = triple_parse(maybe_date)
+    if event_date is None:
+        lines_to_keep.append(line)
         continue
+
+    if event_date == today:
+        print(f"today {event_name.rstrip()}")
+    elif event_date == tomorrow:
+        print(f"tomorrow {event_name.rstrip()}")
+        lines_to_keep.append(line)
+    elif event_date > tomorrow:
+        lines_to_keep.append(line)
 
 with open(file_path, "w") as file:
     file.writelines(lines_to_keep)
