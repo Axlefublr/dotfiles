57863eb7b74b56baea62e315b4bcbc0f5c4487ae
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-07-01 16:25:28 +0800

    sdt: get github notifications every 10 minutes

--- scripts/systemd/executables/ten-minutes.fish
+++ scripts/systemd/executables/ten-minutes.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
 pjs
+set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
+if test "$notifications"
+    task -u $notifications
+end
