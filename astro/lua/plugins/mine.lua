4158d756ce45de8068a92e10bcbd956474b432d1
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-07-24 15:16:46 +0800

    astro: add border to edister

--- astro/lua/plugins/mine.lua
+++ astro/lua/plugins/mine.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
-	{ 'Axlefublr/edister.nvim', lazy = true, dev = true },
+	{ 'Axlefublr/edister.nvim', lazy = true, dev = true, opts = { border = env.borders } },
 	{
 		'Axlefublr/harp-nvim',
 		lazy = true,
@@ -10,24 +10,24 @@ return {
 	{
 		'Axlefublr/qfetter.nvim',
 		lazy = true,
-		dev = true
+		dev = true,
 	},
 	{
 		'Axlefublr/lupa.nvim',
 		lazy = true,
-		dev = true
+		dev = true,
 	},
 	{
 		'Axlefublr/selabel.nvim',
 		dev = true,
 		opts = {
 			win_opts = {
-				border = env.border,
-			}
+				border = env.borders,
+			},
 		},
 		config = function(_, opts)
 			require('selabel').setup(opts)
 			env.select = require('selabel').select_nice
-		end
+		end,
 	},
 }
