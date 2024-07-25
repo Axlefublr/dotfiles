059c3883036e86ed7446e31f6876942a90068c2f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-07-25 21:40:04 +0800

    awesome: getup widget is resettable preemptively

--- scripts/widget_update/getup.fish
+++ scripts/widget_update/getup.fish
@@ -1,11 +1,16 @@
 #!/usr/bin/env fish
 
+set -l minutes 60
+
 while true
-    sleep 10
+    while test (clorange getup show) -lt $minutes
+        sleep 1m
+        clorange getup increment
+    end
     awesome-client 'Getup_we()'
-    while not test -s /dev/shm/Getup_f
+    while test (clorange getup show) -ge $minutes
         sleep 5
     end
-    truncate -s 0 /dev/shm/Getup_f
+    clorange getup reset
     awesome-client 'Getup_wd()'
 end
