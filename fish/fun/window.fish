cfd376f39e0bb0b4b9dcb8dfbd270a192762ee32
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-07-26 00:13:23 +0800

    astro: change listchars to display indentation spaces as single char

--- fish/fun/window.fish
+++ fish/fun/window.fish
@@ -1,63 +1,63 @@
 #!/usr/bin/env fish
 
 function get_windows
-	wmctrl -xl | awk '{printf "%s — %s —", $1, $3; for (i=5; i<=NF; i++) printf " %s", $i; print ""}'
+    wmctrl -xl | awk '{printf "%s — %s —", $1, $3; for (i=5; i<=NF; i++) printf " %s", $i; print ""}'
 end
-funcsave get_windows > /dev/null
+funcsave get_windows >/dev/null
 
 function matches
-	get_windows | rg $argv[1]
+    get_windows | rg $argv[1]
 end
-funcsave matches > /dev/null
+funcsave matches >/dev/null
 
 function not_matches
-	if get_windows | rg $argv[1]
-		return 1
-	else
-		return 0
-	end
+    if get_windows | rg $argv[1]
+        return 1
+    else
+        return 0
+    end
 end
-funcsave not_matches > /dev/null
+funcsave not_matches >/dev/null
 
 function matches_except
-	get_windows | rg -v $argv[1] | rg $argv[2]
+    get_windows | rg -v $argv[1] | rg $argv[2]
 end
-funcsave matches_except > /dev/null
+funcsave matches_except >/dev/null
 
 function win_wait
-	loopuntil "matches '$argv[1]'" $argv[2..] | awk '{print $1}'
+    loopuntil "matches '$argv[1]'" $argv[2..] | awk '{print $1}'
 end
-funcsave win_wait > /dev/null
+funcsave win_wait >/dev/null
 
 function win_wait_closed
-	loopuntil "not_matches '$argv[1]'" $argv[2..] | awk '{print $1}'
+    loopuntil "not_matches '$argv[1]'" $argv[2..] | awk '{print $1}'
 end
-funcsave win_wait_closed > /dev/null
+funcsave win_wait_closed >/dev/null
 
 function win_wait_except
-	loopuntil "matches_except '$argv[1]' '$argv[2]'" $argv[3..] | awk '{print $1}'
+    loopuntil "matches_except '$argv[1]' '$argv[2]'" $argv[3..] | awk '{print $1}'
 end
-funcsave win_wait_except > /dev/null
+funcsave win_wait_except >/dev/null
 
 function move_all
-	if not set -q argv[2]
-		return 1
-	end
-	set -l tag (math $argv[1] - 1)
-	for win_id in $argv[2..]
-		wmctrl -ir $win_id -t $tag
-	end
+    if not set -q argv[2]
+        return 1
+    end
+    set -l tag (math $argv[1] - 1)
+    for win_id in $argv[2..]
+        wmctrl -ir $win_id -t $tag
+    end
 end
-funcsave move_all > /dev/null
+funcsave move_all >/dev/null
 
 function minigun_save
-	set -l coords (xdotool getmouselocation | string match -gr '\\w:(\\d+) \\w:(\\d+)')
-	printf $coords[1]' '$coords[2] > ~/.local/share/minigun/$argv[1]
+    set -l coords (xdotool getmouselocation | string match -gr '\\w:(\\d+) \\w:(\\d+)')
+    printf $coords[1]' '$coords[2] >~/.local/share/minigun/$argv[1]
 end
-funcsave minigun_save > /dev/null
+funcsave minigun_save >/dev/null
 
 function minigun_apply
-	set -l coords (cat ~/.local/share/minigun/$argv[1] | string split ' ')
-	xdotool mousemove $coords[1] $coords[2]
+    set -l coords (cat ~/.local/share/minigun/$argv[1] | string split ' ')
+    xdotool mousemove $coords[1] $coords[2]
 end
-funcsave minigun_apply > /dev/null
+funcsave minigun_apply >/dev/null
