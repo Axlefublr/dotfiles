f9d5714d0c2ac119a1aeadd554757d1c8891b713
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-02 06:10:18 +0800

    nvim: fix nvim-various-textobjs once again

--- !nvim/lua/plugins/vartextobjs.lua
+++ !nvim/lua/plugins/vartextobjs.lua
@@ -3,9 +3,9 @@ return {
 	'chrisgrieser/nvim-various-textobjs',
 	lazy = true,
 	keys = {
-		{ mode = { 'o', 'x' }, 'g;', [[<Cmd>require('various-textobjs').lastChange('inner')<CR>]] },
-		{ mode = { 'o', 'x' }, 'is', [[<Cmd>require('various-textobjs').subword('inner')<CR>]] },
-		{ mode = { 'o', 'x' }, 'as', [[<Cmd>require('various-textobjs').subword('outer')<CR>]] },
+		{ mode = { 'o', 'x' }, 'g;', [[<Cmd>lua require('various-textobjs').lastChange('inner')<CR>]] },
+		{ mode = { 'o', 'x' }, 'is', [[<Cmd>lua require('various-textobjs').subword('inner')<CR>]] },
+		{ mode = { 'o', 'x' }, 'as', [[<Cmd>lua require('various-textobjs').subword('outer')<CR>]] },
 	},
 	-- opts = {
 	-- 	lookForwardSmall = 2,
