76491d4055a421399c3c7355202c06d2d9d36e72
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-02 06:14:41 +0800

    sdt: anki widget updates correctly, remove checker

--- scripts/systemd/executables/ten-minutes.fish
+++ scripts/systemd/executables/ten-minutes.fish
@@ -2,7 +2,6 @@
 
 widget_update mature_tasks_line Loago
 widget_update update_anki Anki
-indeed ~/.local/share/magazine/2 "anki at: $(clorange anki show)"
 set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
 if test "$notifications"
     task -u $notifications
