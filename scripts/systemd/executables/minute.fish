3f1e3417d822923c74a9a9280ed0ff7963336afd
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-08 12:59:35 +0800

    sdt: github notifications check minutely

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
 awesome-client 'Hunger_wu()'
+set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
+if test "$notifications"
+    task -u $notifications
+end
--- scripts/systemd/executables/ten-minutes.fish
+++ scripts/systemd/executables/ten-minutes.fish
@@ -2,7 +2,3 @@
 
 widget_update mature_tasks_line Loago
 widget_update update_anki Anki
-set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
-if test "$notifications"
-    task -u $notifications
-end
