f261d4710eaad5e1d856c63ec44fe392c77ff24a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-09 09:59:46 +0800

    sdt: gh notifications go to magazine 4

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -3,5 +3,6 @@
 awesome-client 'Hunger_wu()'
 set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
 if test "$notifications"
-    task -u $notifications
+    indeed -u ~/.local/share/magazine/4 $notifications
+    update_magazine 4
 end
