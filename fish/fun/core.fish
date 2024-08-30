52e1c4e56ea6adeb018e7ff64c5516736f95807d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-30 20:29:41 +0800

    yazi: `zoxide` actually works in cwd changing yazi alias

--- fish/fun/core.fish
+++ fish/fun/core.fish
@@ -44,10 +44,10 @@ function l
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
     if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        z -- "$cwd"
+        z $cwd
     end
-    rm -f -- "$tmp"
     commandline -f repaint
+    rm -f -- "$tmp"
 end
 funcsave l >/dev/null
 
