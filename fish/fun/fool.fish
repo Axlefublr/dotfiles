1982cd5fc09d5e621330d6869d2b3a70688a92d6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-05 07:13:43 +0800

    fish: loago shadow function autocommits `do`s and `remove`s

---
+++ fish/fun/fool.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+function loago
+    command loago $argv
+    if contains $argv[1] do remove
+        set prevdir $PWD
+        cd ~/prog/autocommit
+        cp -f ~/.local/share/loago/loago.json .
+        git add loago.json &>/dev/null
+        and git commit -m "loago $argv" &>/dev/null
+        cd $prevdir
+    end
+end
+funcsave loago >/dev/null
