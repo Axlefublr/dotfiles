324c1eeffd399e0f115823e189f7afc4221a1693
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-07 12:28:44 +0800

    xremap: `+!Tab` closes most recent notification

--- awesome/global.lua
+++ awesome/global.lua
@@ -1,8 +1,7 @@
-function DestroyLatestNotification()
-	local latest_notification = nil
-	for _, value in pairs(naughty.notifications[awful.screen.focused()].bottom_right) do
-		latest_notification = value
-		break
-	end
-	naughty.destroy(latest_notification)
+---@param side 'latest'|'oldest'
+function DestroySidedNotification(side)
+	local notification_list = naughty.notifications[awful.screen.focused()].bottom_right
+	local side_notification = notification_list[side == 'oldest' and 1 or #notification_list]
+	if not side_notification then return end
+	naughty.destroy(side_notification)
 end
