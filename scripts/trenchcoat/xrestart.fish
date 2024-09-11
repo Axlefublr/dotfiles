e878af0b839756d0106a0549507092f53771e937
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-11 12:07:39 +0800

    x: key delay 130->45 repeat 135->40

--- scripts/trenchcoat/xrestart.fish
+++ scripts/trenchcoat/xrestart.fish
@@ -6,6 +6,6 @@ end
 xremap --mouse ~/prog/dotfiles/xremap.yml &>/tmp/log/user.txt & disown
 notify-send -t 2000 'restarted xremap'
 sleep 2
-xset r rate 130 45 &>/tmp/log/xset.txt
+xset r rate 135 40 &>/tmp/log/xset.txt
 setxkbmap -layout us,ru &>/tmp/log/user.txt
 notify-send -t 2000 'applied xset'
