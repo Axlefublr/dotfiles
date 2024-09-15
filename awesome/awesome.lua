c924a9f9ddb2a61ff4b77df70f578e340d3f7eb5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-15 23:30:58 +0800

    awm: remove awful help widget

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -10,9 +10,6 @@ beautiful = require('beautiful')
 naughty = require('naughty')
 menubar = require('menubar')
 hotkeys_popup = require('awful.hotkeys_popup')
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require('awful.hotkeys_popup.keys')
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
