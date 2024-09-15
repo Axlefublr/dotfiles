7c8766c76a15fd18a63439f9501c40f2a8714c6c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-15 23:29:39 +0800

    awm: remove luarocks support

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -1,7 +1,3 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, 'luarocks.loader')
-
 -- Standard awesome library
 gears = require('gears')
 awful = require('awful')
