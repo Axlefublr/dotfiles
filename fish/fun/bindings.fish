fca46a484a3fa5c3dae8d66e658cc0a62aec9a23
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-22 00:18:04 +0800

    fish: better bindings abstraction

--- fish/fun/bindings.fish
+++ fish/fun/bindings.fish
@@ -1,26 +1,23 @@
 #!/usr/bin/env fish
 
-function bind_visual
-    bind -M visual $argv
+function binds
+    argparse v/visual d/default i/insert -- $argv
+    or return 1
+    if test "$_flag_visual"
+        bind -M visual $argv
+    end
+    if test "$_flag_default"
+        bind -M default $argv
+    end
+    if test "$_flag_insert"
+        bind -M insert $argv
+    end
 end
-funcsave bind_visual >/dev/null
-
-function bind_all
-    bind -M default $argv
-    bind -M insert $argv
-    bind -M visual $argv
-end
-funcsave bind_all >/dev/null
-
-function bind_both
-    bind -M default $argv
-    bind -M visual $argv
-end
-funcsave bind_both >/dev/null
+funcsave binds >/dev/null
 
 function fish_user_key_bindings
     #------------------------------------------------disabling------------------------------------------------
-    bind_all \es true
+    binds -vid \es true
 
     #------------------------------------------------helix mode------------------------------------------------
     bind w forward-word
@@ -28,51 +25,51 @@ function fish_user_key_bindings
 
     bind D delete-char backward-char
     bind d delete-char forward-single-char backward-char
-    bind_visual -m default d kill-selection end-selection
+    binds -v -m default d kill-selection end-selection
 
     bind -m visual x begin-selection
-    bind_visual -m default x end-selection
+    binds -v -m default x end-selection
 
     bind -m visual v beginning-of-line begin-selection end-of-line
     bind -m visual V end-of-line begin-selection beginning-of-line
-    bind_visual v down-line
-    bind_visual V up-line
+    binds -v v down-line
+    binds -v V up-line
 
-    bind_both -m visual zl begin-selection end-of-line
-    bind_both -m visual zh begin-selection beginning-of-line
-    bind_both gh beginning-of-line
-    bind_both gl end-of-line
-    bind_both gi beginning-of-buffer
-    bind_both go end-of-buffer
+    binds -vd -m visual zl begin-selection end-of-line
+    binds -vd -m visual zh begin-selection beginning-of-line
+    binds -vd gh beginning-of-line
+    binds -vd gl end-of-line
+    binds -vd gi beginning-of-buffer
+    binds -vd go end-of-buffer
 
     bind -m visual % beginning-of-buffer begin-selection end-of-buffer
 
     bind S 'commandline | copy'
 
-    bind_both e edit_command_buffer
+    binds -vd e edit_command_buffer
 
-    bind_both K exit
+    binds -vd K exit
 
     #--------------------------------------------------core--------------------------------------------------
-    bind_all \em yazi-cd
-    bind_all \ep 'over lazygit'
-    bind_all \eh 'zi && commandline -f repaint'
+    binds -vid \em yazi-cd
+    binds -vid \ep 'over lazygit'
+    binds -vid \eh 'zi && commandline -f repaint'
 
     #-------------------------------------------------other-------------------------------------------------
-    bind_all \ej complete
-    bind_all \ek complete-and-search
-    bind_all \cn complete
-    bind_all \cp complete-and-search
-    bind_all \eo expand-abbr
-    bind -M insert / expand-abbr self-insert
+    binds -vid \ej complete
+    binds -vid \ek complete-and-search
+    binds -vid \cn complete
+    binds -vid \cp complete-and-search
+    binds -vid \eo expand-abbr
+    binds -i / expand-abbr self-insert
 
-    bind -M insert \e\r expand-abbr insert-line-under
-    bind -M insert -k f4 'commandline ""' # is ctrl+alt+u
+    binds -i \e\r expand-abbr insert-line-under
+    binds -i -k f4 'commandline ""' # is ctrl+alt+u
 
-    bind_all -k f5 forward-word # is shift+alt+;
-    bind_all \e\; accept-autosuggestion
+    binds -vid -k f5 forward-word # is shift+alt+;
+    binds -vid \e\; accept-autosuggestion
 
-    bind_all \ed clear-screen repaint
-    bind_all \eu 'for cmd in sudo doas please; if command -q $cmd; fish_commandline_prepend $cmd; break; end; end'
+    binds -vid \ed clear-screen repaint
+    binds -vid \eu 'for cmd in sudo doas please; if command -q $cmd; fish_commandline_prepend $cmd; break; end; end'
 end
 funcsave fish_user_key_bindings >/dev/null
