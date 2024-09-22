4ef63fc3660c08f90d5a0bf066936b0c251781dc
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-22 22:24:58 +0800

    helix: only `:reload-all` mapping to force checking for unsaved changes

--- helix/generator.py
+++ helix/generator.py
@@ -446,8 +446,7 @@ normal_select_mappings: dict[str, Any] = {
         'a': 'append_mode',
         'n': 'select_all_siblings',
         'N': 'select_all_children',
-        "'": ':reload',
-        '"': ':reload-all',
+        "'": ':reload-all',
         **disable(
             [
                 '?',
