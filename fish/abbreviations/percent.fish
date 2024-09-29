65e33768a9d4a44f4a1f729246c1845b6446a0ce
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-09-29 23:36:20 +0800

    fish: remove `f` abbreviation

--- fish/abbreviations/percent.fish
+++ fish/abbreviations/percent.fish
@@ -1,6 +1,5 @@
 abbr -a .. --regex '^\.\.+$' --function multiple_dot
 
-abbr --set-cursor -a f 'nvim -c Young % && clx'
 abbr --set-cursor -a ytv 'yt-dlp -o \'%.%(ext)s\' \'\''
 abbr --set-cursor -a yta 'yt-dlp -x --audio-format mp3 -o \'%.%(ext)s\' \'\''
 abbr --set-cursor -a diff 'diff -u % | diff-so-fancy'
