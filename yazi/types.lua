b1af5ea59aa77b37a48baed0ac2cfc7a34881cb0
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-12 19:06:20 +0800

    yazi: create harp.yazi

--- yazi/types.lua
+++ yazi/types.lua
@@ -1,6 +1,6 @@
 ya = {}
 
----@alias YaInputPositionRelativity
+---@alias InputPositionAnchor
 ---| 'top-left'
 ---| 'top-center'
 ---| 'top-right'
@@ -10,27 +10,107 @@ ya = {}
 ---| 'center'
 ---| 'hovered'
 
----@class YaInputPosition
----@field [1] YaInputPositionRelativity
+---@class InputPosition
+---@field [1] InputPositionAnchor
 ---@field x integer? offset from origin position
 ---@field y integer? offset from origin position
 ---@field w integer
 ---@field h integer?
 
----@class YaInputOpts
+---@class InputOpts
 ---@field title string
 ---@field value string? the default value in the inputbox
----@field position YaInputPosition
+---@field position InputPosition
 ---@field realtime boolean? report user input in real time
 ---@field debounce number? the number of seconds to wait for the user to stop typing (float). Can only be used when `realtime = true`
 
----@alias YaInputEvent
+---@alias InputEvent
 ---| 0 unknown error
 ---| 1 user confirmed input
 ---| 2 user cancelled input
 ---| 3 user changed input (only if `realtime` is true)
 
----@param opts YaInputOpts
----@return string?, YaInputEvent
+---@param opts InputOpts
+---@return string?, InputEvent
 ---When `realtime = true` specified, `ya.input()` returns a receiver, which has a `recv()` method that can be called multiple times to receive events.
 function ya.input(opts) return '', 0 end
+
+---@alias LogLevel
+---| 'info'
+---| 'warn'
+---| 'error'
+
+---@class NotifyOpts
+---@field title string
+---@field content string
+---@field timeout number
+---@field level LogLevel? is `'info'` by default
+
+---@alias Sendable
+---| nil
+---| boolean
+---| number
+---| string
+---| [Sendable]
+---| table<string|number, Sendable>
+
+---@param opts NotifyOpts
+function ya.notify(opts) end
+
+---@alias ManagerCommands
+---| 'cd'
+
+---@param cmd ManagerCommands
+---@param args table<string|number, Sendable>
+function ya.manager_emit(cmd, args) end
+
+---@param func fun():any?
+function ya.sync(func)
+	return function() end
+end
+
+---@class Url
+
+---@param url string
+---@return Url
+function Url(url)
+	return {} --[[@as Url]]
+end
+
+---@class FolderFolder
+---@field cwd Url
+
+---@class TabTab
+---@field current FolderFolder
+
+cx = {
+	---@type TabTab
+	active = {
+		---@type FolderFolder
+		current = {
+			cwd = {} --[[@as Url]],
+		},
+	},
+}
+
+---@class Status
+---@field success boolean
+---@field code Exitcode
+
+---@class Output
+---@field status Status
+---@field stdout string
+---@field stderr string
+
+---@alias Exitcode integer
+
+---@class Command
+---@field args (fun(self:Command, args: string[]):Command) append multiple arguments to the command
+---@field output (fun(self:Command):(Output?, Exitcode)) spawn the command and wait for it to finish
+
+---@param cmd string
+---@return Command
+function Command(cmd)
+	---@diagnostic disable-next-line: missing-fields
+	return {} --[[@as Command]]
+end
