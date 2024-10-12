6a4e8936e1cde3ab740ffc874d08f1d6824042d5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-12 12:12:36 +0800

    fish: simplify logic for fool aliases

--- fish/fun/fool.fish
+++ fish/fun/fool.fish
@@ -15,46 +15,32 @@ end
 funcsave loago >/dev/null
 
 function gh
-    if not test "$argv"
-        ? gh
-        return 1
+    if test "$argv[1..2]" = 'repo clone'
+        command gh repo clone $argv[3..] -- --depth 1
+        z (path basename $argv[3])
+        clx
+    else if test "$argv[1..2]" = 'repo fork'
+        command gh repo fork $argv[3..] --clone --default-branch-only -- --depth 1
+        z (path basename $argv[3])
+        clx
+    else
+        command gh $argv
     end
-    if test $argv[1] = repo
-        switch $argv[2]
-            case clone
-                command gh repo clone $argv[3..] -- --depth 1
-                z (path basename $argv[3])
-                clx
-            case fork
-                command gh repo fork $argv[3..] --clone --default-branch-only -- --depth 1
-                z (path basename $argv[3])
-                clx
-            case '*'
-                command gh $argv
-        end
-        return
-    end
-    command gh $argv
 end
 funcsave gh >/dev/null
 
 function alien_temple
-    if not test "$argv"
-        ? alien_temple
-        return 1
-    end
-    switch $argv[1]
-        case shark s
-            set -l shark (command alien_temple shark)
-            printf '%s\n' $shark
-            echo $shark[1] | copy
-        case consent c
-            command alien_temple consent | tee /dev/tty | copy
-        case play
-            command alien_temple $argv
-            loago do liked
-        case '*'
-            command alien_temple $argv
+    if test "$argv[1]" = shark -o "$argv[1]" = s
+        set -l shark (command alien_temple shark)
+        printf '%s\n' $shark
+        echo $shark[1] | copy
+    else if test "$argv[1]" = consent -o "$argv[1]" = c
+        command alien_temple consent | tee /dev/tty | copy
+    else if test "$argv[1]" = play
+        command alien_temple $argv
+        loago do liked
+    else
+        command alien_temple $argv
     end
 end
 funcsave alien_temple >/dev/null
