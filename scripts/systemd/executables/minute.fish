bc0ff3bd72e7a4527e5d14a60ed4ecd23b3efb97
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-20 03:30:22 +0800

    sdt: disk usage updates minutely

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 awesome-client 'Hunger_wu()'
+widget_update disk_usage Disk_usage
 pjs
 set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
 if test "$notifications"
