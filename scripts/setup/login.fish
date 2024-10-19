87a95aa8ac0dd9e69ba6d5450849afba7b2fc2e1
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-20 02:02:43 +0800

    login: change priorities for startup apps

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -6,26 +6,21 @@ else
     mkdir -p /tmp/log
 end
 
-for script in ~/prog/dotfiles/scripts/widget_update/*.fish
-    $script 2>>/tmp/log/user.txt & disown
-end
-
-ydotoold 2>>/tmp/log/user.txt & disown
-# ollama serve 2>>/tmp/log/user.txt & disown
 picom 2>>/tmp/log/user.txt & disown
-gromit.fish 2>>/tmp/log/user.txt & disown
-copyq 2>>/tmp/log/user.txt & disown
-xset s off -dpms 2>>/tmp/log/user.txt
 redshift -O 5700 2>>/tmp/log/user.txt
-playerctld daemon
-
+xset s off -dpms 2>>/tmp/log/user.txt
 xrestart.fish
 
 kitty -T editor -d ~/prog/dotfiles --hold helix 2>>/tmp/log/user.txt & disown
 kitty -T men 2>>/tmp/log/user.txt & disown
 kitty -T oil-content -d ~/vid/content --hold fish -c yazi 2>>/tmp/log/user.txt & disown
 
+for script in ~/prog/dotfiles/scripts/widget_update/*.fish
+    $script 2>>/tmp/log/user.txt & disown
+end
+
 vivaldi-stable --force-dark-mode 2>>/tmp/log/user.txt & disown
+anki 2>>/tmp/log/user.txt & disown
 set vivaldis (win_wait 'Vivaldi-stable' 0.1 5 200)
 move_all 2 $vivaldis
 set discord (win_wait 'Vivaldi-stable — .*(Discord|Mastodon|Messenger)' 0.1 0 50)
@@ -34,11 +29,14 @@ set youtube (win_wait 'Vivaldi-stable — .*YouTube' 0.1 0 50)
 move_all 7 $youtube
 set spotify (win_wait 'Vivaldi-stable — .*Spotify' 0.1 0 50)
 move_all 7 $spotify
-
-anki 2>>/tmp/log/user.txt & disown
 set ankis (win_wait 'anki\.Anki' 0.1 0 50)
 move_all 8 $ankis
 
+ydotoold 2>>/tmp/log/user.txt & disown
+# ollama serve 2>>/tmp/log/user.txt & disown
+gromit.fish 2>>/tmp/log/user.txt & disown
+copyq 2>>/tmp/log/user.txt & disown
+playerctld daemon
 RUST_LOG=debug axleizer &>/tmp/log/axleizer.txt & disown
 
 # notify-send -t 2000 spotify
