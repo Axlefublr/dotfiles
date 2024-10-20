f9a733a86d0555f8a2287ff5c6a32fa2d7a255f8
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-21 03:04:46 +0800

    sdt: autocommits are made by the loago alias only

--- scripts/widget_update/loago.fish
+++ scripts/widget_update/loago.fish
@@ -4,10 +4,4 @@ while true
     inotifywait -q -e close_write -e move_self ~/.local/share/loago/loago.json
     widget_update mature_tasks_line Loago
     awesome-client 'Hunger_wu()'
-    set prevdir $PWD
-    cd ~/prog/autocommit
-    cp -f ~/.local/share/loago/loago.json .
-    git add loago.json &>/dev/null
-    and git commit -m "loago $argv" &>/dev/null
-    cd $prevdir
 end
