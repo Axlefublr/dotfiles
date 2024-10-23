81482eb166ac446d240125fde7c9d7541205cdc3
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-23 08:39:17 +0800

    script: ibsort uses `[[sort on]]` and `[[sort off]]`

--- scripts/ibsort.fish
+++ scripts/ibsort.fish
@@ -5,10 +5,10 @@ for file in $argv
         set -l to_sort
         set -l grabbing false
         for line in (cat $file)
-            if string match -qe '[[son]]' "$line"
+            if string match -qe '[[sort'' on]]' "$line"
                 echo "$line"
                 set grabbing true
-            else if string match -qe '[[soff]]' "$line"
+            else if string match -qe '[[sort'' off]]' "$line"
                 printf '%s\n' $to_sort | sort.py
                 set to_sort
                 echo "$line"
