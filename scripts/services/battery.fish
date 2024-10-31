e626a0b13c8aa447bca7eb278ccfa98ccd9e8323
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-31 21:00:06 +0800

    wup: notification if battery <= 5

--- scripts/services/battery.fish
+++ scripts/services/battery.fish
@@ -6,6 +6,9 @@ end
 
 while true
     set -U battery_charge (battery_percentage)
+    if test $battery_charge -le 5
+        notify-send -t 0 'battery <= 5'
+    end
     widget_update echo $battery_charge Charge
     sleep 30
 end
