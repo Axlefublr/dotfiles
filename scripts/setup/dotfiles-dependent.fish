b8c29647c78d766381ef9a16fd44e79fcb9b0a5b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-11-23 01:55:22 +0800

    fish: dotfiles-dependent is all in functions

--- scripts/setup/dotfiles-dependent.fish
+++ scripts/setup/dotfiles-dependent.fish
@@ -1,89 +1,111 @@
 #!/usr/bin/env fish
 
-# X
-ln -sf ~/prog/dotfiles/x11/xresources ~/.Xresources
-ln -sf ~/prog/dotfiles/x11/.XCompose ~/.XCompose
-
-# Fish shell
-chsh -s /usr/bin/fish
-mkdir -p ~/.config/fish
-ln -sf ~/prog/dotfiles/fish/config.fish ~/.config/fish/config.fish
-for file in ~/prog/dotfiles/fish/fun/*.fish
-    $file
+function inst-x
+    ln -sf ~/prog/dotfiles/x11/xresources ~/.Xresources
+    ln -sf ~/prog/dotfiles/x11/.XCompose ~/.XCompose
 end
-~/prog/dotfiles/fish/once.fish
-~/prog/dotfiles/fish/universal.fish
 
-# Paru
-mkdir -p ~/.config/paru
-ln -sf ~/prog/dotfiles/paru.conf ~/.config/paru/paru.conf
+function inst-fish
+    chsh -s /usr/bin/fish
+    mkdir -p ~/.config/fish
+    ln -sf ~/prog/dotfiles/fish/config.fish ~/.config/fish/config.fish
+    for file in ~/prog/dotfiles/fish/fun/*.fish
+        $file
+    end
+    ~/prog/dotfiles/fish/once.fish
+    ~/prog/dotfiles/fish/universal.fish
+end
 
-# Xremap
-sudo usermod -aG input $USER
-sudo usermod -aG video $USER
-echo 'KERNEL=="uinput", GROUP="input", MODE="0660"' | sudo tee /etc/udev/rules.d/99-uinput.rules
+function inst-fish-lsp
+    git clone https://github.com/ndonfris/fish-lsp ~/prog/stored/fish-lsp
+    cd ~/prog/stored/fish-lsp
+    yarn install
+end
 
-# Awesome wm
-mkdir -p ~/.config/awesome
-rm -fr ~/.config/awesome
-ln -sf ~/prog/dotfiles/awesome ~/.config/
-ln -sf ~/prog/dotfiles/awesome/awesome.lua ~/.config/awesome/rc.lua
+function inst-paru
+    mkdir -p ~/.config/paru
+    ln -sf ~/prog/dotfiles/paru.conf ~/.config/paru/paru.conf
+end
 
-# Helix
-cd ~/prog/forks
-gh-repo-clone Axlefublr/helix
-cd ~/prog/forks/helix
-cargo install --path helix-term --locked
-ln -sf ~/prog/dotfiles/helix ~/.config
-mkdir -p ~/.cargo/bin/
-rm -fr ~/.cargo/bin/runtime
-ln -sf ~/prog/forks/helix/runtime ~/.cargo/bin/
+function inst-xremap
+    sudo usermod -aG input $USER
+    sudo usermod -aG video $USER
+    echo 'KERNEL=="uinput", GROUP="input", MODE="0660"' | sudo tee /etc/udev/rules.d/99-uinput.rules
+end
 
-# Kitty terminal
-mkdir -p ~/.config/kitty
-ln -sf ~/prog/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -sf ~/prog/dotfiles/kitty/theme.conf ~/.config/kitty/current-theme.conf
+function inst-awm
+    mkdir -p ~/.config/awesome
+    rm -fr ~/.config/awesome
+    ln -sf ~/prog/dotfiles/awesome ~/.config/
+    ln -sf ~/prog/dotfiles/awesome/awesome.lua ~/.config/awesome/rc.lua
+end
 
-# Lazygit
-mkdir -p ~/.config/lazygit
-ln -sf ~/prog/dotfiles/lazygit.yml ~/.config/lazygit/config.yml
+function inst-helix
+    cd ~/prog/forks
+    git clone https://github.com/Axlefublr/helix
+    cd ~/prog/forks/helix
+    cargo install --path helix-term --locked
+    ln -sf ~/prog/dotfiles/helix ~/.config
+    mkdir -p ~/.cargo/bin/
+    rm -fr ~/.cargo/bin/runtime
+    ln -sf ~/prog/forks/helix/runtime ~/.cargo/bin/
+end
 
-# Yazi
-rm -fr ~/.config/yazi
-ln -s ~/prog/dotfiles/yazi ~/.config
-ya pack -a yazi-rs/plugins:hide-preview
-ya pack -a yazi-rs/plugins:jump-to-char
-ya pack -a Ape/open-with-cmd
-ya pack -a KKV9/compress
+function inst-kitty
+    mkdir -p ~/.config/kitty
+    ln -sf ~/prog/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+    ln -sf ~/prog/dotfiles/kitty/theme.conf ~/.config/kitty/current-theme.conf
+end
 
-# Ruby
-gem install solargraph
+function inst-lazygit
+    mkdir -p ~/.config/lazygit
+    ln -sf ~/prog/dotfiles/lazygit.yml ~/.config/lazygit/config.yml
+end
 
-# Scriptisto
-scriptisto new crystal >~/prog/the
+function inst-yazi
+    rm -fr ~/.config/yazi
+    ln -s ~/prog/dotfiles/yazi ~/.config
+    ya pack -a yazi-rs/plugins:hide-preview
+    ya pack -a yazi-rs/plugins:jump-to-char
+    ya pack -a Ape/open-with-cmd
+    ya pack -a KKV9/compress
+end
 
-# Brillo
-begin
-    echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"'
-    echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"'
-end | sudo tee /etc/udev/rules.d/backlight.rules
+function inst-ruby
+    gem install solargraph
+end
 
-# Ttyper
-ln -sf ~/.local/share/magazine/U ~/.config/ttyper/language/uclanr
+function inst-scriptisto
+    scriptisto new crystal >~/prog/the/crystalthe.cr
+    scriptisto new elixir >~/prog/the/elixirthe.ex
+end
 
-# Neovim
-# trash-put ~/.config/nvim
-# mkdir -p ~/.config/nvim
-# for file in ~/prog/dotfiles/!nvim/*
-#     ln -sf $file ~/.config/nvim
-# end
-# for file in ~/prog/dotfiles/!nvim/.*
-#     ln -sf $file ~/.config/nvim
-# end
+function inst-brillo
+    begin
+        echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"'
+        echo 'ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl1", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"'
+    end | sudo tee /etc/udev/rules.d/backlight.rules
+end
 
-# Alacritty
-# mkdir -p ~/.config/alacritty
-# ln -sf ~/prog/dotfiles/alacritty.toml ~/.config/alacritty.toml
+function inst-ttyper
+    ln -sf ~/.local/share/magazine/U ~/.config/ttyper/language/uclanr
+end
+
+function inst-nvim
+    trash-put ~/.config/nvim
+    mkdir -p ~/.config/nvim
+    for file in ~/prog/dotfiles/!nvim/*
+        ln -sf $file ~/.config/nvim
+    end
+    for file in ~/prog/dotfiles/!nvim/.*
+        ln -sf $file ~/.config/nvim
+    end
+end
+
+function inst-alacritty
+    mkdir -p ~/.config/alacritty
+    ln -sf ~/prog/dotfiles/alacritty.toml ~/.config/alacritty.toml
+end
 
 # Vscode
 # mkdir -p ~/.config/Code/User
@@ -133,97 +155,119 @@ sudo ln -sf /usr/bin/rofi /usr/bin/dmenu
 # Screenkey
 sudo ln -sf ~/prog/dotfiles/desktop/screenkey.desktop /usr/share/applications
 
-# Tsoding/boomer
-cd ~/prog
-git clone https://github.com/tsoding/boomer
-cd boomer
-nimble build
-ln -sf ~/prog/dotfiles/boomer.conf ~/.config/boomer/config
+function install-nim
+    curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+end
 
-# Ghostty
-cd ~/prog/stored
-gh repo clone https://github.com/ghostty-org/ghostty -- --depth 1
-cd ghostty
-zig build -p $HOME/.local -Doptimize=ReleaseFast -Dapp-runtime=gtk
-mkdir -p ~/.config/ghostty
-ln -sf ~/prog/dotfiles/ghostty.conf ~/.config/ghostty/config
+function install-nim-tooling
+    # nimble install nimlangserver
+    nimble install nimlsp
+end
 
-# Zathura
-mkdir -p ~/.config/zathura
-ln -sf ~/prog/dotfiles/zathura ~/.config/zathura/zathurarc
-xdg-mime default org.pwmt.zathura.desktop application/pdf
+function install-boomer
+    cd ~/prog/forks
+    git clone https://github.com/Axlefublr/boomer
+    cd boomer
+    nimble build
+    ln -sf ~/prog/dotfiles/boomer.conf ~/.config/boomer/config
+end
 
-# Display
-ln -sf ~/prog/dotfiles/desktop/display.desktop ~/.local/share/applications/display.desktop
-xdg-mime default display.desktop image/svg+xml
-xdg-mime default display.desktop image/png
-xdg-mime default display.desktop image/jpeg
-xdg-mime default display.desktop image/gif
-xdg-mime default display.desktop image/webp
+function install-ghostty
+    cd ~/prog/stored
+    git clone --depth=1 https://github.com/ghostty-org/ghostty
+    cd ghostty
+    zig build -p $HOME/.local -Doptimize=ReleaseFast -Dapp-runtime=gtk
+    mkdir -p ~/.config/ghostty
+    ln -sf ~/prog/dotfiles/ghostty.conf ~/.config/ghostty/config
+end
 
-# Ruff
-mkdir -p ~/.config/ruff
-ln -sf ~/prog/dotfiles/defconf/pyproject.toml ~/.config/ruff/pyproject.toml
+function install-zathura
+    mkdir -p ~/.config/zathura
+    ln -sf ~/prog/dotfiles/zathura ~/.config/zathura/zathurarc
+    xdg-mime default org.pwmt.zathura.desktop application/pdf
+end
 
-# Krita
-mkdir -p ~/.local/share/color-schemes
-ln -f ~/prog/dotfiles/krita/gruvbox.colors ~/.local/share/krita/color-schemes
+function install-display
+    ln -sf ~/prog/dotfiles/desktop/display.desktop ~/.local/share/applications/display.desktop
+    xdg-mime default display.desktop image/svg+xml
+    xdg-mime default display.desktop image/png
+    xdg-mime default display.desktop image/jpeg
+    xdg-mime default display.desktop image/gif
+    xdg-mime default display.desktop image/webp
+end
 
-# Eww
-# cd ~/prog/stored
-# git clone https://github.com/elkowar/eww
-# cd eww
-# cargo build --release --no-default-features --features x11
-# ln $PWD/target/release/eww ~/prog/binaries
-# mkdir -p ~/.config/eww
-# touch ~/.config/eww/eww.yuck
+function install-ruff
+    mkdir -p ~/.config/ruff
+    ln -sf ~/prog/dotfiles/defconf/pyproject.toml ~/.config/ruff/pyproject.toml
+end
 
-# !Eww
-# rm -fr ~/prog/stored/eww
-# rm -fr ~/.config/eww
-# rm -fr ~/prog/binaries/eww
+function install-krita
+    mkdir -p ~/.local/share/color-schemes
+    ln -f ~/prog/dotfiles/krita/gruvbox.colors ~/.local/share/krita/color-schemes
+end
 
-# Bubbly
-# curl https://raw.githubusercontent.com/siduck/bubbly/buttons/install.sh | sh
+function inst-eww
+    cd ~/prog/stored
+    git clone https://github.com/elkowar/eww
+    cd eww
+    cargo build --release --no-default-features --features x11
+    ln $PWD/target/release/eww ~/prog/binaries
+    mkdir -p ~/.config/eww
+    touch ~/.config/eww/eww.yuck
+end
 
-# !Bubbly
-# rm -fr ~/.local/share/bubbly
-# rm -fr ~/.config/bubbly
-# rm -fr ~/.local/share/applications/bubbly.desktop
+function uninst-eww
+    rm -fr ~/prog/stored/eww
+    rm -fr ~/.config/eww
+    rm -fr ~/prog/binaries/eww
+end
 
-# Tridactyl
-# :nativeinstall
-# to copy the command to execute
-# mkdir -p ~/.config/tridactyl
-# ln -f ~/prog/dotfiles/tridactyl ~/.config/tridactyl/tridactylrc
+function inst-bubbly
+    curl https://raw.githubusercontent.com/siduck/bubbly/buttons/install.sh | sh
+end
 
-# Qalculate
-# set vspace off
-# set curconv off
-# set update exchange rates 1
+function uninst-bubbly
+    rm -fr ~/.local/share/bubbly
+    rm -fr ~/.config/bubbly
+    rm -fr ~/.local/share/applications/bubbly.desktop
+end
 
-# Postgresql
-ln -sf ~/prog/dotfiles/psqlrc ~/.psqlrc
-sudo -iu postgres initdb -D /var/lib/postgres/data
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
-sudo usermod -aG postgres $USER
-sudo setfacl -R -m u:axlefublr:rwx /var/lib/postgres/data
-nvim /var/lib/postgres/data/postgresql.conf # listen_addresses = '*'
-sudo sed -i '/^host/s/ident/md5/' /var/lib/postgres/data/pg_hba.conf
-sudo sed -i '/^local/s/peer/trust/' /var/lib/postgres/data/pg_hba.conf
-echo "host all all 0.0.0.0/0 md5" | sudo tee -a /var/lib/postgres/data/pg_hba.conf
-sudo setfacl -R -x u:axlefublr /var/lib/postgres/data
-sudo systemctl restart postgresql
-sudo ufw allow 5432/tcp
-sudo touch /var/lib/postgres/.psql_history
-sudo chown postgres:postgres /var/lib/postgres/.psql_history
-psql -U postgres -c "ALTER USER postgres PASSWORD '$(read -sP 'enter your password: ')'"
-cd ~/Documents
-curl -O https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
-unzip dvdrental.zip
-rm -fr dvdrental.zip
-psql -U postgres -c 'CREATE DATABASE dvdrental;'
-pg_restore -U postgres --dbname=dvdrental --verbose dvdrental.tar
-rm -fr dvdrental.tar
-psql -U postgres -d dvdrental -c 'SELECT count(*) FROM film;' -c 'SELECT version();'
+function inst-tridactyl
+    :nativeinstall
+    to copy the command to execute
+    mkdir -p ~/.config/tridactyl
+    ln -f ~/prog/dotfiles/tridactyl ~/.config/tridactyl/tridactylrc
+end
+
+function inst-qalc
+    # set vspace off
+    # set curconv off
+    # set update exchange rates 1
+end
+
+function inst-postgresql
+    ln -sf ~/prog/dotfiles/psqlrc ~/.psqlrc
+    sudo -iu postgres initdb -D /var/lib/postgres/data
+    sudo systemctl start postgresql
+    sudo systemctl enable postgresql
+    sudo usermod -aG postgres $USER
+    sudo setfacl -R -m u:axlefublr:rwx /var/lib/postgres/data
+    nvim /var/lib/postgres/data/postgresql.conf # listen_addresses = '*'
+    sudo sed -i '/^host/s/ident/md5/' /var/lib/postgres/data/pg_hba.conf
+    sudo sed -i '/^local/s/peer/trust/' /var/lib/postgres/data/pg_hba.conf
+    echo "host all all 0.0.0.0/0 md5" | sudo tee -a /var/lib/postgres/data/pg_hba.conf
+    sudo setfacl -R -x u:axlefublr /var/lib/postgres/data
+    sudo systemctl restart postgresql
+    sudo ufw allow 5432/tcp
+    sudo touch /var/lib/postgres/.psql_history
+    sudo chown postgres:postgres /var/lib/postgres/.psql_history
+    psql -U postgres -c "ALTER USER postgres PASSWORD '$(read -sP 'enter your password: ')'"
+    cd ~/Documents
+    curl -O https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+    unzip dvdrental.zip
+    rm -fr dvdrental.zip
+    psql -U postgres -c 'CREATE DATABASE dvdrental;'
+    pg_restore -U postgres --dbname=dvdrental --verbose dvdrental.tar
+    rm -fr dvdrental.tar
+    psql -U postgres -d dvdrental -c 'SELECT count(*) FROM film;' -c 'SELECT version();'
+end
