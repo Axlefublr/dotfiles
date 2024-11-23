6fbb33093fe28f3f6627d260f982d64b3794417f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-11-23 11:34:24 +0800

    anki: freqing is sequential, not randomized

--- fish/fun/anki.fish
+++ fish/fun/anki.fish
@@ -26,7 +26,11 @@ funcsave anki-once >/dev/null
 
 function anki-should
     anki-should-impl (anki-once)
-    shuf -n 1 -e freq dont dont
+    if test (math (clorange freqs increment) % 4) -eq 0
+        echo freq
+    else
+        echo dont
+    end
 end
 funcsave anki-should >/dev/null
 
