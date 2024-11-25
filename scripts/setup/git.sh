45c44d33cd06304e234795be2627ffe845f68d33
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-11-25 09:59:36 +0800

    git: better move semantics config

--- scripts/setup/git.sh
+++ scripts/setup/git.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu --noconfirm git
-git config --global user.email 101342105+Axlefublr@users.noreply.github.com
-git config --global user.name Axlefublr
-git config --global init.defaultBranch main
+sudo pacman -Syyu git
+# [[sort on]]
+git config --global branch.sort -committerdate
+git config --global checkout.defaultRemote origin
+git config --global commit.verbose true
+git config --global core.editor helix
 git config --global credential.helper store
-git config --global push.autoSetupRemote true
+git config --global diff.colormoved default
+git config --global diff.colormovedws allow-indentation-change
+git config --global init.defaultBranch main
 git config --global interactive.singleKey true
 git config --global pull.ff only
-git config --global commit.verbose true
-git config --global checkout.defaultRemote origin
+git config --global push.autoSetupRemote true
 git config --global rerere.enabled true
-git config --global branch.sort -committerdate
-git config --global core.editor helix
+git config --global user.email 101342105+Axlefublr@users.noreply.github.com
+git config --global user.name Axlefublr
