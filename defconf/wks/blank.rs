3cde9f826a6a71a1bfac1670fe5bdfa3d2de550c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-12-26 01:00:29 +0800

    add wks defaults

---
+++ defconf/wks/blank.rs
@@ -0,0 +1,13 @@
+// ~/prog/dotfiles/scripts/
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Hello, world!");
+    Ok(())
+}
