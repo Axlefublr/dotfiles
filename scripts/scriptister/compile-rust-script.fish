8d0e17d8a3496e5689a97b99015ed1d805d5d050
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-03 09:29:28 +0800

    wks: binary is only updated if you don't close the compiling window

--- scripts/scriptister/compile-rust-script.fish
+++ scripts/scriptister/compile-rust-script.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 cargo build --release
-and rsync ./target/release/wks ~/prog/binaries/wks/$argv[1]
 read -P 'press enter to continue' -ln1 the
+rsync ./target/release/wks ~/prog/binaries/wks/$argv[1]
