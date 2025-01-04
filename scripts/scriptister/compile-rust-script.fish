78c19bb9b839d604db371ada4dbd24ea3fed4597
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-05 05:33:37 +0800

    wks: compilation script actually copies over once the compilation is done

--- scripts/scriptister/compile-rust-script.fish
+++ scripts/scriptister/compile-rust-script.fish
@@ -1,5 +1,3 @@
 #!/usr/bin/env fish
 
-pueue add -g c -- 'cargo build --release'
-trash-put ~/prog/binaries/wks/$argv[1]
-rsync ./target/release/wks ~/prog/binaries/wks/$argv[1]
+pueue add -g c -- "cargo build --release ; rsync ./target/release/wks ~/prog/binaries/wks/$argv[1]"
