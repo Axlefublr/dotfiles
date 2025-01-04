f9ef3d8781c77d7069b38655d686c2df79cf6bc8
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-04 11:35:23 +0800

    add pueue fool alias

---
+++ scripts/fool/pueue.rs
@@ -0,0 +1,57 @@
+// begin Cargo.toml
+// [dependencies]
+// end Cargo.toml
+// /home/axlefublr/prog/dotfiles/scripts/fool/pueue.rs
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::env::args;
+use std::env::Args;
+use std::env::{self};
+use std::error::Error;
+use std::ffi::OsStr;
+use std::fs;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::stdin;
+use std::io::Read;
+use std::io::Write;
+use std::iter::Skip;
+use std::os::unix::process::CommandExt;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = args().skip(1);
+    let mut cmd = Command::new("/usr/bin/pueue");
+
+    fn etc(mut cmd: Command) -> ! {
+        cmd.exec();
+        unreachable!("process didn't transfer as expected");
+    }
+
+    let Some(subcommand) = args.next() else { etc(cmd) };
+
+    match &subcommand[..] {
+        "g" => {
+            cmd.arg("group");
+        },
+        "cl" => {
+            cmd.arg("clean");
+        },
+        "cls" => {
+            cmd.arg("clean").arg("-s");
+        },
+        "ll" => {
+            cmd.arg("parallel");
+        },
+        other => {
+            cmd.arg(other);
+        },
+    }
+    cmd.args(args);
+    cmd.exec();
+    Ok(())
+}
