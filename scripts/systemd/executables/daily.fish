d341d0d3dc914472e43f6cb796e0cdb28ea90987
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-08 09:25:59 +0800

    sdt: daily nom refresh

--- scripts/systemd/executables/daily.fish
+++ scripts/systemd/executables/daily.fish
@@ -60,6 +60,8 @@ end
 
 loopuntil is_internet 0.5 0 60 # otherwise, as soon as I wake my laptop from sleep, it hasn't connected to wifi at that point, but *has* started executing this script. so what ends up happening is gpp fails to push all the directories because it doesn't have internet to do so yet.
 
+nom refresh
+
 for dir in ~/prog/forks/*
     git -C $dir fetch
     git -C $dir fetch upstream
