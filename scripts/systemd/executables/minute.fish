c53c31e05aa9ca5569f97bb41dcee485a243e040
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-08 08:57:40 +0800

    sdt: github notifications *overwrite* mag 4, so if you mark them as read on github, that is reflected in mag 4 in about a minute

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -6,5 +6,6 @@ widget_update disk_usage /dev/nvme0n1p2 Disk_usage
 pjs
 set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
 if test "$notifications"
-    task -m 4 $notifications
+    printf %s\n $notifications >~/.local/share/magazine/4
+    _magazine_commit ~/.local/share/magazine/4 task
 end
