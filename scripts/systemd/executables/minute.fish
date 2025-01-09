119ce2210c87b20e6dec3442e96cd124bcd3422f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-09 11:11:34 +0800

    sdt: mag 4 is truncated if no github notifications

--- scripts/systemd/executables/minute.fish
+++ scripts/systemd/executables/minute.fish
@@ -7,5 +7,7 @@ pjs
 set notifications (gh api notifications | jq -r '.[] | "\\(.repository.full_name): \\(.subject.title)"')
 if test "$notifications"
     printf %s\n $notifications >~/.local/share/magazine/4
-    _magazine_commit ~/.local/share/magazine/4 task
+else
+    truncate -s 0 ~/.local/share/magazine/4
 end
+_magazine_commit ~/.local/share/magazine/4 'gh notifs'
