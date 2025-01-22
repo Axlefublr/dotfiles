d2e3a7988ae8bb856f03379b1c56f4f9b72c94b6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-22 19:27:36 +0800

    calcure: ! marks orders

---
+++ stdext/target/rust-analyzer/deps/stdext-c08ee364e58a6106.d
@@ -0,0 +1,9 @@
+/home/axlefublr/prog/dotfiles/stdext/target/rust-analyzer/deps/libstdext-c08ee364e58a6106.rmeta: src/lib.rs Cargo.toml
+
+/home/axlefublr/prog/dotfiles/stdext/target/rust-analyzer/deps/stdext-c08ee364e58a6106.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
