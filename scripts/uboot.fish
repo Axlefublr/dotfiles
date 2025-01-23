b8c120581874f9d91949e622c0eab787902f7d05
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-01-24 02:59:35 +0800

    uboot: add eget and pipx updaters

--- scripts/uboot.fish
+++ scripts/uboot.fish
@@ -5,8 +5,14 @@ for package in (cat ~/.local/share/magazine/C)
 end
 truncate -s 0 ~/.local/share/magazine/C
 
+for package in (cat ~/.local/share/magazine/W)
+    eget $package
+end
+
 rustup update
 cargo install-update -a
+
+pipx upgrade-all
 
 cat ~/prog/info/pswds/sudo | sudo -Sv
 yes | sudo pacman -Syyu
