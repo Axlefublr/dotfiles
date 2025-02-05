e1146a52ec1c8e70b57d84f6db8cbaba881f13ae
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-05 21:08:57 +0800

    awm: remove config

--- awesome/global.lua
+++
@@ -1,7 +0,0 @@
----@param side 'latest'|'oldest'
-function env.destroy_sided_notification(side)
-	local notification_list = naughty.notifications[screen.primary].bottom_right
-	local side_notification = notification_list[side == 'oldest' and 1 or #notification_list]
-	if not side_notification then return end
-	naughty.destroy(side_notification)
-end
