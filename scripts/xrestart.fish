20d646916dd49af8f562b2e5253fa6502d4adcce
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-05 21:19:05 +0800

    xremap: remove xrestart script

--- scripts/xrestart.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-if pidof xremap &>/dev/null
-    killall xremap
-end
-xremap --mouse ~/prog/dotfiles/xremap.yml &>/tmp/log/xremap.txt & disown
-pueue add -d '2 seconds' -- 'xset r rate 135 40 ; setxkbmap -layout us,ru'
