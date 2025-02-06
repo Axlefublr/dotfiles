54c7ee7d4e6c72174f8b35743c90bb2816503912
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-06 19:41:36 +0800

    fix cursor theme configuration

--- scripts/setup/dode.fish
+++ scripts/setup/dode.fish
@@ -118,6 +118,16 @@ end
 
 function dode-cowsay
     paccy cowsay
+
+function dode-cursors
+    # Lighted Pixel Butter — https://www.gnome-look.org/p/2103612
+    sudo cp -fr ~/auto/cursors/Lighted-Pixel-Butter /usr/share/icons
+    # this is only needed because firefox is stupid and reverts to Adwaita for some reason
+    sudo ln -sn /usr/share/icons/Lighted-Pixel-Butter /usr/share/icons/mine
+    sudo cp -fr /usr/share/icons/Adwaita/cursors{,.bak}
+    for file in /usr/share/icons/Adwaita/cursors/*
+        sudo ln -sf /usr/share/icons/mine/cursors/(path basename $file) $file
+    end
 end
 
 function dode-display
