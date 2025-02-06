23e5f01c61fccbcada3bb9c02d13076674b7f289
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-06 19:35:08 +0800

    remove top bar services

--- scripts/services/loago.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-while true
-    inotifywait -q -e close_write -e move_self ~/.local/share/loago/loago.json
-    filter_mature_tasks >~/.local/share/magazine/doublequote
-    # awesome-client 'Hunger_wu()'
-end
--- scripts/services/numbered.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-while true
-    inotifywait -qq -e close_write -e move_self ~/.local/share/magazine/(seq 1 9)
-    awesome-client 'Registers_wu()'
-end
