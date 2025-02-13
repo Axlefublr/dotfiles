3fe1fe3147ca6f938282ab76f4d95e9ffc142ddb
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-14 06:48:52 +0800

    wallpaper randomizer keeps 33% non-repeat

--- scripts/randomize-wallpaper.fish
+++ scripts/randomize-wallpaper.fish
@@ -2,11 +2,12 @@
 
 while true
     set -f decided_path (shuf -n 1 ~/.local/share/magazine/T)
-    if test (count (cat ~/.local/share/magazine/T)) -le 3
-        break
-    else if not contains $decided_path (cat ~/bs/recent-wallpapers)
+    set -l stored_count (count (cat ~/.local/share/magazine/T))
+    test $stored_count -eq 0 && exit 1
+    set -l desired_non_repeat (math "floor($stored_count / 3)")
+    if not contains $decided_path (cat ~/bs/recent-wallpapers)
         echo $decided_path >>~/bs/recent-wallpapers
-        tail -n 3 ~/bs/recent-wallpapers | sponge ~/bs/recent-wallpapers
+        tail -n $desired_non_repeat ~/bs/recent-wallpapers | sponge ~/bs/recent-wallpapers
         break
     end
 end
