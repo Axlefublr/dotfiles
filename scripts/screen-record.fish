e490692b1d1962f4d368f369ee721fe9080bada2
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-15 17:42:43 +0800

    screen record: compress output video

--- scripts/screen-record.fish
+++ scripts/screen-record.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
 
-wf-recorder -r 30 -yf ~/z.mp4
+wf-recorder -Dyf ~/.cache/mine/screen-recording-original.mkv
+ffmpeg -y -i ~/.cache/mine/screen-recording-original.mkv -c:v libx264 -preset slow -crf 21 -b:v 2M -maxrate 3M -bufsize 4M -c:a aac -b:a 96k -movflags +faststart ~/.cache/mine/screen-recording-compressed.mp4
+ln -sf ~/.cache/mine/screen-recording-compressed.mp4 ~/z.mp4
