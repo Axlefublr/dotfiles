4bcdadb96948160edc21b2ed155f4e417ec19f59
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-16 12:15:11 +0800

    rename directories in structure

--- scripts/setup/structure.fish
+++ scripts/setup/structure.fish
@@ -1,20 +1,15 @@
 #!/usr/bin/env fish
 
 cd ~
-mkdir -p ups pic vid prog bs t trans .local/bin .cache/mine
-git clone https://github.com/Axlefublr/deleted-bangers
-mv deleted-bangers mus
-git clone https://github.com/Axlefublr/autocommit
-mv autocommit auto
-
+mkdir -p w i v r trans .local/bin .cache/mine
 fish_add_path ~/.local/bin
+git clone https://github.com/Axlefublr/deleted-bangers m
+git clone https://github.com/Axlefublr/autocommit auto
 
 cd ~/i
-git clone https://github.com/Axlefublr/pictures-tree
-mv pictures-tree tree
+git clone https://github.com/Axlefublr/pictures-tree e
 git clone https://github.com/Axlefublr/twemoji-svg
-git clone https://github.com/Axlefublr/video-creation-tools
-mv video-creation-tools tools
+git clone https://github.com/Axlefublr/video-creation-tools tools
 
 cd ~/r
 mkdir -p forks proj stored the
@@ -38,15 +33,13 @@ cd ~/r/forks
 git clone https://github.com/Axlefublr/helix
 
 cd ~/.local/share
-mkdir -p harp loago mine
+mkdir -p harp loago
 cp -f ~/auto/harp.yml ~/.local/share/harp/harp.yml
 cp -f ~/auto/loago.json ~/.local/share/loago/loago.json
 cp -f ~/auto/axleizer_invalid.json ~/.local/share/axleizer_invalid.json
 git clone https://github.com/Axlefublr/magazine
-git clone https://github.com/Axlefublr/music
-mv music alien_temple
-git clone https://github.com/Axlefublr/shows
-mv shows glaza
+git clone https://github.com/Axlefublr/music alien_temple
+git clone https://github.com/Axlefublr/shows glaza
 
 cd ~/.cache
 mkdir -p wks
