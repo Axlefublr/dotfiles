eccdd89b13e48c48446239cc6679c8b51650d2fb
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-17 15:08:41 +0800

    screen record: original is mp4

--- scripts/screen-record.fish
+++ scripts/screen-record.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-wf-recorder -Dyf ~/i/s/original.mkv
-ffmpeg -y -i ~/i/s/original.mkv -c:v libx264 -preset slow -crf 21 -b:v 2M -maxrate 3M -bufsize 4M -c:a aac -b:a 96k -movflags +faststart ~/i/s/compressed.mp4
+wf-recorder -Dyf ~/i/s/original.mp4
+ffmpeg -y -i ~/i/s/original.mp4 -c:v libx264 -preset slow -crf 21 -b:v 2M -maxrate 3M -bufsize 4M -c:a aac -b:a 96k -movflags +faststart ~/i/s/compressed.mp4
