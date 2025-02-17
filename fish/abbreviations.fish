4b2bb8fb7876277a64f9be3546586a6b5e66cccd
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-17 15:42:38 +0800

    fish: change swayimg abbreviations to d

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -7,6 +7,9 @@ abbr -a c lazygit
 abbr -a ca cargo
 abbr -a chx 'chmod +x'
 abbr -a cp 'rsync -r'
+abbr -a d swayimg
+abbr -a dq 'swayimg -g ; kitten @ close-window'
+abbr -a ds swayimg -g
 abbr -a e helix
 abbr -a ff ffmpeg
 abbr -a ffi 'ffmpeg -i'
@@ -26,8 +29,6 @@ abbr -a qrtool qrtool.rs
 abbr -a read 'read -p rdp' # can only be an abbreviation because `read` is a builtin
 abbr -a rm trash-put
 abbr -a rmf 'rm -fr'
-abbr -a sim swayimg
-abbr -a sime 'swayimg -g ; kitten @ close-window'
 abbr -a trr trash-restore
 abbr -a u pueue
 abbr -a v yazi-cd
