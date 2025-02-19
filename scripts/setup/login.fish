ed9898ab3101d47a69c70ef2077e3e147913550f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-19 08:54:31 +0800

    login: pueue is restarted immediately

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -2,13 +2,10 @@
 
 # pueue add -g s 'RUST_LOG=debug axleizer'
 
+pueue restart -g s
+playerctld daemon
 xremap ~/r/dot/xremap.yml & disown
 wlr-randr --output HDMI-A-1 --mode 1920x1080@75
 
 kitty -d ~/r/dot & disown
 firefox & disown
-
-playerctld daemon
-
-sleep 10
-pueue restart -g s
