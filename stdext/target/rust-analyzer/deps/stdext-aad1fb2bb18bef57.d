8a658bc2e8fda0d293d97361045833413f0c104e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-02-27 00:03:25 +0800

    stdext: remove what should've been gitignored

--- stdext/target/rust-analyzer/deps/stdext-aad1fb2bb18bef57.d
+++
@@ -1,9 +0,0 @@
-/home/axlefublr/prog/dotfiles/stdext/target/rust-analyzer/deps/libstdext-aad1fb2bb18bef57.rmeta: src/lib.rs Cargo.toml
-
-/home/axlefublr/prog/dotfiles/stdext/target/rust-analyzer/deps/stdext-aad1fb2bb18bef57.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
