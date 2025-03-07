848f53b99b9f7c9635471025c8cb5858ccf7dffe
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-03-07 20:42:20 +0800

    don't repeat last half of the wallpapers

--- scripts/randomize-wallpaper.fish
+++ scripts/randomize-wallpaper.fish
@@ -4,7 +4,7 @@ while true
     set -f decided_path (shuf -n 1 ~/.local/share/magazine/T)
     set -l stored_count (count (cat ~/.local/share/magazine/T))
     test $stored_count -eq 0 && exit 1
-    set -l desired_non_repeat (math "floor($stored_count / 3)")
+    set -l desired_non_repeat (math "floor($stored_count / 2)")
     if not contains $decided_path (cat ~/.cache/mine/recent-wallpapers)
         echo $decided_path >>~/.cache/mine/recent-wallpapers
         tail -n $desired_non_repeat ~/.cache/mine/recent-wallpapers | sponge ~/.cache/mine/recent-wallpapers
