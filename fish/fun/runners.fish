94d8057e062188419c1bef6554378c7f73f21829
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-03-17 20:06:15 +0800

    xremap: #u writes into clipboard directly, #!u appends onto it (with fuzzel)

--- fish/fun/runners.fish
+++ fish/fun/runners.fish
@@ -19,6 +19,32 @@ function runner
 end
 funcsave runner >/dev/null
 
+function runner_clipboard
+    set result (get-input)
+    test $status -ne 0 && return 1
+    test "$result" || return 1
+    echo $result | copy
+end
+funcsave runner_clipboard >/dev/null
+
+function runner_clipboard_append
+    set result (get-input)
+    test $status -ne 0 && return 1
+    test "$result" || return 1
+    begin
+        ypocn
+        echo $result
+    end | copyn -f
+end
+funcsave runner_clipboard_append >/dev/null
+
+function runner_interactive_unicode
+    set input (cat ~/.local/share/magazine/e | fuzzel -d --cache ~/.cache/mine/runner-interactive-unicode 2>/dev/null)
+    test $status -ne 0 && return 1
+    echo (string split ' ' $input[1])[1] | copy
+end
+funcsave runner_interactive_unicode >/dev/null
+
 function runner_kill
     set selected (ps -eo pid,command | zat.rs - 2.. | string trim --left | fuzzel -d 2>/dev/null)
     for line in $selected
@@ -26,6 +52,22 @@ function runner_kill
     end
 end
 funcsave runner_kill >/dev/null
+
+function runner_link
+    set file ~/.local/share/magazine/l
+    set result (cat $file | sd ' — .+$' '' | fuzzel -d --index --cache ~/.cache/mine/runner-link 2>/dev/null)
+    test $status -ne 0 && return 1
+    set line (math $result + 1)
+    set link (awk "NR==$line { print \$NF }" $file)
+    if test "$argv[1]"
+        $BROWSER $link
+        ensure_browser
+    else
+        echo $link | copy
+        notify-send -t 2000 "copied link: $link"
+    end
+end
+funcsave runner_link >/dev/null
 
 function runner_math
     footclient -N qalc
@@ -37,6 +79,23 @@ function runner_math
     # echo | qalc --set 'save mode yes'
 end
 funcsave runner_math >/dev/null
+
+function runner_notification
+    set -f do_it_again true
+    while $do_it_again
+        set do_it_again false
+        set input (fuzzel -dl 0 2>/dev/null)
+        set -l result $status
+        if test $result -eq 10
+            set do_it_again true
+        else if test $result -ne 0
+            return 1
+        end
+        notify-send -t 0 -- "$input"
+        echo "$input" | copy
+    end
+end
+funcsave runner_notification >/dev/null
 
 function runner_symbol
     set input (fuzzel -dl 0 2>/dev/null)
@@ -57,43 +116,3 @@ function runner_symbol_name
     printf '\U'(string pad --char 0 --width 8 (string split ' ' $input)[1]) 2>/dev/null | copy
 end
 funcsave runner_symbol_name >/dev/null
-
-function runner_link
-    set file ~/.local/share/magazine/l
-    set result (cat $file | sd ' — .+$' '' | fuzzel -d --index --cache ~/.cache/mine/runner-link 2>/dev/null)
-    test $status -ne 0 && return 1
-    set line (math $result + 1)
-    set link (awk "NR==$line { print \$NF }" $file)
-    if test "$argv[1]"
-        $BROWSER $link
-        ensure_browser
-    else
-        echo $link | copy
-        notify-send -t 2000 "copied link: $link"
-    end
-end
-funcsave runner_link >/dev/null
-
-function runner_notification
-    set -f do_it_again true
-    while $do_it_again
-        set do_it_again false
-        set input (fuzzel -dl 0 2>/dev/null)
-        set -l result $status
-        if test $result -eq 10
-            set do_it_again true
-        else if test $result -ne 0
-            return 1
-        end
-        notify-send -t 0 -- "$input"
-        echo "$input" | copy
-    end
-end
-funcsave runner_notification >/dev/null
-
-function runner_interactive_unicode
-    set input (cat ~/.local/share/magazine/e | fuzzel -d --cache ~/.cache/mine/runner-interactive-unicode 2>/dev/null)
-    test $status -ne 0 && return 1
-    echo (string split ' ' $input[1])[1] | copy
-end
-funcsave runner_interactive_unicode >/dev/null
