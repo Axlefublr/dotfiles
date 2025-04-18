4b96576d7d24c6004250ac2f073048ed1f745663
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-04-18 22:49:17 +0800

    switch to 100hz

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -2,7 +2,7 @@
 
 pueue restart -g s
 playerctld daemon
-wlr-randr --output HDMI-A-1 --mode 1920x1080@75
+wlr-randr --output HDMI-A-1 --mode 1920x1080@100.003998
 
 floorp & disown
 footclient -D ~/r/dot & disown
