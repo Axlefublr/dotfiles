7004f9e127da3655f260f9f6ffd6c3f589fc1ebd
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-08-17 08:13:05 +0800

    niri: configure the right output

--- scripts/setup/login.fish
+++ scripts/setup/login.fish
@@ -3,7 +3,7 @@
 systemctl --user restart kanata.service
 fcf.nu fix
 playerctld daemon
-wlr-randr --output HDMI-A-1 --mode 1920x1080@100.003998
+# wlr-randr --output HDMI-A-1 --mode 1920x1080@100.003998
 
 foot -D ~/fes/dot & disown
 gtk-launch floorp
