53c5c0628936425efbf4a8475d9e381583728330
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-08-25 03:36:08 +0800

    change helix/generator.py

--- helix/generator.py
+++ helix/generator.py
@@ -323,7 +323,7 @@ normal_select_mappings: dict[str, Any] = {
     'V': 'increment',
     'W': 'repeat_last_motion',
     'X': 'join_selections_space',
-    'Z': '@c<ret><esc>',
+    'Z': ':pipe $in + "\\n"',
     '[': 'shell_insert_output',
     '\\': ':sort',
     ']': 'shell_append_output',
