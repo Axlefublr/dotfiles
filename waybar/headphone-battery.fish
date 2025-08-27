c03eaed95ebb0f87ba8ad9c59f6636b806266841
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-08-28 02:48:02 +0800

    change waybar/headphone-battery.fish

--- waybar/headphone-battery.fish
+++ waybar/headphone-battery.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
 # mac codes discovered via `bluetoothctl` → `devices Connected`
-set -l output (busctl get-property org.bluez /org/bluez/hci0/dev_F4_9D_8A_18_05_B5 org.bluez.Battery1 Percentage 2>/dev/null || busctl get-property org.bluez /org/bluez/hci0/dev_68_D6_ED_18_9A_56 org.bluez.Battery1 Percentage 2>/dev/null)
-echo -n $output | cut -d ' ' -f 2
+begin
+    busctl get-property org.bluez /org/bluez/hci0/dev_F4_9D_8A_18_05_B5 org.bluez.Battery1 Percentage
+    or busctl get-property org.bluez /org/bluez/hci0/dev_68_D6_ED_18_9A_56 org.bluez.Battery1 Percentage
+end | cut -d ' ' -f 2
