fac10f2cb824d0a9fdbe5627f2038b16e4db4dd0
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-08-30 12:03:29 +0800

    waybar: move statusline to mag 0

--- waybar/statusline.fish
+++ waybar/statusline.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
 while true
-    if test -s ~/.local/share/magazine/9
-        cat ~/.local/share/magazine/9
+    if test -s ~/.local/share/magazine/0
+        cat ~/.local/share/magazine/0
     else
         echo
     end
-    inotifywait -qq -e close_write -e move_self ~/.local/share/magazine/9
+    inotifywait -qq -e close_write -e move_self ~/.local/share/magazine/0
 end
