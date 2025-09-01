62c7c2b748ddb95b3a7d39b8a8560b639d53bf1b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-09-01 13:24:19 +0800

    more direct ffmpeg command in screen-record

--- eli/screen-record.fish
+++ eli/screen-record.fish
@@ -4,7 +4,12 @@ touch /tmp/mine/recordilock
 echo recording >~/.local/share/mine/waybar-screen-record
 wf-recorder -Dyf ~/iwm/sco/original.mp4
 echo compressing >~/.local/share/mine/waybar-screen-record
-ffmpeg_compress ~/iwm/sco/original.mp4 ~/iwm/sco/compressed.mp4 -preset slow -crf 21 -b:v 2M -maxrate 3M -bufsize 4M -b:a 96k
+ffmpeg -y -i ~/iwm/sco/original.mp4 \
+    -an \
+    -c:v libx264 -preset medium -crf 21 \
+    -movflags +faststart \
+    -b:v 3M -maxrate 4.5M -bufsize 6M \
+    ~/iwm/sco/compressed.mp4
 rm -f /tmp/mine/recordilock
 echo copying >~/.local/share/mine/waybar-screen-record
 sl.fish ~/iwm/sco/compressed.mp4
