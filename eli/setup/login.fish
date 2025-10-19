b496823a3657dcc2a6e743731f5f494b83b56f50
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-10-20 01:30:05 +0800

    change eli/setup/login.fish

--- eli/setup/login.fish
+++ eli/setup/login.fish
@@ -4,7 +4,6 @@ cat ~/fes/uviw/afen/sudo | sudo -S modprobe uinput
 systemctl --user restart kanata.service
 fcf.nu fix
 playerctld daemon
-# wlr-randr --output HDMI-A-1 --mode 1920x1080@100.003998
 
 foot -D ~/fes/dot & disown
 gtk-launch floorp
