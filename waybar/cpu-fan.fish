7050cc7db6dfa7324437fd728e59ac6478c8d7e5
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-10-20 23:57:03 +0800

    change waybar/cpu-fan.fish

--- waybar/cpu-fan.fish
+++ waybar/cpu-fan.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
-math "round($(sensors -j | jq -- '."it8620-isa-0a30"."fan1"."fan1_input"') / 50) * 50"
+set -l the (math "round($(sensors -j | jq -- '."it8620-isa-0a30"."fan1"."fan1_input"') / 50) * 50")
+test "$the" = 0 && echo || echo $the
