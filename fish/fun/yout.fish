015aed2d8319c760e0bced79bae3597e2782bc7c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-11-18 05:30:21 +0800

    refactor youtube downloader

--- fish/fun/yout.fish
+++ fish/fun/yout.fish
@@ -1,38 +1,27 @@
 #!/usr/bin/env fish
 
-function batch-link-downloader
-    set -l extra (begin
-        echo youtube
-        echo longform
-        echo asmr
-    end | fuzzel -d 2>/dev/null)
-    test $status -ne 0 && return 1
+function yteet_clipboard
     set -l links (wl-paste -n | sttr extract-url)
     if not test "$links"
         notify-send -t 2000 'no copied links'
         return 1
     end
     for link in $links
-        pueue add -g k -- "install-yt-video $extra $link"
+        pueue add -g network -- "yteet $link"
+        notify-send "start $link"
     end
 end
-funcsave batch-link-downloader >/dev/null
+funcsave yteet_clipboard >/dev/null
 
-function install-yt-video
-    if not test "$argv[1..2]"
-        return 1
-    end
-
-    set -l extra $argv[1]
-    set -l link $argv[2]
-
-    yt ~/iwm/voe/$extra $link
+function yteet
+    not test "$argv" && return 1
+    yt ~/iwm/voe/youtube $argv
 end
-funcsave install-yt-video >/dev/null
+funcsave yteet >/dev/null
 
 function yt
     yt-dlp \
-        --proxy (cat ~/.local/share/magazine/p)[1] \
+        --proxy http://127.0.0.1:8118 \
         -o "$argv[1]/%(channel)s — %(title)s — ;%(id)s;.%(ext)s" \
         $argv[2..]
 end
