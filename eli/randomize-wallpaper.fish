c0f2b2eaa54922eb2a79fa16e04f279c5f46765b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-11-18 05:30:26 +0800

    change eli/randomize-wallpaper.fish

--- eli/randomize-wallpaper.fish
+++ eli/randomize-wallpaper.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-wpchange ~/iwm/osl/kavu/(ls ~/iwm/osl/kavu | propose.rs wallpaper 70% | tee -a /tmp/mine/recent-wallpapers)
+wpchange ~/iwm/osl/kavu/(ls ~/iwm/osl/kavu | propose.rs wallpaper 70%)
