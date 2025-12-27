064c81d18f756aa7f0cebec9bdcaafa96b1246c3
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-12-27 09:37:25 +0800

    change fish/fun/bindings.fish

--- fish/fun/bindings.fish
+++ fish/fun/bindings.fish
@@ -1,23 +1,5 @@
 #!/usr/bin/env fish
 
-function _travel_helix_cwd
-    z (cat ~/.cache/mine/helix-cwd-suspend | path_expand)
-    commandline -f repaint
-end
-funcsave _travel_helix_cwd >/dev/null
-
-function _travel_yazi_cwd
-    z (cat ~/.cache/mine/yazi-cwd-suspend)
-    commandline -f repaint
-end
-funcsave _travel_yazi_cwd >/dev/null
-
-function _travel_buffer_head
-    z (cat ~/.cache/mine/helix-buffer-head-suspend | path_expand)
-    commandline -f repaint
-end
-funcsave _travel_buffer_head >/dev/null
-
 function _execute_via_pueue
     set -l cmd (commandline)
     history append -- $cmd
@@ -37,27 +19,58 @@ function _reexec
 end
 funcsave _reexec >/dev/null
 
+function _kb_follow_by
+    set -l cmd "$(commandline)"
+    if not test "$(string trim $cmd)"
+        $argv
+        return
+    end
+    history append -- $cmd
+    commandline " $cmd && $argv"
+    commandline -f execute
+end
+funcsave _kb_follow_by >/dev/null
+
+function _kb_follow_by_lazygit
+    set -l cmd "$(commandline)"
+    if not test "$(string trim $cmd)"
+        commandline lazygit
+        commandline -f execute
+        return
+    end
+    history append -- $cmd
+    commandline " $cmd && lazygit"
+    commandline -f execute
+end
+funcsave _kb_follow_by_lazygit >/dev/null
+
+function _kb_and_exit
+    set -l cmd "$(commandline)"
+    not test "$(string trim $cmd)" && return
+    history append -- $cmd
+    commandline " $cmd"\n'and exit'
+    commandline -f execute
+end
+funcsave _kb_and_exit >/dev/null
+
 function fish_user_key_bindings
     # [[sort on]]
     bind / expand-abbr self-insert
     bind alt-d _blammo_pwd
     bind alt-enter expand-abbr insert-line-under
-    bind ctrl-1 _travel_buffer_head
-    bind ctrl-3 _travel_yazi_cwd
-    bind ctrl-5 _travel_helix_cwd
     bind ctrl-e _reexec
     bind ctrl-i edit_command_buffer
-    bind ctrl-l 'commandline -f clear-screen'
-    bind ctrl-o _execute_via_pueue
-    bind ctrl-s 'commandline -f repaint'
+    bind ctrl-l clear-screen
+    bind ctrl-o _kb_and_exit
+    bind ctrl-s repaint
     bind ctrl-space 'commandline -i " "'
-    bind ctrl-z d
-    bind f1 'commandline nu ; commandline -f execute'
-    bind f11 'commandline -f execute and exit'
-    bind f2 'commandline lazygit ; commandline -f execute'
-    bind f3 yazi_cd
-    bind f5 'test "$(commandline)" = " " && helix . || helix'
-    bind f6 'swayimg -g & disown ; exit'
+    bind ctrl-z fg
+    bind f1 '_kb_follow_by nu'
+    bind f2 _kb_follow_by_lazygit
+    bind f3 'z (cat ~/.cache/mine/blammo | path dirname) ; commandline -f repaint'
+    bind f4 '_kb_follow_by helix .'
+    bind f5 '_kb_follow_by helix'
+    bind f6 '_kb_follow_by yazi'
     # [[sort off]]
 end
 funcsave fish_user_key_bindings >/dev/null
