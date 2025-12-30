316d8acd2cec08fb77a93d5b544531f7d7b2458c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-12-30 19:52:52 +0800

    change fish/fun/fool.fish

--- fish/fun/fool.fish
+++ fish/fun/fool.fish
@@ -1,5 +1,25 @@
 #!/usr/bin/env fish
 
+# [[sort on]]
+alias --save dust 'dust -r' >/dev/null
+alias --save eza 'eza --icons=auto --group-directories-first -x --time-style "+%y.%m.%d %H:%M" --smart-group --git --git-repos' >/dev/null
+alias --save fd 'fd --no-require-git' >/dev/null
+alias --save icat 'kitten icat --align left' >/dev/null
+alias --save jpeg2png 'jpeg2png -i 100 -w 1.0 -f' >/dev/null
+alias --save less 'less --use-color -R' >/dev/null
+alias --save octogit-set "octogit --color-all-commits ffd75f --color-all-staged 87ff5f --color-all-unstaged 00d7ff" >/dev/null
+alias --save pipes 'pipes -p 3 -c 1 -c 2 -c 3 -c 4 -c 5 -R' >/dev/null
+alias --save pv 'pv -g' >/dev/null
+alias --save qalc 'qalc -s "vspace off" -s "curconv off" -s "update exchange rates 1" -s "decimal comma off" -s "autocalc"' >/dev/null
+alias --save rg 'rg --engine auto' >/dev/null
+alias --save rsync 'rsync --mkpath -P' >/dev/null
+alias --save termdown 'termdown -W -f roman' >/dev/null
+alias --save termframe 'termframe -o ~/iwm/sco/(date +%Y.%m.%d-%H:%M:%S).svg' >/dev/null
+alias --save tree 'tree --noreport --dirsfirst --matchdirs --gitignore -Ca -I .git -I bin -I obj -I target -I .vscode' >/dev/null
+alias --save tuisky 'tuisky -c ~/fes/dot/tuisky.toml' >/dev/null
+alias --save unimatrix 'unimatrix -s 95 -abf' >/dev/null
+# [[sort off]]
+
 function loago
     command loago $argv
     test "$argv[1]" = --help && return
