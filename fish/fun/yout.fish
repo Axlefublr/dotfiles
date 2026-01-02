3724ced40247e836fc22a84f317cd802edde8589
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2026-01-03 05:01:21 +0800

    change fish/fun/yout.fish

--- fish/fun/yout.fish
+++ fish/fun/yout.fish
@@ -13,6 +13,25 @@ function yteet_clipboard
 end
 funcsave yteet_clipboard >/dev/null
 
+function yt_audio
+    set -l picked (begin
+        echo ad
+        echo bd
+        echo cd
+    end | fuzzel -dl 3)
+    test "$picked" || return 1
+    set -l links (wl-paste -n | sttr extract-url)
+    if not test "$links"
+        notify-send -t 2000 'no copied links'
+        return 1
+    end
+    for link in $links
+        pueue add -g network -- "ytroxy -o ~/iwm/lwkc/$picked/$(uclanr -j - 3).'%(ext)s' $link"
+        notify-send "start $link"
+    end
+end
+funcsave yt_audio >/dev/null
+
 function yteet
     not test "$argv" && return 1
     yt ~/iwm/voe/youtube $argv
@@ -20,9 +39,10 @@ end
 funcsave yteet >/dev/null
 
 function yt
-    yt-dlp \
-        --proxy http://127.0.0.1:8118 \
+    ytroxy \
         -o "$argv[1]/%(channel)s — %(title)s — ;%(id)s;.%(ext)s" \
         $argv[2..]
 end
 funcsave yt >/dev/null
+
+alias --save ytroxy 'yt-dlp --proxy http://127.0.0.1:8118' >/dev/null
