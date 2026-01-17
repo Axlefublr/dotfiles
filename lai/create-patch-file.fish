b67c689f3882661831da54e3725c48bf1039c80f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2026-01-17 17:08:16 +0800

    change lai/create-patch-file.fish

--- lai/create-patch-file.fish
+++ lai/create-patch-file.fish
@@ -11,8 +11,8 @@ if not path extension $patch_name
     set patch_name $patch_name.patch
 end
 set -l full_patch_path $parent_dir/$patch_name
-if test -f $full_patch_path
-    git apply --3way $full_patch_path
-else
-    wl-paste -n >$full_patch_path
-end
+# if test -f $full_patch_path
+#     git apply --3way $full_patch_path
+# else
+wl-paste -n >$full_patch_path
+# end
