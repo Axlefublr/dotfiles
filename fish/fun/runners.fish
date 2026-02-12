8159f1fdccbadfa4dc5925ec98844242601201d8
Axlefublr <axlefublr.ls@gmail.com>
2026-02-13 05:01:08 +0800

    change fish/fun/runners.fish

--- fish/fun/runners.fish
+++ fish/fun/runners.fish
@@ -39,7 +39,7 @@ function runner_math
     # set -l calculated_result (qalc -t -- $input_expr | tee -a ~/.cache/mine/runner-math)
     # notify-send -t 0 -- "$calculated_result"
     # echo $calculated_result | wl-copy -n
-    footclient -NT floating-calculator qalc.fish
+    footclient -NT floating-calculator calc
 end
 funcsave runner_math >/dev/null
 
