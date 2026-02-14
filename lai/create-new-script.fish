816e2d108066cdd09036f6d2d80a986cac062ce4
Axlefublr <axlefublr.ls@gmail.com>
2026-02-15 05:01:09 +0800

    change lai/create-new-script.fish

--- lai/create-new-script.fish
+++ lai/create-new-script.fish
@@ -4,12 +4,12 @@ fuzzel -dl 0 </dev/null | read -l input
 test "$input" || return 1
 set -l file "$argv[1]/$input"
 switch (path extension -- $input)
-    case .dash .sh '*'
-        echo -e "#!/usr/bin/env dash\n\n" >$file
     case .fish
         echo -e "#!/usr/bin/env fish\n\n" >$file
     case .nu
         echo -e "#!/usr/bin/env -S nu --no-std-lib -n\n\n" >$file
+    case .dash .sh '*'
+        echo -e "#!/usr/bin/env dash\n\n" >$file
 end
 chmod +x $file
 echo -n $file:3
