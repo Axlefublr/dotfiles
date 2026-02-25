9160037bb33fd7cd22f96aca83b260129d0c64d1
Axlefublr <axlefublr.ls@gmail.com>
2026-02-25 12:53:05 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -3,8 +3,12 @@
 fcf.nu fix
 cat ~/fes/uviw/afen/sudo | sudo -S modprobe uinput
 systemctl --user restart kanata.service
-playerctld daemon
 
 niri msg action focus-workspace one
 foot -ND ~/fes/dot
 gtk-launch firefox
+playerctld daemon
+foot -NT abilities abilities.fish
+foot -NT loago-tracker fish -c loago_tracker
+foot -NT calcure calcure
+gtk-launch anki
