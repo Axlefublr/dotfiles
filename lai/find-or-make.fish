db3afa0d8f725152b60d1905a668b0e79f557170
Axlefublr <axlefublr.ls@gmail.com>
2026-03-01 12:25:32 +0800

    change lai/find-or-make.fish

--- lai/find-or-make.fish
+++ lai/find-or-make.fish
@@ -4,13 +4,17 @@ set -l wheres
 for arg in $argv[..-2]
     set -a wheres "| where $arg"
 end
-na -c "niri msg -j windows
+na -c "
+    let found = niri msg -j windows
     | from json
     $wheres
-    | get id
-    | first
-    | try {
-        niri msg action focus-window --id \$in
-    } catch {
+    if (\$found | is-not-empty) {
+    	if (\$found | any { get is_focused }) {
+    		niri msg action focus-window-previous
+    	} else {
+    		\$found | first | each { niri msg action focus-window --id (\$in | get id) }
+    	}
+    } else {
         niri msg action spawn -- $argv[-1]
-}"
+    }
+"
