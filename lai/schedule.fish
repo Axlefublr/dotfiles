797f24d1a549ab1180366be4f835ccb7ef6f7a08
Axlefublr <axlefublr.ls@gmail.com>
2026-03-07 11:20:43 +0800

    add lai/schedule.fish

---
+++ lai/schedule.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+argparse D= -- $argv
+
+begin
+    if set -q _flag_D
+        echo -n $_flag_D
+        echo -ne '\0'
+    else
+        echo -n $PWD
+        echo -ne '\0'
+    end
+
+    echo -n "$argv"
+    echo -ne '\0'
+end >~/.local/share/mine/task-scheduler-intermediary
