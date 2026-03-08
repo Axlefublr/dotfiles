4509d9360ee6000db10faf53c2b580ab240396b0
Axlefublr <axlefublr.ls@gmail.com>
2026-03-08 11:52:21 +0800

    remove lai/intermediary.fish

--- lai/intermediary.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-mkfifo ~/.local/share/mine/task-scheduler
-mkfifo ~/.local/share/mine/task-scheduler-intermediary
-tail -f ~/.local/share/mine/task-scheduler-intermediary >~/.local/share/mine/task-scheduler
