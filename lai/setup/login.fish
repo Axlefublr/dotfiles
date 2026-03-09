72733e4093cb07721614fef06d4e785e03bb0d62
Axlefublr <axlefublr.ls@gmail.com>
2026-03-09 11:43:52 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -6,9 +6,11 @@ systemctl --user restart kanata.service
 
 niri msg action focus-workspace one
 foot -T toggleterm & disown
+sleep 10ms
 foot -T dotfiles -D ~/fes/dot helix & disown
 gtk-launch firefox
 playerctld daemon
+footclient -NT finances helix ~/ake/finances
 footclient -NT abilities abilities.fish
 footclient -NT loago-tracker fish -c loago_tracker
 footclient -NT calcure calcure
