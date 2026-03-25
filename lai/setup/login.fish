2e65ab4de369285909c512f169ea3c4748dc2128
Axlefublr <axlefublr.ls@gmail.com>
2026-03-25 10:53:03 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -5,16 +5,16 @@ cat ~/fes/uviw/afen/sudo | sudo -S modprobe uinput
 systemctl --user restart kanata.service
 
 niri msg action focus-workspace main
-foot -T toggleterm & disown
-sleep 10ms
-foot -T dotfiles -D ~/fes/dot helix & disown
 gtk-launch firefox
-playerctld daemon
+footclient -NT toggleterm
+footclient -NT dotfiles -D ~/fes/dot helix
+niri msg action move-column-to-last
 footclient -NT finances helix ~/ake/finances
 footclient -NT earnings earnings.fish
 footclient -NT abilities abilities.fish
 footclient -NT loago-tracker fish -c loago_tracker
 footclient -NT calcure calcure
-footclient -NT voe -D ~/iwm/voe yazi
 footclient -NT receiver receiver.fish
 gtk-launch anki
+playerctld daemon
+footclient -NT voe -D ~/iwm/voe yazi
