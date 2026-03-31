e7eb1af56ced9216599f616892034ac4f5d5e9a8
Axlefublr <axlefublr.ls@gmail.com>
2026-03-31 08:55:47 +0800

    change lai/screen-record.fish

--- lai/screen-record.fish
+++ lai/screen-record.fish
@@ -2,16 +2,17 @@
 
 touch /tmp/mine/recordilock
 echo recording >~/.local/share/mine/waybar-screen-record
-wf-recorder -Dyf ~/iwm/sco/original.mp4
+set -l timestamp (date +%Y.%m.%d-%H:%M:%S)
+wf-recorder -Dyf ~/iwm/sco/$timestamp-original.mp4
 echo compressing >~/.local/share/mine/waybar-screen-record
-ffmpeg -y -i ~/iwm/sco/original.mp4 \
+ffmpeg -y -i ~/iwm/sco/$timestamp-original.mp4 \
     -an \
     -c:v libx264 -preset medium -crf 21 \
     -movflags +faststart \
     -b:v 3M -maxrate 4.5M -bufsize 6M \
     -vf scale=-1:1080 \
-    ~/iwm/sco/compressed.mp4
+    ~/iwm/sco/$timestamp-compressed.mp4
 rm -f /tmp/mine/recordilock
 echo copying >~/.local/share/mine/waybar-screen-record
-sl.fish ~/iwm/sco/compressed.mp4
+sl.fish ~/iwm/sco/$timestamp-compressed.mp4
 echo >~/.local/share/mine/waybar-screen-record
