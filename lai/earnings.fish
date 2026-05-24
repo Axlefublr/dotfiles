7102451df5c5f5dc1590db5b86057470e66bc0d6
Axlefublr <axlefublr.ls@gmail.com>
2026-05-24 10:02:12 +0800

    change lai/earnings.fish

--- lai/earnings.fish
+++ lai/earnings.fish
@@ -3,6 +3,10 @@
 while true
     clear
     finance.nu data
+    echo
+    desires.nu input (cat ~/iwm/nak/desire-input.txt) >>~/.local/share/magazine/V
+    truncate -s 0 ~/iwm/nak/desire-input.txt
+    desires.nu output | tee ~/iwm/nak/desire-output.txt
     ansi_hide_cursor
-    inotifytheusual ~/ake/finances
+    inotifytheusual -t 3600 ~/ake/finances ~/iwm/nak/desire-input.txt ~/.local/share/magazine/V
 end
