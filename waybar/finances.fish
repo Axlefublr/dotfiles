df596ff5fe170ee933c47d2fb5d67735e0c53ace
Axlefublr <axlefublr.ls@gmail.com>
2026-06-01 08:27:07 +0800

    change waybar/finances.fish

--- waybar/finances.fish
+++ waybar/finances.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 while true
-    na ~/.local/share/magazine/o.nu
-    inotifytheusual ~/.local/share/magazine/o
+    na ~/.local/share/magazine/o.nu | tee ~/iwm/nak/↓money.txt
+    inotifytheusual ~/.local/share/magazine/o ~/iwm/nak/↑money.txt
 end
