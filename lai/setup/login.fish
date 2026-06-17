02fa03967946ee94903f1fdc78510c5aaf68b967
Axlefublr <axlefublr.ls@gmail.com>
2026-06-17 11:43:39 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -6,14 +6,15 @@ systemctl --user restart kanata.service
 
 niri msg action focus-workspace main
 gtk-launch firefox
-footclient -NT toggleterm
-footclient -NT dotfiles -D ~/fes/dot helix
+foottitled.sh toggleterm -N
+foottitled.sh dotfiles -ND ~/fes/dot helix
 niri msg action move-column-to-last
-footclient -NT voe -D ~/iwm/voe yazi
-footclient -NT finances ov --status-line=false --follow-name ~/.local/share/magazine/J
-footclient -NT earnings earnings.fish
-footclient -NT loago-tracker fish -c loago_tracker
-footclient -NT receiver receiver.fish
+foottitled.sh voe -ND ~/iwm/voe yazi
+foottitled.sh finances -N ov --status-line=false --follow-name ~/.local/share/magazine/J
+foottitled.sh earnings -N earnings.fish
+foottitled.sh loago-tracker -N fish -c loago_tracker
+foottitled.sh receiver -N receiver.fish
 playerctld daemon
 gtk-launch anki
 niri msg action spawn -- Todoist.AppImage
+make-em-shut-up.nu
