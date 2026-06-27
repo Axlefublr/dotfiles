2873d41d98bc2002febba0dbde8cc23e73777a83
Axlefublr <axlefublr.ls@gmail.com>
2026-06-27 13:03:48 +0800

    change fish/fun/runners.fish

--- fish/fun/runners.fish
+++ fish/fun/runners.fish
@@ -85,10 +85,17 @@ function runner_timer
     not test "$input" && return 1
     set -l the (string split ' ' -- $input)
     set -l title $the[1]
+    if string match -qr '!$' -- $title
+        set -f repeat true
+    else
+        set -f repeat false
+    end
     set -l rest $the[2..]
     # doing some titling logic here so that window rules work as expected; window title may change afterwards
     set -l genre (runner_timer_resolve_genre "$the[2]")
-    foottitled.sh "$genre $title" -N timer.fish $title $rest
+    set -l prev_win_id (na -c "niri msg -j windows | from json | where is_focused == true | get id | first")
+    niri msg action do-screen-transition -d 110
+    niri msg action spawn -- foottitled.sh "$genre $title" -N timer.fish $repeat "$prev_win_id" $title $rest
     indeed.rs -u ~/.local/share/magazine/c-t -- "$input"
     tail -n 100 ~/.local/share/magazine/c-t | sponge ~/.local/share/magazine/c-t
 end
