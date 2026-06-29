3774c279fd0fa40872778415c8b7695062ff9030
Axlefublr <axlefublr.ls@gmail.com>
2026-06-29 10:25:12 +0800

    change waybar/mouse-mode.fish

--- waybar/mouse-mode.fish
+++ waybar/mouse-mode.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-not test -p ~/.local/share/mine/waybar-mouse-mode && mkfifo ~/.local/share/mine/waybar-mouse-mode
-tail -f ~/.local/share/mine/waybar-mouse-mode
+not test -p ~/fes/zufi/waybar-mouse-mode && mkfifo ~/fes/zufi/waybar-mouse-mode
+tail -f ~/fes/zufi/waybar-mouse-mode
