298562cbf6ba64100cccfb8729ad10faa7d25250
Axlefublr <axlefublr.ls@gmail.com>
2026-06-29 10:25:12 +0800

    change fish/fun/sdt.fish

--- fish/fun/sdt.fish
+++ fish/fun/sdt.fish
@@ -21,13 +21,13 @@ end
 funcsave systemd_wake >/dev/null
 
 function systemd_wallpaper
-    set -l last_ran (cat ~/.cache/mine/wallpaper-lockfile)
+    set -l last_ran (cat ~/fes/zufi/wallpaper-lockfile)
     set -l current_time (date +%s)
     if test "$last_ran" && test "$last_ran" -eq $current_time
         return
     end
     randomize-wallpaper.fish
-    echo $current_time >~/.cache/mine/wallpaper-lockfile
+    echo $current_time >~/fes/zufi/wallpaper-lockfile
 end
 funcsave systemd_wallpaper >/dev/null
 
