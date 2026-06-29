3c2b7e8a70af33883d337cd1b0292a67fcb8973b
Axlefublr <axlefublr.ls@gmail.com>
2026-06-29 10:25:12 +0800

    change yazi/plugins/blammo.yazi/main.lua

--- yazi/plugins/blammo.yazi/main.lua
+++ yazi/plugins/blammo.yazi/main.lua
@@ -8,7 +8,7 @@ return {
 		if #collector == 0 then
 			collector = tostring(cx.active.current.hovered.url) .. '\n'
 		end
-		local file = io.open('/home/axlefublr/.cache/mine/blammo', 'w')
+		local file = io.open('/home/axlefublr/fes/zufi/blammo', 'w')
 		if file then
 			file:write(collector)
 			file:close()
