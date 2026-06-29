5f01ed38881c767cb9933ca04998d1026a442d10
Axlefublr <axlefublr.ls@gmail.com>
2026-06-29 10:25:12 +0800

    change lai/receiver.fish

--- lai/receiver.fish
+++ lai/receiver.fish
@@ -1,9 +1,16 @@
 #!/usr/bin/env fish
 
-mkfifo ~/.local/share/mine/task-scheduler 2>/dev/null
+# make pressing ctrl+c only send it to the eventual `fish -c $second` process, not the entire script
+function _receive_sigint --on-signal INT
+    echo
+end
+rm -f ~/fes/zufi/tms/receiver-ongoing 2>/dev/null
+mkfifo ~/fes/zufi/task-scheduler 2>/dev/null
 set -l this_window_id (na -c 'niri msg -j windows | from json | where app_id starts-with foot | where title == "receiver" | get id | first')
-tail -f ~/.local/share/mine/task-scheduler | while read -z first
+# this bash wrapper (that *turns* into the direct tail process thanks to `exec`) protects `tail` from ctrl+c; without it, ctrl+c-ing `fish -c $second` *also* ctrl+c-s the tail, ending the listening loop, and requiring me to restart the receiver.fish window (and I think losing data?)
+bash -c 'trap "" INT ; exec tail -f ~/fes/zufi/task-scheduler' | while read -z first
     and read -z second
+    touch ~/fes/zufi/tms/receiver-ongoing
     if test $first != ~
         set_color -o b58cc6
         path_shrink $first
@@ -14,11 +21,8 @@ tail -f ~/.local/share/mine/task-scheduler | while read -z first
         echo $second
         set_color normal
         set -l first_word (string split ' ' $second)[1]
-        function _receive_sigint --on-signal INT
-        end
         fish -c $second </dev/tty
         set -l stored_status $status
-        functions -e _receive_sigint
         if test $stored_status -eq 0
             set_color -o a9b665
             echo success
@@ -27,17 +31,18 @@ tail -f ~/.local/share/mine/task-scheduler | while read -z first
             echo
             break
         end
-        set_color -o ea6962
+        set_color -o (test $stored_status -eq 130 && echo e49641 || echo ea6962)
         notify-send "❌ $first_word"
         niri msg action set-window-urgent --id $this_window_id
-        confirm.rs failure '[r]estart' '[e]dit' '[k]ancel' | read -l response
+        confirm.rs (test $stored_status -eq 130 && echo canceled || echo failure) '[r]estart' '[e]dit' 'pro[j]eed' | read -l response
         set_color normal
         echo
-        if test $response = k
+        if test $response = j
             break
         else if test $response = e
             set second (echo $second | vipe --suffix fish)
         end
     end
     cd ~
+    rm -f ~/fes/zufi/tms/receiver-ongoing 2>/dev/null
 end
