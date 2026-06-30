a1015b071a2324cf99f371521f2af9a65a873a5a
Axlefublr <axlefublr.ls@gmail.com>
2026-06-30 13:15:58 +0800

    change lai/fool/help.fish

--- lai/fool/help.fish
+++ lai/fool/help.fish
@@ -2,7 +2,9 @@
 
 set -gx COLUMNS 130
 set -l help_page "$argv"
-set -l path "/tmp/mine/$help_page.help"
+set -l dir ~/fes/zufi/men
+mkdir -p $dir
+set -l path $dir/$help_page.help
 $argv --help &>$path
 focus-if-exists.fish "app_id starts-with foot" "title == 'help $help_page'"
 or flour -T "help $help_page" --disown --man $path:14
