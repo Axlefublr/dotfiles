62e980138fbf2469d639fe67cc3fb8dc7ea1924d
Axlefublr <axlefublr.ls@gmail.com>
2026-06-30 13:15:58 +0800

    change lai/fool/man.fish

--- lai/fool/man.fish
+++ lai/fool/man.fish
@@ -2,7 +2,9 @@
 
 set -gx COLUMNS 130
 set -l manpage "$argv[..-3]($argv[-2])"
-set -l path /tmp/mine/$manpage.man
+set -l dir ~/fes/zufi/men
+mkdir -p $dir
+set -l path $dir/$manpage.man
 if $argv[-1]
     man $argv[..-3] 2>/dev/null >$path
 else
