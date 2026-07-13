6e6755bcf039e59451121df9ff3410cd4d57eb5d
Axlefublr <axlefublr.ls@gmail.com>
2026-07-13 11:48:05 +0800

    change waybar/meat.fish

--- waybar/meat.fish
+++ waybar/meat.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
 while true
-    loago list -m eat | string match -gr '(\\d+)h (\\d+)m' | string join ':'
+    set -l groups (loago list -m eat | string match -gr '(\\d+)h (\\d+)m')
+    set -l minutes (string pad -c 0 -w 2 $groups[2])
+    echo $groups[1]:$minutes
     inotifytheusual -t 60 ~/.local/share/loago/loago.json &>/dev/null
 end
