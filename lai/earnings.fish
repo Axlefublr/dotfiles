789cd989942b350efdb623531ad8d936f942a0f7
Axlefublr <axlefublr.ls@gmail.com>
2026-07-16 12:32:25 +0800

    change lai/earnings.fish

--- lai/earnings.fish
+++ lai/earnings.fish
@@ -3,6 +3,6 @@
 while true
     clear
     finance.nu data-wrapper
-    ansi_hide_cursor
+    ansi_cursor_hide
     inotifytheusual -t 3600 ~/.local/share/magazine/J ~/iwm/nak/↑earnings.txt ~/iwm/nak/↑desire.txt ~/.local/share/magazine/V
 end
