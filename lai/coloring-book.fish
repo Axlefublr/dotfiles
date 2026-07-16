91380f6db8e75eb4194ca6e03f4db874b86a7464
Axlefublr <axlefublr.ls@gmail.com>
2026-07-16 12:32:25 +0800

    change lai/coloring-book.fish

--- lai/coloring-book.fish
+++ lai/coloring-book.fish
@@ -2,7 +2,7 @@
 
 # set -l stored_colors ~/fes/dot/colors.nuon
 function input_color
-    set -l picked_color (coloring-book.nu list | fzf --height 50%)
+    set -l picked_color (coloring-book.nu list | fzf --height 50% -e)
     if string match -qe '│' -- "$picked_color"
         echo (string split -f 2 ' │ ' -- "$picked_color")
     else
@@ -11,7 +11,7 @@ function input_color
 end
 
 function input_name
-    set -l picked_name (coloring-book.nu list | fzf --height 50%)
+    set -l picked_name (coloring-book.nu list | fzf --height 50% -e)
     if string match -qe '│' -- "$picked_name"
         echo (string split -f 1 ' │ ' -- "$picked_name" | string trim)
     else
@@ -21,66 +21,109 @@ end
 
 function warnage
     set_color e49641
-    warn (string pad -rc ' ' -w $COLUMNS "$argv") # because we are probably overwriting the confirm.rs above
+    warn $argv
     set_color normal
 end
 
-function recolors
-    ansi_move_cursor_up (coloring-book.nu all count)
-    feline
-    ansi_move_cursor_up 2
+function reconfirm
+    ansi_erase_line
+    for the in (seq 1 1)
+        ansi_cursor_move_start_up
+        ansi_erase_line
+    end
 end
-
-alias feline 'string pad -rc " " -w $COLUMNS ""'
-alias reconfirm 'ansi_move_cursor_up 2'
 
 function act_on_color -a color
-    not test "$color" && return
-    pastel color $color
     while true
-        confirm.rs '' '[w]rite' '[i]nput' 'he[s]' '[r]gb' 'hs[l]' | read -l response
-        switch "$response"
-            case w
-                set -l picked_name (input_name)
-                if test "$picked_name"
-                    coloring-book.nu write -- "'$color'" $picked_name
+        not test "$color" && return
+        set color (pastel format hex $color)
+        pastel color $color
+        set_color $color
+        echo -n extended example text
+        set_color reset
+        echo -n '   '
+        set_color (pastel textcolor $color | pastel format hex)
+        set_color -b $color
+        echo -n extended example text
+        set_color reset
+        echo
+        echo
+        while true
+            confirm.rs '' '[w]rite' '[i]nput' '[e]dit' '[s]ex' '[c]rgb' '[d]hsl' '[f]lip' '' \
+                '[j]ue' '[J]ue' '[l]ight' '[L]ight' 'sa[k]urate' 'sa[K]urate' | read -l response
+            switch "$response"
+                case w
+                    set -l picked_name (input_name)
+                    if test "$picked_name"
+                        coloring-book.nu write -- "'$color'" $picked_name
+                        reconfirm
+                        warnage "color “$picked_name” written"
+                        continue
+                    end
+                case e
                     reconfirm
-                    warnage "color “$picked_name” written"
+                    niri msg action set-column-width 85%
+                    niri msg action center-column
+                    helix ~/fes/dot/colors.nuon
+                    niri msg action set-column-width 70%
+                    niri msg action center-column
+                case s
+                    set -l hex_repr (pastel format hex -- $color)
+                    echo $hex_repr | wl-copy -n
+                    reconfirm
+                    warnage "$hex_repr copied"
                     continue
-                end
-            case s
-                set -l hex_repr (pastel format hex -- $color)
-                echo $hex_repr | wl-copy -n
-                reconfirm
-                warnage "$hex_repr copied"
-                continue
-            case r
-                set -l rgb_repr (pastel format rgb -- $color)
-                echo $rgb_repr | wl-copy -n
-                reconfirm
-                warnage "$rgb_repr copied"
-                continue
-            case l
-                set -l hsl_repr (pastel format hsl -- $color)
-                echo $hsl_repr | wl-copy -n
-                reconfirm
-                warnage "$hsl_repr copied"
-                continue
-            case i
-                reconfirm
-                return
+                case r
+                    set -l rgb_repr (pastel format rgb -- $color)
+                    echo $rgb_repr | wl-copy -n
+                    reconfirm
+                    warnage "$rgb_repr copied"
+                    continue
+                case d
+                    set -l hsl_repr (pastel format hsl -- $color)
+                    echo $hsl_repr | wl-copy -n
+                    reconfirm
+                    warnage "$hsl_repr copied"
+                    continue
+                case f
+                    reconfirm
+                    warnage "flip $color"
+                    set color (pastel complement -- $color)
+                case L
+                    reconfirm
+                    warnage "dark $color"
+                    set color (pastel darken 0.01 -- $color)
+                case l
+                    reconfirm
+                    warnage "light $color"
+                    set color (pastel lighten 0.01 -- $color)
+                case K
+                    reconfirm
+                    warnage "saturate ↓ $color"
+                    set color (pastel desaturate 0.01 -- $color)
+                case k
+                    reconfirm
+                    warnage "saturate ↑ $color"
+                    set color (pastel saturate 0.01 -- $color)
+                case j
+                    reconfirm
+                    warnage "rotate → $color"
+                    set color (pastel rotate 1 -- $color)
+                case J
+                    reconfirm
+                    warnage "rotate ← $color"
+                    set color (pastel rotate -- -1 $color)
+                case i
+                    reconfirm
+                    return
+            end
+            break
         end
-        break
     end
-    act_on_color $color
 end
 
-set -g first_print true
 while true
-    if $first_print
-        coloring-book.nu all
-        set -g first_print false
-    end
+    coloring-book.nu all
     set -l color (input_color)
     if not test "$color"
         coloring-book.nu all
