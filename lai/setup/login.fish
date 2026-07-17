7ce64e496cef97d73a7725fba0f92476d5d2e9ea
Axlefublr <axlefublr.ls@gmail.com>
2026-07-18 00:29:26 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -5,17 +5,19 @@ cat ~/fes/uviw/afen/sudo | sudo -S modprobe uinput
 systemctl --user restart kanata.service
 
 niri msg action focus-workspace main
-foottitled.sh nugleterm -N nu
-foottitled.sh toggleterm -N
+foot -T nugleterm -o environment.TIT=nugleterm nu & disown
+foot -T toggleterm -o environment.TIT=toggleterm & disown
+foot -T dotfiles -o environment.TIT=dotfiles -D ~/fes/dot helix & disown
 ntf_dismiss_all
-foottitled.sh dotfiles -ND ~/fes/dot helix
 gtk-launch firefox
-foottitled.sh voe -ND ~/iwm/voe yazi
-foottitled.sh finances -N ov --status-line=false --follow-name ~/.local/share/magazine/J
-foottitled.sh earnings -N earnings.fish
-foottitled.sh loago-tracker -N fish -c loago_tracker
-foottitled.sh receiver -N receiver.fish
+foot -T finances -o environment.TIT=finances ov --status-line=false --follow-name ~/.local/share/magazine/J & disown
+foot -T earnings -o environment.TIT=earnings earnings.fish & disown
+foot -T loago-tracker -o environment.TIT=loago-tracker fish -c loago_tracker & disown
+foot -T receiver -o environment.TIT=receiver receiver.fish & disown
 playerctld daemon
 gtk-launch anki
+wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
 niri msg action spawn -- Todoist.AppImage
+wm_wait_if_or_until_exists 'app_id == todoist'
+foot -T voe -o environment.TIT=voe -D ~/iwm/voe yazi & disown
 make-em-shut-up.nu
