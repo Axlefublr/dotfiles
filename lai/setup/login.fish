0420aa3ca3bbf7b88202ae7d1cc5052388bfa42e
Axlefublr <axlefublr.ls@gmail.com>
2026-07-21 12:20:46 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -12,12 +12,13 @@ ntf_dismiss_all
 gtk-launch firefox
 foot -T finances -o environment.TIT=finances ov --status-line=false --follow-name ~/.local/share/magazine/J & disown
 foot -T earnings -o environment.TIT=earnings earnings.fish & disown
+wm_wait_if_or_until_exists 'app_id starts-with foot' 'title == earnings'
 foot -T loago-tracker -o environment.TIT=loago-tracker fish -c loago_tracker & disown
 foot -T receiver -o environment.TIT=receiver receiver.fish & disown
-gtk-launch anki
-wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
+foot -T voe -o environment.TIT=voe -D ~/iwm/voe yazi & disown
 niri msg action spawn -- Todoist.AppImage
 wm_wait_if_or_until_exists 'app_id == todoist'
-foot -T voe -o environment.TIT=voe -D ~/iwm/voe yazi & disown
+gtk-launch anki
+wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
 make-em-shut-up.nu
 playerctld daemon
