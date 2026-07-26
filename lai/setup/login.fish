543d3a78c0d8fcc7e62767a041620191da6a9704
Axlefublr <axlefublr.ls@gmail.com>
2026-07-26 12:11:19 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -13,6 +13,10 @@ niri msg action consume-or-expel-window-left
 foot -T dotfiles -o environment.TIT=dotfiles -D ~/fes/dot helix & disown
 ntf_dismiss_all
 gtk-launch firefox
+niri msg action spawn -- Todoist.AppImage
+wm_wait_if_or_until_exists 'app_id == todoist'
+gtk-launch anki
+wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
 foot -T finances -o environment.TIT=finances ov --status-line=false --follow-name ~/.local/share/magazine/J & disown
 wm_wait_if_or_until_exists 'app_id starts-with foot' 'title == finances'
 foot -T earnings -o environment.TIT=earnings earnings.fish & disown
@@ -20,9 +24,5 @@ wm_wait_if_or_until_exists 'app_id starts-with foot' 'title == earnings'
 foot -T loago-tracker -o environment.TIT=loago-tracker fish -c loago_tracker & disown
 foot -T receiver -o environment.TIT=receiver receiver.fish & disown
 foot -T voe -o environment.TIT=voe -D ~/iwm/voe yazi & disown
-niri msg action spawn -- Todoist.AppImage
-wm_wait_if_or_until_exists 'app_id == todoist'
-gtk-launch anki
-wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
 make-em-shut-up.nu
 playerctld daemon
