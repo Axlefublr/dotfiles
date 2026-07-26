5a2fa5a0bbb9bc491b132c2cd0c96c7afd6efaae
Axlefublr <axlefublr.ls@gmail.com>
2026-07-26 12:11:19 +0800

    change lai/timer.fish

--- lai/timer.fish
+++ lai/timer.fish
@@ -10,35 +10,20 @@ if not test "$argv[4..]"
     return
 end
 while true
-    for time in $argv[4..]
+    for resolution in (termdown-parser.rs $argv[4..])
+        set -l the (string split \t -- $resolution)
+        set -l time $the[1]
+        set -l resolved_time $the[2]
         set -l genre (resolve_time_counter_genre "$time")
         set -l title "$genre $name $time"
         while true
             tit $title
             notify-send "💣️ $title"
             set -l skip_prompt false
-            set -l passed_time (
-                set -l intermediate_time $time
-                if test (string sub -s -1 -- $time) = '!'
-                    set intermediate_time (string sub -e -1 -- $time)
-                    set skip_prompt true
-                end
-                if not string match -qe ':' -- $intermediate_time
-                    echo $intermediate_time
-                else
-                    set -l current_hour (date +%H)
-                    set -l current_minute (date +%M)
-                    set -l the (string split ':' -- $intermediate_time)
-                    set -l passed_hour $the[1]
-                    set -l passed_minute $the[2]
-                    if test $current_hour -gt $passed_hour -o \( $current_hour -eq $passed_hour -a $current_minute -ge $passed_minute \)
-                        echo "$(date -d 'today + 1 day' '+%Y-%m-%d') $intermediate_time"
-                    else
-                        echo $intermediate_time
-                    end
-                end
-            )
-            termdown -BW $passed_time
+            if test (string sub -s -1 -- $time) = '!'
+                set skip_prompt true
+            end
+            termdown -BW $resolved_time
             # not using the foot-configured bell here because I *usually* don't want infinitely staying notifications on bell, but *can* afford them here thanks to custom fancy logic
             if not $skip_prompt
                 tit "⏰ $title"
