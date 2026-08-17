561354e8a6cc273ebb5e205879bbee914f6e7508
Axlefublr <axlefublr.ls@gmail.com>
2026-08-17 21:47:47 +0800

    change lai/setup/login.fish

--- lai/setup/login.fish
+++ lai/setup/login.fish
@@ -20,9 +20,9 @@ wm_wait_if_or_until_exists 'app_id starts-with foot' 'title == earnings'
 foot -T loago-tracker -o environment.TIT=loago-tracker fish -c loago_tracker & disown
 foot -T receiver -o environment.TIT=receiver receiver.fish & disown
 gtk-launch anki
-niri msg action mark-window-focused --id (wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User')
+wm_wait_if_or_until_exists 'app_id == anki' 'title starts-with User'
 niri msg action spawn -- Todoist.AppImage
-niri msg action mark-window-focused --id (wm_wait_if_or_until_exists 'app_id == todoist')
+wm_wait_if_or_until_exists 'app_id == todoist'
 foot -T voe -o environment.TIT=voe -D ~/iwm/voe yazi & disown
 make-em-shut-up.nu
 playerctld daemon
