29149d832e7028c80fdf2fc3d149e4fdb2d05ff0
Axlefublr <axlefublr.ls@gmail.com>
2026-09-01 01:07:49 +0800

    change lai/cargo-doc-quick.fish

--- lai/cargo-doc-quick.fish
+++ lai/cargo-doc-quick.fish
@@ -1,25 +1,21 @@
 #!/usr/bin/env fish
 
-function open -a file package
-    firefox --new-window $file &>/dev/null
-    set -l id (wm_wait_if_or_until_exists 'app_id == firefox' "title == '$package - Rust — Mozilla Firefox'")
+function open -a title_match
+    set -l id (wm_wait_until_exists 'app_id == firefox' $title_match)
     niri msg action focus-window --id $id
     niri msg action move-column-to-workspace help
     niri msg action move-column-to-last
 end
 
+set -gx BROWSER browser-new-window.sh
 set -l package (cargo.nu default-package)
 set -l file ./target/doc/$package/index.html
-test -f $file
-set -l did_file_exist $status
-if test $did_file_exist -eq 0
-    open $file $package
-end
-# I'm making myself encode the specific cargo doc parameters so that I don't have to maintain it in two places
-$argv
-if test $did_file_exist -ne 0 -a -f $file
-    open $file $package
-else if not test -f $file
-    # we probably figured out the target file wrong, shouldn't happen but last resort
-    $argv --open
+
+if test -f $file
+    $BROWSER $file &>/dev/null
+    open "title == '$package - Rust — Mozilla Firefox'"
+    $argv
+else
+    $argv --open # chances are, this is the first time I'm opening the docs for this project, so landing on the correct package is significant
+    open "title ends-with ' - Rust — Mozilla Firefox'"
 end
