78f30f744cac924bfb4f83a77b26d1d0398a487e
Axlefublr <axlefublr.ls@gmail.com>
2026-09-02 13:13:47 +0800

    change lai/fool/help.fish

--- lai/fool/help.fish
+++ lai/fool/help.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
+argparse -i from-current -- $argv
 set -gx COLUMNS 130
 set -l help_page "$argv"
 set -l dir ~/fes/zufi/men
@@ -9,3 +10,8 @@ indeed.rs -u ~/.local/share/magazine/a-comma $input
 $argv --help &>$path
 wm_focus_if_exists "app_id starts-with foot" "title == 'help $help_page'"
 or flour --disown -T "help $help_page" --disown --man $path:14
+if not set -q _flag_from_current
+    set -l id (wm_wait_if_or_until_exists "app_id starts-with foot" "title == 'help $help_page'")
+    niri msg action focus-window --id $id
+    niri msg action move-column-to-last
+end
