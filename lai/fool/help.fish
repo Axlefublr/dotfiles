cb518629992ffb24ccd1b1be0af8d1a51432d5e8
Axlefublr <axlefublr.ls@gmail.com>
2026-09-03 12:20:35 +0800

    change lai/fool/help.fish

--- lai/fool/help.fish
+++ lai/fool/help.fish
@@ -6,7 +6,7 @@ set -l help_page "$argv"
 set -l dir ~/fes/zufi/men
 mkdir -p $dir
 set -l path $dir/$help_page.help
-indeed.rs -u ~/.local/share/magazine/a-comma $input
+indeed.rs -u -- ~/.local/share/magazine/a-comma "$help_page"
 $argv --help &>$path
 wm_focus_if_exists "app_id starts-with foot" "title == 'help $help_page'"
 or flour --disown -T "help $help_page" --disown --man $path:14
