8ba47489d5b4fe2fd9998208daff76f64ff792ad
Axlefublr <axlefublr.ls@gmail.com>
2026-09-03 12:20:35 +0800

    change swayimg/swayimg.lua

--- swayimg/swayimg.lua
+++ swayimg/swayimg.lua
@@ -44,12 +44,17 @@ for _, section in ipairs({ 'gallery', 'viewer' }) do
 	swayimg[section].on_key('Shift+i', function() os.execute(string.format("si.fish %q", swayimg[section].get_image().path)) end)
 	swayimg[section].on_key('c', function() swayimg.imagelist.remove(swayimg[section].get_image().path) end)
 	swayimg[section].on_key('d', function() local path = swayimg[section].get_image().path  os.execute(string.format("trash-put %q", path))  swayimg.imagelist.remove(path) end)
-	swayimg[section].on_key('g', function() os.execute(string.format('niri msg action spawn -- footclient -N yazi %q', swayimg[section].get_image().path))  swayimg.exit() end)
 	swayimg[section].on_key('i', function() os.execute(string.format("sl.fish %q", swayimg[section].get_image().path)) end)
 	swayimg[section].on_key('s', function() os.execute(string.format("fish -c 'echo %q | wl-copy -nt text/plain'", swayimg[section].get_image().path)) end)
 	swayimg[section].on_key('v', function() swayimg.set_fullscreen() end)
 	swayimg[section].on_key('x', function() if swayimg.text.visible() then swayimg.text.hide() else swayimg.text.show() end end)
 	-- [[sort off]]
+	swayimg[section].on_key('g', function()
+		local path = swayimg[section].get_image().path
+		local fish_command = string.format('niri msg action spawn -- footclient -NT swayimg-(path basename (path dirname %q)) yazi %q', path, path)
+		os.execute(string.format('fish -c %q', fish_command))
+		swayimg.exit()
+	end)
 end
 
 -- [[sort on]]
