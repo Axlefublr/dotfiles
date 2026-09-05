8eed1146a836607906923d98cc3230a9592756cc
Axlefublr <axlefublr.ls@gmail.com>
2026-09-05 18:17:48 +0800

    change lai/receiver.fish

--- lai/receiver.fish
+++ lai/receiver.fish
@@ -6,7 +6,7 @@ function _receive_sigint --on-signal INT
 end
 rm -f ~/fes/zufi/tms/receiver-ongoing 2>/dev/null
 mkfifo ~/fes/zufi/task-scheduler 2>/dev/null
-set -l this_window_id (na -c 'niri msg -j windows | from json | where app_id starts-with foot | where title == "receiver" | get id | first')
+set -l this_window_id (wm_wait_if_or_until_exists 'app_id starts-with foot' 'title == "receiver"')
 # this bash wrapper (that *turns* into the direct tail process thanks to `exec`) protects `tail` from ctrl+c; without it, ctrl+c-ing `fish -c $second` *also* ctrl+c-s the tail, ending the listening loop, and requiring me to restart the receiver.fish window (and I think losing data?)
 bash -c 'trap "" INT ; exec tail -f ~/fes/zufi/task-scheduler' | while read -z first
     and read -z second
@@ -31,9 +31,9 @@ bash -c 'trap "" INT ; exec tail -f ~/fes/zufi/task-scheduler' | while read -z f
             echo
             break
         end
-        set_color -o (test $stored_status -eq 130 && echo e49641 || echo ea6962)
         notify-send "❌ $first_word"
         niri msg action set-window-urgent --id $this_window_id
+        set_color -o (test $stored_status -eq 130 && echo e49641 || echo ea6962)
         confirm.rs (test $stored_status -eq 130 && echo canceled || echo failure) '[r]estart' '[e]dit' 'pro[j]eed' | read -l response
         set_color normal
         echo
